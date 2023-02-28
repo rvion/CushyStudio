6b43ef4cdfb59fbc6d8356f1d7bdcb951e6f3617
rvion <vion.remi@gmail.com>
2023-02-28 22:46:22 +0100

    better builder

--- src/builder.gen.ts
+++ src/builder.gen.ts
@@ -2,7 +2,7 @@ import spec from '../spec/nodes-2023-02-28.json' assert { type: 'json' }
 const entries = Object.entries(spec)
 
 export type NodeInput = { name: string; type: string; opts: any }
-export type NodeOutput = { name: string; type: string; opts: any }
+export type NodeOutput = { type: string; name: string }
 export type EnumHash = string
 export type EnumName = string
 export class NodeDecl {
@@ -16,16 +16,24 @@ export class NodeDecl {
     codegen() {
         const out: string[] = []
         const p = (x: string) => out.push(x)
-        p(`type ${this.name}_input = {`)
+        // inputs
+        p(`\n// ${this.name} -------------------------------`)
+        p(`export class ${this.name} {`)
+        p(`    constructor(public p: ${this.name}_input){}`)
+        // p(`    }`)
+        p(`}`)
+        p(`export type ${this.name}_input = {`)
         this.inputs.forEach((i) => {
             p(`    ${i.name}: ${i.type}`)
         })
         p(`}`)
-        p(`type ${this.name}_output = any`)
-        p(`class ${this.name} {`)
-        p(`    constructor(public p: ${this.name}_input){`)
-        p(`    }`)
+        // outputs
+        p(`export type ${this.name}_output = {`)
+        this.outputs.forEach((i) => {
+            p(`    ${i.name}: ${i.type}`)
+        })
         p(`}`)
+
         return out.join('\n')
     }
 }
@@ -45,7 +53,13 @@ export class MAIN {
             const outputs: NodeOutput[] = []
             const node = new NodeDecl(nodeName, inputs, outputs)
             this.nodes.push(node)
-
+            const outputNamer: { [key: string]: number } = {}
+            for (const opt of nodeDef.output) {
+                const at = outputNamer[opt] ??= 0
+                const name = at === 0 ? opt : `${opt}_${at}`
+                outputs.push({ type: opt, name })
+                outputNamer[opt]++
+            }
             for (const ipt of requiredInputs) {
                 const inputName = ipt[0]
                 const typeDef = ipt[1]
@@ -85,16 +99,15 @@ export class MAIN {
     }
     codegen = (): string => {
         return [
-            `// TYPES`,
+            `// TYPES -------------------------------`,
             ...[...this.knownTypes.values()].map((t) => `type ${t} = any`),
-            `// ENUMS`,
+            `\n// ENUMS -------------------------------`,
             ...[...this.knownEnums.values()].map((e) => {
                 if (e.values.length > 0) {
                     return `type ${e.name} = ${e.values.map((v) => `'${v}'`).join(' | ')}`
                 }
                 return `type ${e.name} = never`
             }),
-            `// NODES`,
             this.nodes.map((n) => n.codegen()).join('\n'),
         ].join('\n')
     }
--- src/builder.ts
+++ src/builder.ts
@@ -1,4 +1,4 @@
-// TYPES
+// TYPES -------------------------------
 type MODEL = any
 type INT = any
 type FLOAT = any
@@ -10,10 +10,36 @@ type VAE = any
 type IMAGE = any
 type MASK = any
 type CONTROL_NET = any
-// ENUMS
-type enum_KSampler_sampler_name = 'sample_euler' | 'sample_euler_ancestral' | 'sample_heun' | 'sample_dpm_2' | 'sample_dpm_2_ancestral' | 'sample_lms' | 'sample_dpm_fast' | 'sample_dpm_adaptive' | 'sample_dpmpp_2s_ancestral' | 'sample_dpmpp_sde' | 'sample_dpmpp_2m' | 'ddim' | 'uni_pc' | 'uni_pc_bh2'
+
+// ENUMS -------------------------------
+type enum_KSampler_sampler_name =
+    | 'sample_euler'
+    | 'sample_euler_ancestral'
+    | 'sample_heun'
+    | 'sample_dpm_2'
+    | 'sample_dpm_2_ancestral'
+    | 'sample_lms'
+    | 'sample_dpm_fast'
+    | 'sample_dpm_adaptive'
+    | 'sample_dpmpp_2s_ancestral'
+    | 'sample_dpmpp_sde'
+    | 'sample_dpmpp_2m'
+    | 'ddim'
+    | 'uni_pc'
+    | 'uni_pc_bh2'
 type enum_KSampler_scheduler = 'karras' | 'normal' | 'simple' | 'ddim_uniform'
-type enum_CheckpointLoader_config_name = 'anything_v3.yaml' | 'v1-inference.yaml' | 'v1-inference_clip_skip_2.yaml' | 'v1-inference_clip_skip_2_fp16.yaml' | 'v1-inference_fp16.yaml' | 'v1-inpainting-inference.yaml' | 'v2-inference-v.yaml' | 'v2-inference-v_fp32.yaml' | 'v2-inference.yaml' | 'v2-inference_fp32.yaml' | 'v2-inpainting-inference.yaml'
+type enum_CheckpointLoader_config_name =
+    | 'anything_v3.yaml'
+    | 'v1-inference.yaml'
+    | 'v1-inference_clip_skip_2.yaml'
+    | 'v1-inference_clip_skip_2_fp16.yaml'
+    | 'v1-inference_fp16.yaml'
+    | 'v1-inpainting-inference.yaml'
+    | 'v2-inference-v.yaml'
+    | 'v2-inference-v_fp32.yaml'
+    | 'v2-inference.yaml'
+    | 'v2-inference_fp32.yaml'
+    | 'v2-inpainting-inference.yaml'
 type enum_CheckpointLoader_ckpt_name = 'AbyssOrangeMix2_hard.safetensors' | 'v1-5-pruned-emaonly.ckpt'
 type enum_VAELoader_vae_name = 'vae-ft-mse-840000-ema-pruned.safetensors'
 type enum_LatentUpscale_upscale_method = 'nearest-exact' | 'bilinear' | 'area'
@@ -27,8 +53,12 @@ type enum_LatentFlip_flip_method = 'x-axis: vertically' | 'y-axis: horizontally'
 type enum_LoraLoader_lora_name = 'charturnerbetaLora_charturnbetalora.safetensors'
 type enum_CLIPLoader_clip_name = never
 type enum_ControlNetLoader_control_net_name = 'controlnetPreTrained_openposeV10.safetensors'
-// NODES
-type KSampler_input = {
+
+// KSampler -------------------------------
+export class KSampler {
+    constructor(public p: KSampler_input) {}
+}
+export type KSampler_input = {
     model: MODEL
     seed: INT
     steps: INT
@@ -40,143 +70,188 @@ type KSampler_input = {
     latent_image: LATENT
     denoise: FLOAT
 }
-type KSampler_output = any
-class KSampler {
-    constructor(public p: KSampler_input){
-    }
+export type KSampler_output = {
+    LATENT: LATENT
 }
-type CheckpointLoader_input = {
+
+// CheckpointLoader -------------------------------
+export class CheckpointLoader {
+    constructor(public p: CheckpointLoader_input) {}
+}
+export type CheckpointLoader_input = {
     config_name: enum_CheckpointLoader_config_name
     ckpt_name: enum_CheckpointLoader_ckpt_name
 }
-type CheckpointLoader_output = any
-class CheckpointLoader {
-    constructor(public p: CheckpointLoader_input){
-    }
+export type CheckpointLoader_output = {
+    MODEL: MODEL
+    CLIP: CLIP
+    VAE: VAE
 }
-type CLIPTextEncode_input = {
+
+// CLIPTextEncode -------------------------------
+export class CLIPTextEncode {
+    constructor(public p: CLIPTextEncode_input) {}
+}
+export type CLIPTextEncode_input = {
     text: STRING
     clip: CLIP
 }
-type CLIPTextEncode_output = any
-class CLIPTextEncode {
-    constructor(public p: CLIPTextEncode_input){
-    }
+export type CLIPTextEncode_output = {
+    CONDITIONING: CONDITIONING
 }
-type VAEDecode_input = {
+
+// VAEDecode -------------------------------
+export class VAEDecode {
+    constructor(public p: VAEDecode_input) {}
+}
+export type VAEDecode_input = {
     samples: LATENT
     vae: VAE
 }
-type VAEDecode_output = any
-class VAEDecode {
-    constructor(public p: VAEDecode_input){
-    }
+export type VAEDecode_output = {
+    IMAGE: IMAGE
 }
-type VAEEncode_input = {
+
+// VAEEncode -------------------------------
+export class VAEEncode {
+    constructor(public p: VAEEncode_input) {}
+}
+export type VAEEncode_input = {
     pixels: IMAGE
     vae: VAE
 }
-type VAEEncode_output = any
-class VAEEncode {
-    constructor(public p: VAEEncode_input){
-    }
+export type VAEEncode_output = {
+    LATENT: LATENT
 }
-type VAEEncodeForInpaint_input = {
+
+// VAEEncodeForInpaint -------------------------------
+export class VAEEncodeForInpaint {
+    constructor(public p: VAEEncodeForInpaint_input) {}
+}
+export type VAEEncodeForInpaint_input = {
     pixels: IMAGE
     vae: VAE
     mask: MASK
 }
-type VAEEncodeForInpaint_output = any
-class VAEEncodeForInpaint {
-    constructor(public p: VAEEncodeForInpaint_input){
-    }
+export type VAEEncodeForInpaint_output = {
+    LATENT: LATENT
 }
-type VAELoader_input = {
+
+// VAELoader -------------------------------
+export class VAELoader {
+    constructor(public p: VAELoader_input) {}
+}
+export type VAELoader_input = {
     vae_name: enum_VAELoader_vae_name
 }
-type VAELoader_output = any
-class VAELoader {
-    constructor(public p: VAELoader_input){
-    }
+export type VAELoader_output = {
+    VAE: VAE
 }
-type EmptyLatentImage_input = {
+
+// EmptyLatentImage -------------------------------
+export class EmptyLatentImage {
+    constructor(public p: EmptyLatentImage_input) {}
+}
+export type EmptyLatentImage_input = {
     width: INT
     height: INT
     batch_size: INT
 }
-type EmptyLatentImage_output = any
-class EmptyLatentImage {
-    constructor(public p: EmptyLatentImage_input){
-    }
+export type EmptyLatentImage_output = {
+    LATENT: LATENT
 }
-type LatentUpscale_input = {
+
+// LatentUpscale -------------------------------
+export class LatentUpscale {
+    constructor(public p: LatentUpscale_input) {}
+}
+export type LatentUpscale_input = {
     samples: LATENT
     upscale_method: enum_LatentUpscale_upscale_method
     width: INT
     height: INT
     crop: enum_LatentUpscale_crop
 }
-type LatentUpscale_output = any
-class LatentUpscale {
-    constructor(public p: LatentUpscale_input){
-    }
+export type LatentUpscale_output = {
+    LATENT: LATENT
 }
-type SaveImage_input = {
+
+// SaveImage -------------------------------
+export class SaveImage {
+    constructor(public p: SaveImage_input) {}
+}
+export type SaveImage_input = {
     images: IMAGE
     filename_prefix: STRING
 }
-type SaveImage_output = any
-class SaveImage {
-    constructor(public p: SaveImage_input){
-    }
+export type SaveImage_output = {}
+
+// LoadImage -------------------------------
+export class LoadImage {
+    constructor(public p: LoadImage_input) {}
 }
-type LoadImage_input = {
+export type LoadImage_input = {
     image: enum_LoadImage_image
 }
-type LoadImage_output = any
-class LoadImage {
-    constructor(public p: LoadImage_input){
-    }
+export type LoadImage_output = {
+    IMAGE: IMAGE
 }
-type LoadImageMask_input = {
+
+// LoadImageMask -------------------------------
+export class LoadImageMask {
+    constructor(public p: LoadImageMask_input) {}
+}
+export type LoadImageMask_input = {
     image: enum_LoadImage_image
     channel: enum_LoadImageMask_channel
 }
-type LoadImageMask_output = any
-class LoadImageMask {
-    constructor(public p: LoadImageMask_input){
-    }
+export type LoadImageMask_output = {
+    MASK: MASK
 }
-type ImageScale_input = {
+
+// ImageScale -------------------------------
+export class ImageScale {
+    constructor(public p: ImageScale_input) {}
+}
+export type ImageScale_input = {
     image: IMAGE
     upscale_method: enum_LatentUpscale_upscale_method
     width: INT
     height: INT
     crop: enum_LatentUpscale_crop
 }
-type ImageScale_output = any
-class ImageScale {
-    constructor(public p: ImageScale_input){
-    }
+export type ImageScale_output = {
+    IMAGE: IMAGE
 }
-type ImageInvert_input = {
+
+// ImageInvert -------------------------------
+export class ImageInvert {
+    constructor(public p: ImageInvert_input) {}
+}
+export type ImageInvert_input = {
     image: IMAGE
 }
-type ImageInvert_output = any
-class ImageInvert {
-    constructor(public p: ImageInvert_input){
-    }
+export type ImageInvert_output = {
+    IMAGE: IMAGE
 }
-type ConditioningCombine_input = {
+
+// ConditioningCombine -------------------------------
+export class ConditioningCombine {
+    constructor(public p: ConditioningCombine_input) {}
+}
+export type ConditioningCombine_input = {
     conditioning_1: CONDITIONING
     conditioning_2: CONDITIONING
 }
-type ConditioningCombine_output = any
-class ConditioningCombine {
-    constructor(public p: ConditioningCombine_input){
-    }
+export type ConditioningCombine_output = {
+    CONDITIONING: CONDITIONING
 }
-type ConditioningSetArea_input = {
+
+// ConditioningSetArea -------------------------------
+export class ConditioningSetArea {
+    constructor(public p: ConditioningSetArea_input) {}
+}
+export type ConditioningSetArea_input = {
     conditioning: CONDITIONING
     width: INT
     height: INT
@@ -184,12 +259,15 @@ type ConditioningSetArea_input = {
     y: INT
     strength: FLOAT
 }
-type ConditioningSetArea_output = any
-class ConditioningSetArea {
-    constructor(public p: ConditioningSetArea_input){
-    }
+export type ConditioningSetArea_output = {
+    CONDITIONING: CONDITIONING
 }
-type KSamplerAdvanced_input = {
+
+// KSamplerAdvanced -------------------------------
+export class KSamplerAdvanced {
+    constructor(public p: KSamplerAdvanced_input) {}
+}
+export type KSamplerAdvanced_input = {
     model: MODEL
     add_noise: enum_KSamplerAdvanced_add_noise
     noise_seed: INT
@@ -204,125 +282,160 @@ type KSamplerAdvanced_input = {
     end_at_step: INT
     return_with_leftover_noise: enum_KSamplerAdvanced_return_with_leftover_noise
 }
-type KSamplerAdvanced_output = any
-class KSamplerAdvanced {
-    constructor(public p: KSamplerAdvanced_input){
-    }
+export type KSamplerAdvanced_output = {
+    LATENT: LATENT
 }
-type SetLatentNoiseMask_input = {
+
+// SetLatentNoiseMask -------------------------------
+export class SetLatentNoiseMask {
+    constructor(public p: SetLatentNoiseMask_input) {}
+}
+export type SetLatentNoiseMask_input = {
     samples: LATENT
     mask: MASK
 }
-type SetLatentNoiseMask_output = any
-class SetLatentNoiseMask {
-    constructor(public p: SetLatentNoiseMask_input){
-    }
+export type SetLatentNoiseMask_output = {
+    LATENT: LATENT
 }
-type LatentComposite_input = {
+
+// LatentComposite -------------------------------
+export class LatentComposite {
+    constructor(public p: LatentComposite_input) {}
+}
+export type LatentComposite_input = {
     samples_to: LATENT
     samples_from: LATENT
     x: INT
     y: INT
     feather: INT
 }
-type LatentComposite_output = any
-class LatentComposite {
-    constructor(public p: LatentComposite_input){
-    }
+export type LatentComposite_output = {
+    LATENT: LATENT
 }
-type LatentRotate_input = {
+
+// LatentRotate -------------------------------
+export class LatentRotate {
+    constructor(public p: LatentRotate_input) {}
+}
+export type LatentRotate_input = {
     samples: LATENT
     rotation: enum_LatentRotate_rotation
 }
-type LatentRotate_output = any
-class LatentRotate {
-    constructor(public p: LatentRotate_input){
-    }
+export type LatentRotate_output = {
+    LATENT: LATENT
 }
-type LatentFlip_input = {
+
+// LatentFlip -------------------------------
+export class LatentFlip {
+    constructor(public p: LatentFlip_input) {}
+}
+export type LatentFlip_input = {
     samples: LATENT
     flip_method: enum_LatentFlip_flip_method
 }
-type LatentFlip_output = any
-class LatentFlip {
-    constructor(public p: LatentFlip_input){
-    }
+export type LatentFlip_output = {
+    LATENT: LATENT
 }
-type LatentCrop_input = {
+
+// LatentCrop -------------------------------
+export class LatentCrop {
+    constructor(public p: LatentCrop_input) {}
+}
+export type LatentCrop_input = {
     samples: LATENT
     width: INT
     height: INT
     x: INT
     y: INT
 }
-type LatentCrop_output = any
-class LatentCrop {
-    constructor(public p: LatentCrop_input){
-    }
+export type LatentCrop_output = {
+    LATENT: LATENT
 }
-type LoraLoader_input = {
+
+// LoraLoader -------------------------------
+export class LoraLoader {
+    constructor(public p: LoraLoader_input) {}
+}
+export type LoraLoader_input = {
     model: MODEL
     clip: CLIP
     lora_name: enum_LoraLoader_lora_name
     strength_model: FLOAT
     strength_clip: FLOAT
 }
-type LoraLoader_output = any
-class LoraLoader {
-    constructor(public p: LoraLoader_input){
-    }
+export type LoraLoader_output = {
+    MODEL: MODEL
+    CLIP: CLIP
 }
-type CLIPLoader_input = {
+
+// CLIPLoader -------------------------------
+export class CLIPLoader {
+    constructor(public p: CLIPLoader_input) {}
+}
+export type CLIPLoader_input = {
     clip_name: enum_CLIPLoader_clip_name
     stop_at_clip_layer: INT
 }
-type CLIPLoader_output = any
-class CLIPLoader {
-    constructor(public p: CLIPLoader_input){
-    }
+export type CLIPLoader_output = {
+    CLIP: CLIP
 }
-type ControlNetApply_input = {
+
+// ControlNetApply -------------------------------
+export class ControlNetApply {
+    constructor(public p: ControlNetApply_input) {}
+}
+export type ControlNetApply_input = {
     conditioning: CONDITIONING
     control_net: CONTROL_NET
     image: IMAGE
     strength: FLOAT
 }
-type ControlNetApply_output = any
-class ControlNetApply {
-    constructor(public p: ControlNetApply_input){
-    }
+export type ControlNetApply_output = {
+    CONDITIONING: CONDITIONING
 }
-type ControlNetLoader_input = {
+
+// ControlNetLoader -------------------------------
+export class ControlNetLoader {
+    constructor(public p: ControlNetLoader_input) {}
+}
+export type ControlNetLoader_input = {
     control_net_name: enum_ControlNetLoader_control_net_name
 }
-type ControlNetLoader_output = any
-class ControlNetLoader {
-    constructor(public p: ControlNetLoader_input){
-    }
+export type ControlNetLoader_output = {
+    CONTROL_NET: CONTROL_NET
 }
-type DiffControlNetLoader_input = {
+
+// DiffControlNetLoader -------------------------------
+export class DiffControlNetLoader {
+    constructor(public p: DiffControlNetLoader_input) {}
+}
+export type DiffControlNetLoader_input = {
     model: MODEL
     control_net_name: enum_ControlNetLoader_control_net_name
 }
-type DiffControlNetLoader_output = any
-class DiffControlNetLoader {
-    constructor(public p: DiffControlNetLoader_input){
-    }
+export type DiffControlNetLoader_output = {
+    CONTROL_NET: CONTROL_NET
 }
-type T2IAdapterLoader_input = {
+
+// T2IAdapterLoader -------------------------------
+export class T2IAdapterLoader {
+    constructor(public p: T2IAdapterLoader_input) {}
+}
+export type T2IAdapterLoader_input = {
     t2i_adapter_name: enum_CLIPLoader_clip_name
 }
-type T2IAdapterLoader_output = any
-class T2IAdapterLoader {
-    constructor(public p: T2IAdapterLoader_input){
-    }
+export type T2IAdapterLoader_output = {
+    CONTROL_NET: CONTROL_NET
 }
-type VAEDecodeTiled_input = {
+
+// VAEDecodeTiled -------------------------------
+export class VAEDecodeTiled {
+    constructor(public p: VAEDecodeTiled_input) {}
+}
+export type VAEDecodeTiled_input = {
     samples: LATENT
     vae: VAE
 }
-type VAEDecodeTiled_output = any
-class VAEDecodeTiled {
-    constructor(public p: VAEDecodeTiled_input){
-    }
+export type VAEDecodeTiled_output = {
+    IMAGE: IMAGE
 }
