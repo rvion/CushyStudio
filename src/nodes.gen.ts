34c427d207c56e407c3ae09a7571adc9f9987c56
rvion <vion.remi@gmail.com>
2023-02-28 21:24:28 +0100

    silence hints and security prompts

--- src/nodes.gen.ts
+++ src/nodes.gen.ts
@@ -2,5 +2,5 @@ import JsonToTS from "https://deno.land/x/json_to_ts@v1.7.0/mod.ts";
 
 const rawJSON = await Deno.readTextFile("./spec/nodes-2023-02-28.json");
 const json = JSON.parse(rawJSON);
-const out = JsonToTS(json);
+const out = ["// deno-lint-ignore-file"].concat(JsonToTS(json));
 await Deno.writeTextFile("./src/nodes.ts", out.join("\n"));
--- src/nodes.ts
+++ src/nodes.ts
@@ -1,3 +1,4 @@
+// deno-lint-ignore-file
 interface RootObject {
   KSampler: KSampler;
   CheckpointLoader: CheckpointLoader;
