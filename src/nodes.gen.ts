371595a04c60740e6f981c6125f4b03f4a4b9238
rvion <vion.remi@gmail.com>
2023-02-28 21:21:45 +0100

    type safe object info

---
+++ src/nodes.gen.ts
@@ -0,0 +1,6 @@
+import JsonToTS from "https://deno.land/x/json_to_ts@v1.7.0/mod.ts";
+
+const rawJSON = await Deno.readTextFile("./spec/nodes-2023-02-28.json");
+const json = JSON.parse(rawJSON);
+const out = JsonToTS(json);
+await Deno.writeTextFile("./src/nodes.ts", out.join("\n"));
