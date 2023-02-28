d6aeb58ac2cb4a5af7a02f436b444fc64843b1be
rvion <vion.remi@gmail.com>
2023-02-28 21:40:25 +0100

    codegen from url directly

--- src/nodes.gen.ts
+++ src/nodes.gen.ts
@@ -1,6 +1,9 @@
-import JsonToTS from "https://deno.land/x/json_to_ts@v1.7.0/mod.ts";
+import JsonToTS from 'https://deno.land/x/json_to_ts@v1.7.0/mod.ts'
+import { config } from 'https://deno.land/x/dotenv@v3.2.2/mod.ts'
 
-const rawJSON = await Deno.readTextFile("./spec/nodes-2023-02-28.json");
-const json = JSON.parse(rawJSON);
-const out = ["// deno-lint-ignore-file"].concat(JsonToTS(json));
-await Deno.writeTextFile("./src/nodes.ts", out.join("\n"));
+const SERVER_IP = config().SERVER_IP
+const res = await fetch(`http://${SERVER_IP}/object_info`)
+const rawJSON = await res.text()
+const json = JSON.parse(rawJSON)
+const out = ['// deno-lint-ignore-file'].concat(JsonToTS(json))
+Deno.writeTextFileSync('./src/nodes.types.ts', out.join('\n'))
