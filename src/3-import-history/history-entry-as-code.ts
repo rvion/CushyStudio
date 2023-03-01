81c6880457ca09f99e17c6a0cf5ab7a72f37a8d4
rvion <vion.remi@gmail.com>
2023-03-01 22:26:14 +0100

    cleanup

--- src/3-import-history/history-entry-as-code.ts
+++ src/3-import-history/history-entry-as-code.ts
@@ -1,16 +1,18 @@
 import { Comfy } from '../2-lib/builder.ts'
-export const c = new Comfy()
-export const CheckpointLoader_4 = c.CheckpointLoader({
+
+export const demo = new Comfy()
+
+export const CheckpointLoader_4 = demo.CheckpointLoader({
     config_name: 'v1-inference.yaml',
     ckpt_name: 'v1-5-pruned-emaonly.ckpt',
 }, '4')
-export const EmptyLatentImage_5 = c.EmptyLatentImage({ width: 512, height: 512, batch_size: 1 }, '5')
-export const CLIPTextEncode_6 = c.CLIPTextEncode({
+export const EmptyLatentImage_5 = demo.EmptyLatentImage({ width: 512, height: 512, batch_size: 1 }, '5')
+export const CLIPTextEncode_6 = demo.CLIPTextEncode({
     text: 'masterpiece best quality girl',
     clip: CheckpointLoader_4.CLIP,
 }, '6')
-export const CLIPTextEncode_7 = c.CLIPTextEncode({ text: 'bad hands', clip: CheckpointLoader_4.CLIP }, '7')
-export const KSampler_3 = c.KSampler({
+export const CLIPTextEncode_7 = demo.CLIPTextEncode({ text: 'bad hands', clip: CheckpointLoader_4.CLIP }, '7')
+export const KSampler_3 = demo.KSampler({
     seed: 8566257,
     steps: 20,
     cfg: 8,
@@ -22,5 +24,5 @@ export const KSampler_3 = c.KSampler({
     negative: CLIPTextEncode_7.CONDITIONING,
     latent_image: EmptyLatentImage_5.LATENT,
 }, '3')
-export const VAEDecode_8 = c.VAEDecode({ samples: KSampler_3.LATENT, vae: CheckpointLoader_4.VAE }, '8')
-export const SaveImage_9 = c.SaveImage({ filename_prefix: 'ComfyUI', images: VAEDecode_8.IMAGE }, '9')
+export const VAEDecode_8 = demo.VAEDecode({ samples: KSampler_3.LATENT, vae: CheckpointLoader_4.VAE }, '8')
+export const SaveImage_9 = demo.SaveImage({ filename_prefix: 'ComfyUI', images: VAEDecode_8.IMAGE }, '9')
--- src/3-import-history/test.ts
+++ src/3-import-history/test.ts
@@ -1,6 +1,6 @@
 import { saveJSONFile } from '../0-utils/saveTsFile.ts'
-import { c } from './history-entry-as-code.ts'
+import { demo } from './history-entry-as-code.ts'
 
-console.log(c.toJSON())
+console.log(demo.toJSON())
 
-saveJSONFile('./src/3-import-history/history-entry.out.json', c)
+saveJSONFile('./src/3-import-history/history-entry.out.json', demo)
