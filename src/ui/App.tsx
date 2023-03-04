01f8d38b6172141b1ab1834d57e354c5bf2e5c63
rvion <vion.remi@gmail.com>
2023-03-04 23:51:20 +0100

    setup vite

---
+++ src/ui/App.tsx
@@ -0,0 +1,4 @@
+import { observer } from 'mobx-react-lite'
+export const App = observer(function App_(p: {}) {
+   return <div>Hello ok ok</div>
+})
---
+++ src/ui/main.tsx
@@ -0,0 +1,6 @@
+import ReactDOM from 'react-dom/client'
+import { App } from './App'
+
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<App />)
