25b5610242ccd65887cdc79a16c76562a5e36407
rvion <vion.remi@gmail.com>
2023-03-07 18:37:44 +0100

    bump spec

--- src/core/ComfyColors.ts
+++ src/core/ComfyColors.ts
@@ -1,7 +1,10 @@
 export const comfyColors: { [category: string]: string } = {
     loaders: '#446011',
     latent: '#464689',
+    'latent/transform': '#464689',
+    //
     conditioning: '#0d430d',
+    'conditioning/style_model': '#0d430d',
     sampling: '#634a1b',
     image: '#541a54',
 }
