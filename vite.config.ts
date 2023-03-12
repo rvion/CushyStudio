39d98b53016f0722cda3bc3ffe785b1f5424dab0
rvion <vion.remi@gmail.com>
2023-03-12 21:52:03 +0100

    vite plugin singlefile

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteSingleFile } from 'vite-plugin-singlefile'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-   plugins: [react()],
+    plugins: [react(), viteSingleFile()],
 })
