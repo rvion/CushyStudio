47ca0cc1db57d80dc8e0be3b37a7644c8f1e3a82
rvion <vion.remi@gmail.com>
2023-03-18 21:20:52 +0100

    tweak initial demo

--- src/ui/samples/a.ts
+++ src/ui/samples/a.ts
@@ -3,7 +3,7 @@ const ckpt = C.CheckpointLoader({ config_name: 'v1-inference_clip_skip_2.yaml', 
 const latent_image = C.EmptyLatentImage({ width: 512, height: 512, batch_size: 1 })
 const positive = C.CLIPTextEncode({ text: 'masterpiece cat', clip: ckpt })
 const negative = C.CLIPTextEncode({ text: 'bad hands', clip: ckpt })
-const sampler = C.KSampler({ seed: 8566257, steps: 20, cfg: 8, sampler_name: 'euler', scheduler: 'normal', denoise: 1, model: ckpt, positive, negative, latent_image, }) // prettier-ignore
+const sampler = C.KSampler({ seed: 8566257, steps: 20, cfg: 8, sampler_name: 'euler', scheduler: 'normal', denoise: 1, model: ckpt, positive, negative, latent_image })
 const vae = C.VAEDecode({ samples: sampler, vae: ckpt })
 const image = C.SaveImage({ filename_prefix: 'ComfyUI', images: vae })
 
@@ -12,6 +12,7 @@ do {
     // sampler.inputs.cfg = 10
     latent_image.inputs.batch_size = batchSize++
     await C.get()
-} while ( await C.askBoolean('Continue?') )
+} while ( batchSize < 3 )
+// } while ( await C.askBoolean('Continue?') )
 
 `
