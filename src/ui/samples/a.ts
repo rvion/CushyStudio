8357e8eb1777ea0e7d3abd70a6a039cf68979b1b
rvion <vion.remi@gmail.com>
2023-03-18 23:06:48 +0100

    cleanup demo script

--- src/ui/samples/a.ts
+++ src/ui/samples/a.ts
@@ -3,9 +3,9 @@ export const a__: string = `export {}
 const ckpt = C.CheckpointLoader({ config_name: 'v1-inference_clip_skip_2.yaml', ckpt_name: 'v1-5-pruned-emaonly.ckpt' })
 const latent_image = C.EmptyLatentImage({ width: 512, height: 512, batch_size: 1 })
 
-const topic = await C.askString('what is your subject about? (e.g. "cat")') /* <- 🔥1🔥 */
+const TOPIC = await C.askString('what is your subject about? (e.g. "cat")')
 
-const positive = C.CLIPTextEncode({ text: 'masterpiece '+ topic /* <- 🔥2🔥 */, clip: ckpt })
+const positive = C.CLIPTextEncode({ text: 'masterpiece '+ TOPIC, clip: ckpt })
 const negative = C.CLIPTextEncode({ text: 'bad hands', clip: ckpt })
 const sampler = C.KSampler({ seed: ${
     Math.round(Math.random() * 1000) /*8566257*/
@@ -16,6 +16,9 @@ const image = C.SaveImage({ filename_prefix: 'ComfyUI', images: vae })
 let batchSize = 1
 do {
     latent_image.inputs.batch_size = batchSize++
+    sampler.inputs.seed ++
     await C.get()
-} while ( await C.askBoolean('Continue?') /* <- 🔥3🔥 */ )
+} while (
+    await C.askBoolean('Continue?')
+)
 `
