08346973069169c478166dda0e0d8e294a531880
rvion <vion.remi@gmail.com>
2023-03-18 14:19:07 +0100

    initial documentation website up

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -11,15 +11,15 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://StableIDE.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/StableIDE/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'rvion', // Usually your GitHub org/user name.
+  projectName: 'StableIDE', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -64,24 +64,15 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'StableIDE',
         logo: {
-          alt: 'My Site Logo',
+          alt: 'StableIDE Logo',
           src: 'img/logo.svg',
         },
         items: [
-          {
-            type: 'doc',
-            docId: 'intro',
-            position: 'left',
-            label: 'Tutorial',
-          },
-          {to: '/blog', label: 'Blog', position: 'left'},
-          {
-            href: 'https://github.com/facebook/docusaurus',
-            label: 'GitHub',
-            position: 'right',
-          },
+          { type: 'doc', docId: 'intro', position: 'left', label: 'Tutorial', },
+          { to: '/blog', label: 'Blog', position: 'left' },
+          { href: 'https://github.com/facebook/docusaurus', label: 'GitHub', position: 'right', },
         ],
       },
       footer: {
