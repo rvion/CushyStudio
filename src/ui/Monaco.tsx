6c7866d21ea2076364322861d7b59c58c2f5391a
rvion <vion.remi@gmail.com>
2023-03-19 18:15:25 +0100

    do not package monaco in the html (a bit too big, let it remain dynamically loaded)

--- src/ui/Monaco.tsx
+++ src/ui/Monaco.tsx
@@ -3,29 +3,32 @@ import { Monaco, TypescriptOptions } from './TypescriptOptions'
 
 import { loader } from '@monaco-editor/react'
 
-import * as monaco from 'monaco-editor'
+// import * as monaco from 'monaco-editor'
 
 // @ts-ignore
-import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker'
+// import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker'
 // import jsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker'
 // import cssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker'
 // import htmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker'
 
 // @ts-ignore
-import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker'
+// import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker'
 
-self.MonacoEnvironment = {
-    getWorker(_, label) {
-        // if (label === 'json') return new jsonWorker()
-        // if (label === 'css' || label === 'scss' || label === 'less') return new cssWorker()
-        // if (label === 'html' || label === 'handlebars' || label === 'razor') return new htmlWorker()
-        if (label === 'typescript' || label === 'javascript') return new tsWorker()
+// self.MonacoEnvironment = {
+//     getWorker(_, label) {
+//         // if (label === 'json') return new jsonWorker()
+//         // if (label === 'css' || label === 'scss' || label === 'less') return new cssWorker()
+//         // if (label === 'html' || label === 'handlebars' || label === 'razor') return new htmlWorker()
+//         if (label === 'typescript' || label === 'javascript') return new tsWorker()
 
-        return new editorWorker()
-    },
-}
+//         return new editorWorker()
+//     },
+// }
 
-loader.config({ monaco })
+loader.config({
+    // load monaco from the cdn
+    paths: { vs: 'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.36.1/min/vs' },
+})
 
 export let globalMonaco = null as Monaco | null
 
