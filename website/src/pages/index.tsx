e3a04abc88bda4a78b9d4bd6e9a71331d29442a3
rvion <vion.remi@gmail.com>
2023-03-19 18:34:59 +0100

    link to release

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -15,7 +15,7 @@ function HomepageHeader() {
                 <h1 className='hero__title'>{siteConfig.title}</h1>
                 <p className='hero__subtitle'>{siteConfig.tagline}</p>
                 <div className={styles.buttons}>
-                    <Link className='button button--secondary button--lg' to='/docs/intro'>
+                    <Link className='button button--secondary button--lg' to='/0.0.3-2023-03-19-index.html'>
                         Start the IDE
                     </Link>
                 </div>
