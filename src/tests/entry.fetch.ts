c35acdf8d6f30aaeaee12931da890e13ed794602
rvion <vion.remi@gmail.com>
2023-03-21 22:17:09 +0100

    cleanup

--- src/tests/entry.fetch.ts
+++
@@ -1,9 +0,0 @@
-// ▶️ f5
-// ▶️ y sample
-
-import { writeFileSync } from 'fs'
-
-const x = await fetch('http://192.168.1.19:8188/history', {}).then((x) => x.json())
-
-const def = (Object.values(x)[0] as any).prompt[2]
-writeFileSync('./src/compiler/entry.in.json', JSON.stringify(def, null, 4))
