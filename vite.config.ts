669f66aaa81c6376e408697d99a8f4de881180cd
rvion <vion.remi@gmail.com>
2023-03-22 13:51:20 +0100

    misc

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { viteSingleFile } from 'vite-plugin-singlefile'
+// import { viteSingleFile } from 'vite-plugin-singlefile'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), viteSingleFile()],
+    plugins: [
+        //
+        react(),
+        // viteSingleFile(),
+    ],
 })
