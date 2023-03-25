c328c4d2b6aa0da345ad82f0d3ba54cdf0d2160b
rvion <vion.remi@gmail.com>
2023-03-25 19:21:59 +0100

    new stringifyReadable for workspace-saved files

---
+++ src/utils/stringifyReadable.ts
@@ -0,0 +1,29 @@
+export function readableStringify(obj: { [key: string]: any }, maxLevel = 3, level = 0) {
+    if (level > maxLevel) return JSON.stringify(obj)
+    const indent = '  '.repeat(level)
+    let result = '{\n'
+
+    const keys = Object.keys(obj)
+    for (let i = 0; i < keys.length; i++) {
+        const key = keys[i]
+        const value = obj[key]
+        const valueType = typeof value
+
+        if (valueType === 'object' && value !== null) {
+            result += `${indent}  "${key}": ${readableStringify(value, maxLevel, level + 1)}`
+        } else {
+            const formattedValue = valueType === 'string' ? `"${value}"` : value
+            result += `${indent}  "${key}": ${formattedValue}`
+        }
+
+        if (i < keys.length - 1) {
+            result += ','
+        }
+
+        result += '\n'
+    }
+
+    result += `${'  '.repeat(Math.max(0, level - 1))}}`
+
+    return result
+}
