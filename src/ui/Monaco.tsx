3fbc39fca74c31b17fabe6d10bb009dcbba27737
rvion <vion.remi@gmail.com>
2023-03-29 23:32:59 +0200

    y decl

--- src/ui/Monaco.tsx
+++ src/ui/Monaco.tsx
@@ -11,7 +11,9 @@ import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker'
 // import cssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker'
 // import htmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker'
 
+// @ts-ignore
 import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker'
+
 import { makeAutoObservable } from 'mobx'
 
 export class LocoMonaco {
