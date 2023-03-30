78b3bc38b90d326be80cc35bee2e134982b8c010
rvion <vion.remi@gmail.com>
2023-03-30 19:24:26 +0200

    rename demo scripts

--- workspace/demo-1-basic/script.ts
+++ workspace/demo-1-basic/script.ts
@@ -2,9 +2,9 @@ WORKFLOW(async ($) => {
     // generate an empty table
     const ckpt = $.CheckpointLoaderSimple({ ckpt_name: 'AOM3A1_orangemixs.safetensors' })
     const latent = $.EmptyLatentImage({ width: 512, height: 512, batch_size: 1 })
-    const positive = $.CLIPTextEncode({ text: 'masterpiece, chair', clip: ckpt })
+    const positive = $.CLIPTextEncode({ text: 'masterpiece, (chair:1.3)', clip: ckpt })
     const negative = $.CLIPTextEncode({ text: '', clip: ckpt })
-    const sampler = $.KSampler({ seed: 2123, steps: 20, cfg: 10, sampler_name: 'euler', scheduler: 'normal', denoise: 0.8, model: ckpt, positive, negative, latent_image: latent })
+    const sampler = $.KSampler({ seed: $.randomSeed(), steps: 20, cfg: 10, sampler_name: 'euler', scheduler: 'normal', denoise: 0.8, model: ckpt, positive, negative, latent_image: latent })
     const vae = $.VAEDecode({ samples: sampler, vae: ckpt })
     
     $.SaveImage({ filename_prefix: 'ComfyUI', images: vae })
