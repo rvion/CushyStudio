80e316b4037cf4df61518954fe7cfa21ef3b11f2
rvion <vion.remi@gmail.com>
2023-04-01 17:10:45 +0200

    attempt to fix linux build

--- src/ui/main.tsx
+++ src/ui/main.tsx
@@ -2,24 +2,31 @@ import ReactDOM from 'react-dom/client'
 
 // dock css
 import 'rc-dock/dist/rc-dock-dark.css'
+
 // 🔴 todo remove this now that we have fluentui 9
 import 'react-toastify/dist/ReactToastify.css'
+
+// mixed back of overrides
 import './index.css'
 
 // single import allowed before loading demos
 import { Workflow } from '../core/Workflow'
 
-// 1. monkey patch WORKFLOW so we can properly import demos without crahsing
-// due to missing virtual WORKFLOW function
-// @ts-ignore
-window.WORKFLOW = (...args: ConstructorParameters<typeof Workflow>) => {
-    return new Workflow(...args)
+const start = async () => {
+    // 1. monkey patch WORKFLOW so we can properly import demos without crahsing
+    // due to missing virtual WORKFLOW function
+    // @ts-ignore
+    window.WORKFLOW = (...args: ConstructorParameters<typeof Workflow>) => {
+        return new Workflow(...args)
+    }
+
+    const { AppUI } = await import('../layout/AppUI')
+
+    // APP ENTRYPOINT
+    ReactDOM.createRoot(
+        //
+        document.getElementById('root') as HTMLElement,
+    ).render(<AppUI />)
 }
 
-const { AppUI } = await import('../layout/AppUI')
-
-// APP ENTRYPOINT
-ReactDOM.createRoot(
-    //
-    document.getElementById('root') as HTMLElement,
-).render(<AppUI />)
+void start()
