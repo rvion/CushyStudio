acfc13654ba98c3f80ae49acccbeb1f5c9c583a3
rvion <vion.remi@gmail.com>
2023-04-03 23:20:02 +0200

    cleanup

--- src/ui/LiveControlUI.tsx
+++
@@ -1,9 +0,0 @@
-import { observer } from 'mobx-react-lite'
-
-export const LiveControlUI = observer(function LiveControlUI_() {
-    return (
-        <div>
-            <h1>Hello</h1>
-        </div>
-    )
-})
