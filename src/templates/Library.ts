9361baa7cc625ec39a6fe6da48016518fc251f0a
rvion <vion.remi@gmail.com>
2023-04-04 00:08:22 +0200

    add basic cnet demo

--- src/templates/Library.ts
+++ src/templates/Library.ts
@@ -32,7 +32,7 @@ export const demoLibrary: Template[] = [
     const image = n.SaveImage({ filename_prefix: 'ComfyUI', images: vae })
     let r1 = await n.get()
 
-    const nextBase = n.ImageLoad({ image_path: \`./output/\${r1.images[0].data.filename}\` })
+    const nextBase = n.WASImageLoad({ image_path: \`./output/\${r1.images[0].data.filename}\` })
     const _vaeEncode = n.VAEEncode({ pixels: nextBase, vae: ckpt.VAE })
     sampler.set({ latent_image: _vaeEncode })
 
@@ -44,5 +44,44 @@ export const demoLibrary: Template[] = [
 })
 
 `,
+    ),
+
+    new Template(
+        'demo3-cnet-pose',
+        `export default WORKFLOW(async (C) => {
+    const ckpt = C.CheckpointLoaderSimple({ ckpt_name: 'AOM3A1_orangemixs.safetensors' })
+    const vae = C.VAELoader({ vae_name: 'orangemix.vae.pt' })
+
+    // this is not needed if you work with a local ComfyUI: just use WASImageLoad instead
+    const refPoseImgUpload = await C.uploadImgFromDisk('/Users/loco/Desktop/pose_present.png') // <- change this path 🔴
+    const refPoseImg = C.LoadImage({ image: refPoseImgUpload.name as any })
+
+    const sample = C.KSampler({
+        seed: C.randomSeed(),
+        steps: 10,
+        cfg: 8,
+        sampler_name: 'dpmpp_sde',
+        scheduler: 'normal',
+        denoise: 1,
+        model: ckpt,
+        positive: C.ControlNetApply({
+            strength: 1,
+            conditioning: C.CLIPTextEncode({ text: 'masterpiece, 1girl, solo girl', clip: ckpt }),
+            control_net: C.ControlNetLoader({ control_net_name: 'control_openpose-fp16.safetensors' }),
+            image: refPoseImg,
+        }),
+        negative: C.CLIPTextEncode({ text: 'ugly, naked', clip: ckpt }),
+        latent_image: C.EmptyLatentImage({ width: 1280, height: 704, batch_size: 1 }),
+    })
+
+    // save the image
+    C.SaveImage({
+        filename_prefix: 'ComfyUI',
+        images: C.VAEDecode({ samples: sample.LATENT, vae: vae.VAE }),
+    })
+
+    // start the prompt
+    await C.get()
+})`,
     ),
 ]
