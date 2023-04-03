c292c49d528f08dd48e896761a51a3d06b5eac90
rvion <vion.remi@gmail.com>
2023-04-03 23:39:06 +0200

    misc: add random negative in wildcards for later

---
+++ src/wildcards/negatives.ts
@@ -0,0 +1,1 @@
+export const negative1 = `(deformed, distorted, disfigured:1.3), poorly drawn, bad anatomy, wrong anatomy, extra limb, missing limb, floating limbs, (mutated hands and fingers:1.4), disconnected limbs, mutation, mutated, ugly, disgusting, blurry, amputation`
