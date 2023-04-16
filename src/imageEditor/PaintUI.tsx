bd2b3b484fc2ae0b7a0bc805817082672eed8509
rvion <vion.remi@gmail.com>
2023-04-16 18:39:20 +0200

    more paintero

--- src/imageEditor/PaintUI.tsx
+++ src/imageEditor/PaintUI.tsx
@@ -1,45 +1,57 @@
-import { observer } from 'mobx-react-lite'
+import type { MessageFromExtensionToWebview_askPaint } from '../core-types/MessageFromExtensionToWebview'
+
+import { observer, useLocalObservable } from 'mobx-react-lite'
 import { nanoid } from 'nanoid'
-// @ts-ignore
-
-// import * as Painterro from 'painterro'
-
 import { useMemo } from 'react'
-import { MessageFromExtensionToWebview_askPaint } from '../core-types/MessageFromExtensionToWebview'
-console.log('oks')
+
 // https://github.com/devforth/painterro
 export const PaintUI = observer(function PaintUI_(p: { step: MessageFromExtensionToWebview_askPaint }) {
+    const uiSt = useLocalObservable(() => ({
+        img: '',
+        locked: false,
+    }))
+
     const uid = useMemo(() => nanoid(), [])
-    return (
-        <div
-            style={{ position: 'relative', minWidth: '300px', minHeight: '300px', width: '100%', height: '100%' }}
-            ref={(a) => {
-                console.log('🔥 mounting paint', a)
-                if (a == null) return null
-                setTimeout(() => {
+    return uiSt.locked ? (
+        <img src={uiSt.img} alt='' />
+    ) : (
+        <>
+            <button>OK</button>
+            <div
+                style={{ position: 'relative', minWidth: '300px', minHeight: '300px', width: '100%', height: '100%' }}
+                ref={(a) => {
                     console.log('🔥 mounting paint', a)
-                    // setTimeout(())
-                    // @ts-ignore
-                    Painterro({
-                        id: uid,
-                        // saveHandler: function (image, done) {
-                        //     var formData = new FormData()
-                        //     formData.append('image', image.asBlob())
-                        //     // you can also pass suggested filename
-                        //     // formData.append('image', image.asBlob(), image.suggestedFileName());
-                        //     var xhr = new XMLHttpRequest()
-                        //     xhr.open('POST', 'http://127.0.0.1:5000/save-as-binary/', true)
-                        //     xhr.onload = xhr.onerror = function () {
-                        //         // after saving is done, call done callback
-                        //         done(true) //done(true) will hide painterro, done(false) will leave opened
-                        //     }
-                        //     xhr.send(formData)
-                        // },
-                    }).show(p.step.uri)
-                }, 1000)
-            }}
-        >
-            <div dangerouslySetInnerHTML={{ __html: `<div id="${uid}">🟢</div>` }}></div>
-        </div>
+                    if (a == null) return null
+                    setTimeout(() => {
+                        console.log('🔥 mounting paint', a)
+                        // setTimeout(())
+                        // @ts-ignore
+                        Painterro({
+                            id: uid,
+                            saveHandler: function (image: PainteroImage, done: (a: boolean) => void) {
+                                // const formData = new FormData()
+                                const txt = image.asDataURL('image/png')
+                                console.log(txt)
+                                uiSt.img = txt
+                                uiSt.locked = true
+                                done(true)
+                            },
+                        }).show(p.step.uri)
+                    }, 1000)
+                }}
+            >
+                <div dangerouslySetInnerHTML={{ __html: `<div id="${uid}">🟢</div>` }}></div>
+            </div>
+        </>
     )
 })
+
+type PainteroImage = {
+    asBlob(type: any, quality: any): Blob
+    asDataURL(type?: any, quality?: any): string // returns e.g. "data:image/jpeg;base64,/9j/4AAQS...."
+    suggestedFileName(type: any): string // returns string
+    hasAlphaChannel(): boolean // returns true or false
+    getOriginalMimeType(): 'image/jpeg' | 'image/png' // e.g. image/jpeg;
+    getWidth(): number // integer
+    getHeight(): number // integer
+}
