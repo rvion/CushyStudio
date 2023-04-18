8f4b606869b9995c122a34addd5eecc5303c08b8
rvion <vion.remi@gmail.com>
2023-04-18 20:59:55 +0200

    remove logger history

--- src/logger/LoggerBack.ts
+++ src/logger/LoggerBack.ts
@@ -5,8 +5,6 @@ import { makeAutoObservable } from 'mobx'
 import * as vscode from 'vscode'
 
 export class Logger {
-    history: LogMessage[] = []
-
     /**
      *  - available in the extension process
      *  - not available in the webview process
@@ -17,35 +15,26 @@ export class Logger {
         makeAutoObservable(this)
     }
 
-    private addToLogHistory(level: LogLevel, category: LogCategory, message: string): void {
-        if (this.history.length >= 100) this.history.shift()
-        this.history.push({ message, category, level, timestamp: new Date() })
-    }
-
     public debug(category: LogCategory, message: string): void {
         if (this.level > LogLevel.DEBUG) return
-        this.addToLogHistory(LogLevel.DEBUG, category, message)
         this.chanel?.appendLine(`[DEBUG] ${message}`)
         console.debug(`[DEBUG] ${message}`)
     }
 
     public info(category: LogCategory, message: string): void {
         if (this.level > LogLevel.INFO) return
-        this.addToLogHistory(LogLevel.INFO, category, message)
         this.chanel?.appendLine(`${category} ℹ️ ${message}`)
         console.info(`[INFO] ${message}`)
     }
 
     public warn(category: LogCategory, message: string): void {
         if (this.level > LogLevel.WARN) return
-        this.addToLogHistory(LogLevel.WARN, category, message)
         this.chanel?.appendLine(`${category} 🔶 ${message}`)
         console.warn(`[WARNING] ${message}`)
     }
 
     public error(category: LogCategory, message: string): void {
         if (this.level > LogLevel.ERROR) return
-        this.addToLogHistory(LogLevel.ERROR, category, message)
         this.chanel?.appendLine(`${category} ❌ ${message}`)
         console.error(`[ERROR] ${message}`)
     }
