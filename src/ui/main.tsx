297f3353e06cefa2f24108ad1d3f5e40155f99cf
rvion <vion.remi@gmail.com>
2023-04-22 19:15:15 +0200

    add back rsuite

--- src/ui/main.tsx
+++ src/ui/main.tsx
@@ -1,8 +1,14 @@
 import '../logger/LoggerFront' // inject a global logger
 import ReactDOM from 'react-dom/client'
 
+import 'rsuite/dist/rsuite.min.css'
 import './webview.css'
 import { WebviewUI } from './WebviewUI'
+import { CustomProvider } from 'rsuite'
 
 const root = document.getElementById('root') as HTMLElement
-ReactDOM.createRoot(root).render(<WebviewUI />)
+ReactDOM.createRoot(root).render(
+    <CustomProvider theme='dark'>
+        <WebviewUI />
+    </CustomProvider>,
+)
