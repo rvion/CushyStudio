c867a93c3191d9a99d2b7e1663f02f78502c2ec2
rvion <vion.remi@gmail.com>
2023-04-29 21:24:22 +0200

    improve later

--- src/core-back/LATER.ts
+++ src/core-back/LATER.ts
@@ -1,12 +1,14 @@
-import type { ComfySetup, Enum_CheckpointLoader_ckpt_name, LoadImage } from '../global'
+import type * as T from '../global'
 import type { FlowRun } from './FlowRun'
 
 // prettier-ignore
 export type LATER<T>
     // should be generated
-    = T extends 'ComfySetup' ? ComfySetup
-    : T extends 'LoadImage' ? LoadImage
-    : T extends 'Enum_CheckpointLoader_ckpt_name' ? Enum_CheckpointLoader_ckpt_name
+    = T extends 'ComfySetup' ? T.ComfySetup
+    : T extends 'LoadImage' ? T.LoadImage
+    : T extends 'Enum_CheckpointLoader_ckpt_name' ? T.Enum_CheckpointLoader_ckpt_name
+    : T extends 'HasSingle_CLIP' ?  T.HasSingle_CLIP
+    : T extends 'HasSingle_MODEL' ?  T.HasSingle_MODEL
 
     // those need to be mocked
     : T extends 'FlowRun' ? FlowRun
