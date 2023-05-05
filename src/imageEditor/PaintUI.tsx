5d975e1aa08a56fa45bcd7a83511caa79f02d020
rvion <vion.remi@gmail.com>
2023-05-05 18:38:16 +0200

    replace paintero by minipaint

--- src/imageEditor/PaintUI.tsx
+++ src/imageEditor/PaintUI.tsx
@@ -2,56 +2,58 @@ import type { MessageFromExtensionToWebview_askPaint } from '../core-types/Messa
 
 import { observer, useLocalObservable } from 'mobx-react-lite'
 import { nanoid } from 'nanoid'
-import { useMemo } from 'react'
+import { useLayoutEffect, useMemo, useRef } from 'react'
+import { useSt } from '../core-front/stContext'
+import { Panel } from 'rsuite'
 
 // https://github.com/devforth/painterro
 export const PaintUI = observer(function PaintUI_(p: { step: MessageFromExtensionToWebview_askPaint }) {
-    const uiSt = useLocalObservable(() => ({
-        img: '',
-        locked: false,
-    }))
+    const st = useSt()
+    const uiSt = useLocalObservable(() => ({ img: '', locked: false }))
 
-    const uid = useMemo(() => nanoid(), [])
+    useLayoutEffect(() => {
+        setTimeout(() => {
+            // window.getElemnt
+            const image = document.createElement('img')
+            image.crossOrigin = 'Anonymous'
+            image.src = p.step.uri
+            image.onload = function () {
+                const iframe = document.getElementById('miniPaint') as any
+                var Layers = iframe.contentWindow.Layers
+                var new_layer = {
+                    name: nanoid(),
+                    type: 'image',
+                    data: image,
+                    width: image.naturalWidth || image.width,
+                    height: image.naturalHeight || image.height,
+                    width_original: image.naturalWidth || image.width,
+                    height_original: image.naturalHeight || image.height,
+                }
+                Layers.insert(new_layer)
+            }
+        }, 1000)
+    }, [])
+
     return uiSt.locked ? (
         <img src={uiSt.img} alt='' />
     ) : (
-        <>
-            <button>OK</button>
-            <div
-                style={{ position: 'relative', minWidth: '300px', minHeight: '300px', width: '100%', height: '100%' }}
-                ref={(a) => {
-                    console.log('🔥 mounting paint', a)
-                    if (a == null) return null
-                    setTimeout(() => {
-                        console.log('🔥 mounting paint', a)
-                        // setTimeout(())
-                        // @ts-ignore
-                        Painterro({
-                            id: uid,
-                            saveHandler: function (image: PainteroImage, done: (a: boolean) => void) {
-                                // const formData = new FormData()
-                                const txt = image.asDataURL('image/png')
-                                console.log(txt)
-                                uiSt.img = txt
-                                uiSt.locked = true
-                                done(true)
-                            },
-                        }).show(p.step.uri)
-                    }, 1000)
+        <Panel>
+            <iframe
+                style={{
+                    resize: 'both',
+                    minWidth: '200px',
+                    minHeight: '200px',
+                    width: '100%',
+                    height: '1000px',
                 }}
-            >
+                id='miniPaint'
+                src={'http://localhost:5173/minipaint/index.html'}
+                allow='camera'
+            ></iframe>
+
+            {/* <div id={uid} ref={ref} style={{ position: 'relative', minWidth: '100%', minHeight: '800px', maxHeight: '800px' }}>
                 <div dangerouslySetInnerHTML={{ __html: `<div id="${uid}">🟢</div>` }}></div>
-            </div>
-        </>
+            </div> */}
+        </Panel>
     )
 })
-
-type PainteroImage = {
-    asBlob(type: any, quality: any): Blob
-    asDataURL(type?: any, quality?: any): string // returns e.g. "data:image/jpeg;base64,/9j/4AAQS...."
-    suggestedFileName(type: any): string // returns string
-    hasAlphaChannel(): boolean // returns true or false
-    getOriginalMimeType(): 'image/jpeg' | 'image/png' // e.g. image/jpeg;
-    getWidth(): number // integer
-    getHeight(): number // integer
-}
