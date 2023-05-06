327b3c102dc20f47b75c993e57fec51a3be9cd8a
rvion <vion.remi@gmail.com>
2023-05-06 13:38:28 +0200

    improve previewUI a lot

--- src/ui/PreviewListUI.tsx
+++ src/ui/PreviewListUI.tsx
@@ -1,24 +1,53 @@
-import { observer } from 'mobx-react-lite'
-import { Popover, Tooltip, Whisper } from 'rsuite'
+import { observer, useLocalObservable } from 'mobx-react-lite'
+import { Maybe } from 'src/utils/types'
 import { useSt } from '../core-front/stContext'
 
 export const PreviewListUI = observer(function PreviewListUI_(p: {}) {
     const st = useSt()
+    const uiSt = useLocalObservable(() => ({ hovered: null as Maybe<string> }))
     return (
-        <div style={{ display: 'flex', overflowX: 'auto', width: '100%', gap: '.1rem' }}>
+        <div style={{ display: 'flex', overflowX: 'auto', width: '100%' }}>
             {st.imageURLs.map((i, ix) => (
-                <Whisper
-                    key={ix}
-                    placement='autoVerticalStart'
-                    speaker={
-                        <Popover>
-                            <img style={{ objectFit: 'contain', maxHeight: 'unset', maxWidth: 'unset' }} key={i} src={i} />
-                        </Popover>
-                    }
-                >
-                    <img style={{ objectFit: 'contain', width: '32px', height: '32px' }} key={i} src={i} />
-                </Whisper>
+                <img
+                    onMouseEnter={() => (uiSt.hovered = i)}
+                    onMouseLeave={() => {
+                        if (uiSt.hovered === i) uiSt.hovered = null
+                    }}
+                    style={{ objectFit: 'contain', width: '32px', height: '32px' }}
+                    key={i}
+                    src={i}
+                />
             ))}
+            {uiSt.hovered && (
+                <>
+                    <div
+                        style={{
+                            pointerEvents: 'none',
+                            zIndex: 999998,
+                            position: 'absolute',
+                            inset: 0,
+                            bottom: 0,
+                            top: '5rem',
+                            left: 0,
+                            right: 0,
+                            background: '#272727aa',
+                        }}
+                    ></div>
+                    <img
+                        src={uiSt.hovered}
+                        style={{
+                            //
+                            top: '5rem',
+                            left: 0,
+                            position: 'absolute',
+                            zIndex: 999999,
+                            objectFit: 'contain',
+                            maxHeight: '100vh',
+                            maxWidth: '100vw',
+                        }}
+                    />
+                </>
+            )}
         </div>
     )
 })
