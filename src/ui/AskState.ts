2c3a0cefb215932fc055d51d78e43dbe8e779460
rvion <vion.remi@gmail.com>
2023-05-07 23:28:05 +0200

    add debug to monitor askState value changing

--- src/ui/AskState.ts
+++ src/ui/AskState.ts
@@ -2,11 +2,15 @@ import { makeAutoObservable } from 'mobx'
 
 export type AskPath = (string | number)[]
 
+/** this is the Form state that centralize values for every
+ * ask request */
 export class AskState {
-    /** this value is the root response object the form will progressively fill */
+    /** this value is the root response object
+     * the form will progressively fill */
     value: any = {}
 
-    /** this should be set to true once the component can no longer be interracted with */
+    /** this should be set to true once the component
+     * can no longer be interracted with */
     locked: boolean = false
 
     constructor() {
@@ -25,6 +29,7 @@ export class AskState {
     }
 
     setAtPath = (path: AskPath, value: any) => {
+        console.log(path, value, this.value)
         let current = this.value
         for (let i = 0; i < path.length - 1; i++) {
             const key = path[i]
@@ -32,5 +37,6 @@ export class AskState {
             current = current[key]
         }
         current[path[path.length - 1]] = value
+        console.log(this.value)
     }
 }
