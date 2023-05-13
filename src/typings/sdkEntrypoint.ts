109f58468c6bb6c1267cb37aeb5b2574fb3b9e9c
rvion <vion.remi@gmail.com>
2023-05-13 22:31:04 +0200

    remove legacy workflow type

--- src/typings/sdkEntrypoint.ts
+++ src/typings/sdkEntrypoint.ts
@@ -1,4 +1,3 @@
 export type { FlowRun } from 'src/back/FlowRun'
-export type { Workflow } from '../core/Workflow'
 export type { Graph } from '../core/Graph'
 export type { ActionType } from '../core/Requirement'
