35ea531eb2ac88a6117e284ad1b3443723ebaa61
rvion <vion.remi@gmail.com>
2023-09-26 06:29:31 +0200

    cleanup legacy extension files

--- src/extension/extractWorkflows.ts
+++
@@ -1,7 +0,0 @@
-import * as vscode from 'vscode'
-import { bang } from '../utils/bang'
-import { logger } from '../logger/logger'
-
-// const headingRe = /^(#+)\s*(.+)$/
-// const WorkflowRe = /^WORKFLOW\(/
-// const testRe = /^([0-9]+)\s*([+*/-])\s*([0-9]+)\s*=\s*([0-9]+)/
