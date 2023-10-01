2a31b882df05d132dd720b348ee1f56487b76f51
rvion <vion.remi@gmail.com>
2023-10-01 11:29:31 +0200

    fix overlay menu

--- src/prompter/WidgetPromptUI.tsx
+++ src/prompter/WidgetPromptUI.tsx
@@ -14,6 +14,7 @@ import { HistoryPlugin } from '@lexical/react/LexicalHistoryPlugin'
 
 // theme
 import theme from './theme/WidgetLexicalTheme'
+import './theme/Popover.css'
 
 // nodes
 import { $createEmbeddingNode, EmbeddingNode } from './nodes/EmbeddingNode'
