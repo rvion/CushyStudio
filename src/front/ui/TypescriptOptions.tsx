b7e3abe92b6b24e8a1cd9f19e072aabb9f6c6a83
rvion <vion.remi@gmail.com>
2023-10-11 20:49:24 +0200

    cleanup

--- src/front/ui/TypescriptOptions.tsx
+++
@@ -1,6 +0,0 @@
-// import type * as T from 'monaco-editor/esm/vs/editor/editor.api'
-
-// export type TypescriptOptions = T.languages.typescript.CompilerOptions
-// export type ITextModel = ReturnType<typeof T.editor.createModel>
-// export type IStandaloneCodeEditor = import('monaco-editor').editor.IStandaloneCodeEditor
-// export type Monaco = typeof T
