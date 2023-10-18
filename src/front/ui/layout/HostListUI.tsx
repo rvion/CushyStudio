51b666c25df770bb79826f616f924683b5c0bf33
rvion <vion.remi@gmail.com>
2023-10-18 21:27:50 +0200

    dynamic host switcher

--- src/front/ui/layout/HostListUI.tsx
+++ src/front/ui/layout/HostListUI.tsx
@@ -1,23 +1,41 @@
 import { observer } from 'mobx-react-lite'
-import { Panel } from 'rsuite'
+import { Button, Panel } from 'rsuite'
 import { useSt } from 'src/front/FrontStateCtx'
 
 export const HostListUI = observer(function HostListUI_(p: {}) {
     const st = useSt()
+    const mainComfyHost = st.configFile.value.comfyHost
+    const mainComfyPort = st.configFile.value.comfyPort
     const machines = st.configFile.value.machines
     return (
-        <div>
-            {machines?.map((m, ix) => (
-                <Panel bordered tw='m-2 bg-gray-900' key={ix}>
-                    <div className='flex gap-2'>
-                        <span className='material-symbols-outlined'>computer</span>
-                        <div>
-                            <div>{m.comfyHost}</div>
-                            <div>{m.comfyPort}</div>
+        <div tw='flex flex-col gap-2 p-2'>
+            {machines?.map((m, ix) => {
+                const isMain = m.comfyHost === mainComfyHost && m.comfyPort === mainComfyPort
+                return (
+                    <Button
+                        //
+                        appearance={isMain ? 'primary' : 'ghost'}
+                        color={isMain ? 'green' : 'blue'}
+                        startIcon={<span className='material-symbols-outlined'>computer</span>}
+                        onClick={() => {
+                            st.configFile.update({
+                                comfyHost: m.comfyHost,
+                                comfyPort: m.comfyPort,
+                            })
+                        }}
+                        key={ix}
+                    >
+                        <div className='flex gap-2'>
+                            <div>
+                                <div>
+                                    {m.comfyHost}:{m.comfyPort}
+                                </div>
+                            </div>
+                            {isMain ? <span className='material-symbols-outlined'>star</span> : null}
                         </div>
-                    </div>
-                </Panel>
-            ))}
+                    </Button>
+                )
+            })}
         </div>
     )
 })
