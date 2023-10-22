7a0e1a3bcc575c731c38deee3b41e10fbd8b6701
rvion <vion.remi@gmail.com>
2023-10-22 20:26:43 +0200

    hotfix

--- src/back/ActionPath.ts
+++ src/back/ActionPath.ts
@@ -4,7 +4,8 @@ export type ActionPath = Branded<string, 'ActionPath'>
 
 export const asActionPath = (path: string): ActionPath => {
     if (pathe.isAbsolute(path)) throw new Error(`action path (${path}) must be relative`)
-    if (!path.startsWith('actions/')) throw new Error(`action path (${path}) must start with 'actions/'`)
+    if (!(path.startsWith('actions/') || path.startsWith('actions\\')))
+        throw new Error(`action path (${path}) must start with 'actions/'`)
     if (!path.endsWith('.ts')) throw new Error(`action path (${path}) must end with '.ts'`)
 
     return path as ActionPath
