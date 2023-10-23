dc28e20cf49cc4b63746c478a207c15422905366
rvion <vion.remi@gmail.com>
2023-10-23 22:28:26 +0200

    add demo

---
+++ actions/CushyStudio/tutorial/demo-featherice.ts
@@ -0,0 +1,50 @@
+action({
+    name: 'background-removal-playground',
+    ui: (form) => ({
+        steps: form.choices({
+            defaultActiveBranches: {
+                frame: true,
+            },
+            items: () => ({
+                frame: form.group({
+                    className: ' p-2 bg-blue-800 shadow rounded-xl animate-pulse',
+                    items: () => ({
+                        seed: form.seed({
+                            default: 12,
+                            defaultMode: 'fixed',
+                        }),
+                        positive: form.str({}),
+                    }),
+                }),
+                portrait: form.group({
+                    items: () => ({
+                        seed: form.seed({}),
+                    }),
+                }),
+            }),
+        }),
+    }),
+
+    run: async (flow, form) => {
+        const graph = flow.nodes
+        const ckpt = graph.CheckpointLoaderSimple({ ckpt_name: 'revAnimated_v122.safetensors' })
+        const positive = graph.CLIPTextEncode({ clip: ckpt, text: form.steps.frame?.positive || 'a house' })
+        const negative = graph.CLIPTextEncode({ clip: ckpt, text: 'bad' })
+        const latent_image = graph.EmptyLatentImage({ width: 512, height: 512, batch_size: 1 })
+        const images = graph.VAEDecode({
+            vae: ckpt,
+            samples: graph.KSampler({
+                model: ckpt,
+                sampler_name: 'ddim',
+                scheduler: 'ddim_uniform',
+                positive,
+                negative,
+                latent_image,
+            }),
+        })
+        graph.PreviewImage({ images: images })
+
+        // execute
+        await flow.PROMPT()
+    },
+})
