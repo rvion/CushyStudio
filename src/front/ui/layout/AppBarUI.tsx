5b408e2907469d1757614896b801dfdafd6df91a
rvion <vion.remi@gmail.com>
2023-10-24 19:42:47 +0200

    rework appbar

--- src/front/ui/layout/AppBarUI.tsx
+++ src/front/ui/layout/AppBarUI.tsx
@@ -1,6 +1,6 @@
 import * as I from '@rsuite/icons'
 import { observer } from 'mobx-react-lite'
-import { Button, ButtonGroup, IconButton, InputGroup, SelectPicker } from 'rsuite'
+import { Button, IconButton } from 'rsuite'
 import { useSt } from '../../FrontStateCtx'
 import { SchemaIndicatorUI } from './SchemaIndicatorUI'
 import { UpdateBtnUI } from './UpdateBtnUI'
@@ -16,7 +16,7 @@ export const AppBarUI = observer(function AppBarUI_(p: {}) {
             className='flex gap-1 items-center'
             style={{ borderBottom: '1px solid #383838' }}
         >
-            <div tw='whitespace-nowrap'>🛋️ CushyStudio</div>
+            <CushyStudioLinkUI />
             <Button
                 appearance='subtle'
                 loading={Boolean(st.db.saveTimeout)}
@@ -26,11 +26,7 @@ export const AppBarUI = observer(function AppBarUI_(p: {}) {
             >
                 save
             </Button>
-            <IconButton
-                size='xs'
-                icon={<span className='material-symbols-outlined'>{themeIcon}</span>}
-                onClick={() => st.theme.toggle()}
-            />
+
             <Button
                 //
                 size='sm'
@@ -40,69 +36,41 @@ export const AppBarUI = observer(function AppBarUI_(p: {}) {
             >
                 Reload
             </Button>
-
-            {/* <IconButton
-                size='sm'
-                appearance='subtle'
-                icon={st.cushyStatus?.connected ? <I.CheckRound color='green' /> : <I.ExpiredRound color='red' />}
-            /> */}
-
             <UpdateBtnUI updater={st.updater} />
             <WebsocketIndicatorUI />
             <SchemaIndicatorUI />
+            <OpenComfyExternalUI />
 
-            {/* <Button startIcon={<I.AddOutline />} size='sm' className='self-start' onClick={() => st.startProject()}>
-                create project
-            </Button> */}
-            <div tw='flex items-center'>
-                {/* layout:
-                <div>
-                    <SelectPicker
-                        //
-
-                        value={st.layout.currentPerspectiveName}
-                        data={st.layout.allPerspectives}
-                    />
-                </div> */}
-                <Button
-                    size='xs'
-                    appearance='primary'
-                    color='red'
-                    startIcon={<I.Reload />}
-                    onClick={() => st.layout.resetCurrent()}
-                >
-                    Fix Layout
-                </Button>
-            </div>
             <div className='flex-grow'></div>
+            <Button size='xs' appearance='ghost' color='orange' startIcon={<I.Reload />} onClick={() => st.layout.resetCurrent()}>
+                Layout
+            </Button>
             <DBHealthUI />
-            {/* <Button
-                // startIcon={<I.AddOutline />}
-                size='sm'
-                appearance='subtle'
-                className='self-start'
-                endIcon={<span className='material-symbols-outlined'>open_in_new</span>}
-                onClick={() => {
-                    window.require('electron').shell.openExternal(st.getServerHostHTTP())
-                }}
-            >
-                ComfyUI Web
-            </Button> */}
-            {/* biegert/ComfyUI-CLIPSeg */}
-
-            <a
-                className='ml-auto flex'
-                onClick={(ev) => {
-                    ev.preventDefault()
-                    ev.stopPropagation()
-                    window.require('electron').shell.openExternal('https://github.com/rvion/CushyStudio')
-                }}
-                href='#'
-            >
-                <span className='material-symbols-outlined text-yellow-600'>star</span>
-                <span className='underline text-blue-300'>github.com/rvion/CushyStudio</span>
-            </a>
+            <IconButton
+                appearance='ghost'
+                size='xs'
+                icon={<span className='material-symbols-outlined'>{themeIcon}</span>}
+                onClick={() => st.theme.toggle()}
+            />
         </div>
+    )
+})
+
+export const CushyStudioLinkUI = observer(function CushyStudioLinkUI_(p: {}) {
+    return (
+        <a
+            className='ml-auto flex'
+            onClick={(ev) => {
+                ev.preventDefault()
+                ev.stopPropagation()
+                window.require('electron').shell.openExternal('https://github.com/rvion/CushyStudio')
+            }}
+            href='#'
+        >
+            <span className='material-symbols-outlined text-yellow-600'>star</span>
+            <img src='/GithubLogo2.png' alt='Github Logo' style={{ width: '1.4rem', height: '1.4rem' }} />
+            <span className='underline text-blue-300'>rvion/CushyStudio</span>
+        </a>
     )
 })
 
@@ -114,14 +82,33 @@ export const DBHealthUI = observer(function DBHealthUI_(p: {}) {
         <div>
             <Button
                 //
-                size='sm'
-                // appearance='subtle'
+                size='xs'
+                appearance='ghost'
                 color={color}
                 onClick={() => st.db.reset()}
-                startIcon={<span className='material-symbols-outlined'>power_settings_new</span>}
+                startIcon={<I.Reload />}
+                // startIcon={<span className='material-symbols-outlined'>power_settings_new</span>}
             >
-                Reset DB ({dbHealth.sizeTxt})
+                DB ({dbHealth.sizeTxt})
             </Button>
         </div>
+    )
+})
+
+export const OpenComfyExternalUI = observer(function OpenComfyExternalUI_(p: {}) {
+    const st = useSt()
+    return (
+        <IconButton
+            size='sm'
+            appearance='subtle'
+            className='self-start'
+            icon={<span className='material-symbols-outlined'>open_in_new</span>}
+            // endIcon={}
+            onClick={() => {
+                window.require('electron').shell.openExternal(st.getServerHostHTTP())
+            }}
+        >
+            {/* ComfyUI Web */}
+        </IconButton>
     )
 })
