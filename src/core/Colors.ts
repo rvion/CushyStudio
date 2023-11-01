029aaa2688064b5347de67ed591551e6d77adaf5
rvion <vion.remi@gmail.com>
2023-11-01 12:27:26 +0100

    better Color paletes for ComfyUI { categories / inputs / ouputs }

--- src/core/Colors.ts
+++ src/core/Colors.ts
@@ -1,17 +1,54 @@
-export const comfyColors: { [category: string]: string } = {
-    loaders: '#446011',
+// PALETTE -------------------------------------------------------------------
+export const palette = {
+    primitive: '#554c32',
+    loader: '#446011',
     latent: '#464689',
-    'latent/transform': '#464689',
-    conditioning: '#481413', // '#0d430d',
-    'conditioning/style_model': '#0d430d',
-    sampling: '#634a1b',
+    conditioning: '#481413',
+    sampl: '#634a1b',
     image: '#7e3a3a',
 }
+export const paletteKeys = Object.keys(palette) as (keyof typeof palette)[]
 
-export const getColor = (_category: string) => {
+// CATEGORIES ----------------------------------------------------------------
+export const comfyColors: { [category: string]: string } = {
+    loaders: palette.loader,
+    latent: palette.latent,
+    'latent/transform': palette.latent,
+    conditioning: palette.conditioning, // '#0d430d',
+    'conditioning/style_model': palette.conditioning,
+    sampling: palette.sampl,
+    image: palette.image,
+    primitive: palette.primitive,
+}
+
+export const getColorForCategory = (_category: string) => {
     const category = _category.toLowerCase()
     if (category in comfyColors) {
         return comfyColors[category]
     }
-    return '#000000'
+    return 'black'
+}
+
+// INPUT ----------------------------------------------------------------
+export const getColorForInputNameInComfy = (nameInComfy: string) => {
+    const name = nameInComfy.toLowerCase()
+    if (name.includes('width')) return palette.primitive
+    if (name.includes('height')) return palette.primitive
+    for (const k of paletteKeys) {
+        if (name.startsWith(k)) return palette[k]
+    }
+    return 'black'
+}
+
+// OUTPUTS ----------------------------------------------------------------
+export const getColorForOutputNameInCushy = (nameInComfy: string) => {
+    const name = nameInComfy.toLowerCase()
+    if (name in nameColors) {
+        return comfyColors[name]
+    }
+    return 'black'
+}
+
+export const nameColors = {
+    latent: 'blue',
 }
