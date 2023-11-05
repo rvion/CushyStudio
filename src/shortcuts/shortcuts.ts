0c9c5b4fba2550d04c54ea907b6b9a3516e40e10
rvion <vion.remi@gmail.com>
2023-11-05 22:18:40 +0100

    fix windows shortcuts to quickly open panes

--- src/shortcuts/shortcuts.ts
+++ src/shortcuts/shortcuts.ts
@@ -9,13 +9,13 @@ const always = (fn: (st: STATE) => any) => (st: STATE) => {
     return Trigger.Success
 }
 
-const simple = (combo: Combo, action: (fn: STATE) => void): Shortcut<STATE> => ({
-    combos: [combo],
+const simple = (combo: Combo | Combo[], action: (fn: STATE) => void): Shortcut<STATE> => ({
+    combos: Array.isArray(combo) ? combo : [combo],
     action: always(action),
 })
 
-const simpleValidInInput = (combo: Combo, action: (fn: STATE) => void): Shortcut<STATE> => ({
-    combos: [combo],
+const simpleValidInInput = (combo: Combo | Combo[], action: (fn: STATE) => void): Shortcut<STATE> => ({
+    combos: Array.isArray(combo) ? combo : [combo],
     action: always(action),
     validInInput: true,
 })
@@ -25,14 +25,14 @@ const simpleValidInInput = (combo: Combo, action: (fn: STATE) => void): Shortcut
 export const shortcutsDef: Shortcut<STATE>[] = [
     // simpleValidInInput('meta+shift+k', (st) => (st.showSuperAdmin = !st.showSuperAdmin)),
     // simpleValidInInput('meta+shift+z', (st) => (st.showSuperAdminBubbles = !st.showSuperAdminBubbles)),
-    simpleValidInInput('meta+1', (st) => st.openCardPicker()),
-    simpleValidInInput('meta+2', (st) => st.layout.addMarketplace()),
-    simpleValidInInput('meta+3', (st) => st.layout.addPaint()),
-    simpleValidInInput('meta+4', (st) => st.layout.addComfy()),
-    simpleValidInInput('meta+5', (st) => st.layout.addGallery()),
-    simpleValidInInput('meta+6', (st) => st.layout.addConfig()),
-    simpleValidInInput('meta+7', (st) => st.layout.addCivitai()),
-    simpleValidInInput('meta+8', (st) => st.layout.addHosts()),
+    simpleValidInInput(['meta+1', 'ctrl+1'], (st) => st.openCardPicker()),
+    // simpleValidInInput(['meta+2', 'ctrl+2'], (st) => st.layout.addMarketplace()),
+    simpleValidInInput(['meta+3', 'ctrl+3'], (st) => st.layout.addPaint()),
+    simpleValidInInput(['meta+4', 'ctrl+4'], (st) => st.layout.addComfy()),
+    simpleValidInInput(['meta+5', 'ctrl+5'], (st) => st.layout.addGallery()),
+    simpleValidInInput(['meta+6', 'ctrl+6'], (st) => st.layout.addConfig()),
+    simpleValidInInput(['meta+7', 'ctrl+7'], (st) => st.layout.addCivitai()),
+    simpleValidInInput(['meta+8', 'ctrl+8'], (st) => st.layout.addHosts()),
     // simple('meta+x s', (st) => st.auth.stopImpersonating()),
     // simple('meta+x q', (st) => st.auth.logOut()),
 
