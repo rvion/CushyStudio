d622ec881dd7c0f81d16ce4875b9d479c3afcfeb
rvion <vion.remi@gmail.com>
2023-11-10 18:28:22 +0100

    no more random closing when ctrl/cmd+w

--- src/app/layout/AppUI.tsx
+++ src/app/layout/AppUI.tsx
@@ -1,20 +1,33 @@
 import { observer } from 'mobx-react-lite'
+import { useEffect, useRef } from 'react'
 import { useSt } from 'src/state/stateContext'
 import { AppBarUI } from './AppBarUI'
 import { ProjectUI } from './ProjectUI'
 
 export const CushyUI = observer(function CushyUI_() {
     const st = useSt()
+    const appRef = useRef<HTMLDivElement>(null)
+    useEffect(() => {
+        const current = appRef.current
+        if (current == null) return
+        function handleKeyDown(event: KeyboardEvent) {
+            if (!current!.contains(document.activeElement)) current?.focus()
+            if (event.key === 'w' && (event.ctrlKey || event.metaKey)) {
+                event.preventDefault()
+                event.stopPropagation()
+            }
+        }
+        window.addEventListener('keydown', handleKeyDown)
+        if (document.activeElement === document.body) current.focus()
+        return () => window.removeEventListener('keydown', handleKeyDown)
+    }, [appRef.current])
+
     return (
         <div
             id='CushyStudio'
             tabIndex={-1}
+            ref={appRef}
             onKeyDown={st.shortcuts.processKeyDownEvent}
-            ref={(e) => {
-                if (e == null) return
-                if (!(e instanceof HTMLElement)) return
-                if (document.activeElement === document.body) e.focus()
-            }}
             // global style
             tw={['col grow h100', st.theme.theme]}
         >
@@ -26,6 +39,7 @@ export const CushyUI = observer(function CushyUI_() {
     )
 })
 
+// force a few extra tailwind classNames to be included
 const foo = (
     <div className='grid grid-cols-2 grid-cols-1 grid-cols-3 grid-cols-4 grid-cols-5 grid-cols-6 grid-cols-7 grid-cols-8' />
 )
