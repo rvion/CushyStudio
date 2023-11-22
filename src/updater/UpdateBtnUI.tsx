9ac07f1d4495d13db1998cc93dfc9777e69ba33a
rvion <vion.remi@gmail.com>
2023-11-22 18:16:08 +0100

    tweak UpdateBtnUI

--- src/updater/UpdateBtnUI.tsx
+++ src/updater/UpdateBtnUI.tsx
@@ -1,7 +1,7 @@
 import { observer } from 'mobx-react-lite'
 import { GitManagedFolder } from 'src/updater/updater'
 import { FolderGitStatus } from 'src/cards/FolderGitStatus'
-import { Button, Loader } from 'src/rsuite/shims'
+import { Button, Joined, Loader, Message } from 'src/rsuite/shims'
 import { ReleaseChannelUI } from '../app/layout/ReleaseChannelUI'
 import { RevealUI } from 'src/rsuite/RevealUI'
 import { GitInitBtnUI } from './GitInitBtnUI'
@@ -9,12 +9,27 @@ import { GitInstallUI } from './GitInstallUI'
 import { _getRelativeTimeString } from './_getRelativeTimeString'
 import { UninstallUI } from './GitUninstallUI'
 import { UpdaterErrorUI } from './UpdaterErrorUI'
+import { ReactNode } from 'react'
 
-export const UpdateBtnUI = observer(function UpdateBtnUI_(p: { updater: GitManagedFolder }) {
+export const UpdateBtnUI = observer(function UpdateBtnUI_(p: { updater: GitManagedFolder; children?: ReactNode }) {
     const updater = p.updater
+    let ANCHOR = (
+        <Button tw={[updater.hasUpdateAvailable ? 'btn-warning' : 'btn-ghost', 'btn-sm']}>
+            {p.children}
+            <UpdaterAnchorUI updater={updater} />
+        </Button>
+    )
+    if (updater.hasUpdateAvailable)
+        ANCHOR = (
+            <div className='indicator'>
+                <span className='indicator-item badge badge-secondary'></span>
+                {ANCHOR}
+            </div>
+        )
+
     return (
         <RevealUI>
-            <UpdaterAnchorUI updater={updater} />
+            {ANCHOR}
             <UpdaterDetailsUI updater={updater} />
         </RevealUI>
     )
@@ -22,66 +37,46 @@ export const UpdateBtnUI = observer(function UpdateBtnUI_(p: { updater: GitManag
 
 export const UpdaterAnchorUI = observer(function UpdaterAnchorUI_(p: { updater: GitManagedFolder }) {
     const updater = p.updater
-    const hasErrors = updater.hasErrors
     const status = updater.status
     if (status === FolderGitStatus.Unknown) return <Loader />
     if (status === FolderGitStatus.DoesNotExist) return <GitInstallUI udpater={updater} />
     if (status === FolderGitStatus.NotADirectory) return <div>❓ unexpected file</div>
     if (status === FolderGitStatus.FolderWithoutGit) return <GitInitBtnUI updater={updater} />
     if (status === FolderGitStatus.FolderWithGitButWithProblems) return <div>❓</div>
-    if (status === FolderGitStatus.FolderWithGit)
-        return (
-            <div tw={['flex gap-1 cursor-help']}>
-                <div tw='flex gap-1'>
-                    {
-                        hasErrors ? (
-                            <>
-                                <span className='text-error-content material-symbols-outlined'>error</span>
-                                version
-                            </>
-                        ) : updater.hasUpdateAvailable ? (
-                            <Button
-                                className='animate-pulse'
-                                color='red'
-                                size='sm'
-                                appearance='primary'
-                                icon={<span className='material-symbols-outlined'>update</span>}
-                                onClick={async (ev) => {
-                                    ev.stopPropagation()
-                                    ev.preventDefault()
-                                    await updater.updateToLastCommitAvailable()
-                                    window.location.reload()
-                                }}
-                            >
-                                update
-                                {/* to version {updater.nextVersion} */}
-                            </Button>
-                        ) : null
-                        // <span className='text-green-400 material-symbols-outlined'>check_circle</span>
-                    }
-
-                    <Button tw={[updater.hasUpdateAvailable ? 'btn-warning' : 'btn-ghost', 'btn-sm']}>
-                        {updater.currentVersion}
-                        {/* {updater.mainBranchName} */}
-                        {/* {updater.headCommitsCount ? `v${updater.currentVersion}` : <Loader />} */}
-                    </Button>
-                </div>
-            </div>
-        )
+    if (status === FolderGitStatus.FolderWithGit) return updater.currentVersion
     exhaust(status)
 })
 
 export const UpdaterDetailsUI = observer(function UpdaterDetailsUI_(p: { updater: GitManagedFolder }) {
     const updater = p.updater
+    const hasErrors = updater.hasErrors
+
     return (
         <div tw='menu flex flex-col gap-2'>
-            <div className='virtualBorder'>status: {p.updater.status}</div>
-            <div className='virtualBorder'>currentAction: {p.updater.currentAction}</div>
+            {hasErrors && <Message type='error'>error</Message>}
+            <div>
+                {updater.hasUpdateAvailable ? (
+                    <Button
+                        className='animate-pulse'
+                        color='red'
+                        size='sm'
+                        appearance='primary'
+                        icon={<span className='material-symbols-outlined'>update</span>}
+                        onClick={async (ev) => {
+                            ev.stopPropagation()
+                            ev.preventDefault()
+                            await updater.updateToLastCommitAvailable()
+                            window.location.reload()
+                        }}
+                    >
+                        update
+                        {/* to version {updater.nextVersion} */}
+                    </Button>
+                ) : null}
+            </div>
             {updater.config.betaBranch ? <ReleaseChannelUI onChange={(e) => console.log(e)} /> : null}
             <UpdaterErrorUI updater={updater} />
-            <div tw='virtualBorder flex items-center'>
-                <span className='material-symbols-outlined'>folder</span> <div>{updater.relPath || 'root'}</div>
-            </div>
+
             <div tw='virtualBorder'>
                 {updater.lastFetchAt ? (
                     <div>
@@ -121,6 +116,13 @@ export const UpdaterDetailsUI = observer(function UpdaterDetailsUI_(p: { updater
                     ))}
                 </tbody>
             </table>
+            <div tw='virtualBorder flex flex-wrap items-center'>
+                <span className='material-symbols-outlined'>folder</span> <div>{updater.relPath || 'root'}</div>
+            </div>
+            <Joined tw='flex gap-2'>
+                <div className='virtualBorder'>#{p.updater.status}</div>
+                <div className='virtualBorder'>action: {p.updater.currentAction ?? 'ø'}</div>
+            </Joined>
         </div>
     )
 })
