a9aa963623372e17eee27434ecbab75c808af646
rvion <vion.remi@gmail.com>
2023-11-23 19:45:56 +0100

    impreve start script on windows

--- start.cjs
+++ start.cjs
@@ -2,7 +2,7 @@ const { spawn } = require('child_process')
 const path = require('path')
 const os = require('os')
 
-// ensure tsconfig.custom.json exists
+// Ensure tsconfig.custom.json exists
 const fs = require('fs')
 const tsconfigPath = path.join(__dirname, 'tsconfig.custom.json')
 if (!fs.existsSync(tsconfigPath)) {
@@ -10,7 +10,7 @@ if (!fs.existsSync(tsconfigPath)) {
     fs.writeFileSync(tsconfigPath, JSON.stringify(defaultTsconfigJSON, null, 4))
 }
 
-// cross platform run command
+// Cross-platform run command
 const runCommand = (command, args) => {
     const cmd = os.platform() === 'win32' ? `${command}.cmd` : command
     const cmdPath = path.join('.', 'node_modules', '.bin', cmd)
@@ -23,20 +23,57 @@ const runCommand = (command, args) => {
 
     return spawnedProcess
 }
+
 console.log('Starting vite...')
 const vite = runCommand('vite', ['dev'])
 
 console.log('Starting electron...')
 const electron = runCommand('electron', ['-i', 'src/shell'])
 
-const cleanup = () => {
-    console.log('Killing processes...')
-    vite.kill()
+const cleanup = (source) => {
+    console.log(`Killing processes from ${source}...`)
+    vite.kill('SIGKILL')
     electron.kill()
+
+    // Forcefully terminate if not closed within a timeout
+    forceKill(vite, 1000) // Timeout in milliseconds
+    forceKill(electron, 1000)
 }
 
-process.on('SIGINT', cleanup)
-process.on('SIGTERM', cleanup)
+// Customized forceful kill for stubborn processes
+const forceKill = (process, timeout = 5000) => {
+    const checkInterval = 100
+    let elapsed = 0
 
-vite.on('close', cleanup)
-electron.on('close', cleanup)
+    const interval = setInterval(() => {
+        if (process.killed) {
+            clearInterval(interval)
+        } else if (elapsed > timeout) {
+            console.warn(`Forcefully terminating process after ${timeout}ms`)
+            process.kill('SIGKILL')
+            clearInterval(interval)
+        } else {
+            elapsed += checkInterval
+        }
+    }, checkInterval)
+}
+
+// ... [Your existing code for process handling]
+
+// Additional Windows-specific signal handling
+if (os.platform() === 'win32') {
+    const rl = require('readline').createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    })
+
+    rl.on('SIGINT', function () {
+        process.emit('SIGINT')
+    })
+}
+
+process.on('SIGINT', () => cleanup('SIGINT'))
+process.on('SIGTERM', () => cleanup('SIGTERM'))
+
+electron.on('close', () => cleanup('Electron'))
+// vite.on('close', () => cleanup('Vite'));
