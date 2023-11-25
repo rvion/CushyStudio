fa5e82319f19fe0dfaecfee59c6ccd056a083a8d
rvion <vion.remi@gmail.com>
2023-11-25 13:41:33 +0100

    un-gitignore src/utils/custom-jsx/global.d.ts fix #83

---
+++ src/utils/custom-jsx/global.d.ts
@@ -0,0 +1,17 @@
+import 'react'
+
+type ClassLike = string | { [cls: string]: any } | null | undefined | boolean
+
+declare global {
+    namespace JSX {
+        interface IntrinsicAttributes {
+            tw?: string | ClassLike[]
+        }
+    }
+}
+
+declare module 'react' {
+    interface DOMAttributes<T> {
+        tw?: string | ClassLike[]
+    }
+}
