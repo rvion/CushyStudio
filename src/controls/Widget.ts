b24cc5e70807530ae80a76c4e18dc42129a2c301
Rick Love <rick@toldpro.com>
2023-12-01 21:02:41 -0600

    wip optimizer

--- src/controls/Widget.ts
+++ src/controls/Widget.ts
@@ -76,9 +76,19 @@ export class Widget_str implements IRequest<'str', Widget_str_opts, Widget_str_s
 }
 
 // 🅿️ markdown ==============================================================================
-export type Widget_markdown_opts = ReqInput<{ markdown: string | ((formRoot:Widget_group<any>) => string); }>
-export type Widget_markdown_serial = StateFields<{ type: 'markdown', active: true }>
-export type Widget_markdown_state  = StateFields<{ type: 'markdown', active: true }>
+export type Widget_CustomComponentPropsUI = {
+    image: (props: { imageId: string }) => JSX.Element
+}
+export type Widget_CustomComponentProps<T = unknown> = {
+    value: T
+    onChange: (value: T) => void
+    ui: Widget_CustomComponentPropsUI
+}
+export type Widget_CustomComponent = (props: Widget_CustomComponentProps) => JSX.Element
+
+export type Widget_markdown_opts = ReqInput<{ markdown: string | ((formRoot:Widget_group<any>) => string); customComponent?: Widget_CustomComponent}>
+export type Widget_markdown_serial = StateFields<{ type: 'markdown', active: true, renderId: number, componentValue: unknown }>
+export type Widget_markdown_state  = StateFields<{ type: 'markdown', active: true, renderId: number, componentValue: unknown }>
 export type Widget_markdown_output = { type: 'markdown', active: true }
 export interface Widget_markdown extends IWidget<'markdown', Widget_markdown_opts, Widget_markdown_serial, Widget_markdown_state, Widget_markdown_output> {}
 export class Widget_markdown implements IRequest<'markdown', Widget_markdown_opts, Widget_markdown_serial, Widget_markdown_state, Widget_markdown_output> {
@@ -93,6 +103,19 @@ export class Widget_markdown implements IRequest<'markdown', Widget_markdown_opt
         return md(this.builder._ROOT)
     }
 
+    get customComponent(): undefined|Widget_CustomComponent {
+        return this.input.customComponent;
+    }
+    get renderId() {
+        return this.state.renderId;
+    }
+    get componentValue() {
+        return this.state.componentValue;
+    }
+    set componentValue(v:unknown) {
+         this.state.componentValue=v;
+    }
+
     constructor(
         public builder: FormBuilder,
         public schema: SchemaL,
@@ -100,7 +123,7 @@ export class Widget_markdown implements IRequest<'markdown', Widget_markdown_opt
         serial?: Widget_markdown_serial,
     ) {
         this.id = serial?.id ?? nanoid()
-        this.state = serial ?? { type:'markdown', active: true, id: this.id }
+        this.state = serial ?? { type:'markdown', active: true, id: this.id, renderId: 0, componentValue: undefined }
         makeAutoObservable(this)
     }
     get serial(): Widget_markdown_serial { return this.state }
@@ -360,7 +383,7 @@ export class Widget_bool implements IRequest<'bool', Widget_bool_opts, Widget_bo
 }
 
 // 🅿️ inlineRun ==============================================================================
-export type Widget_inlineRun_opts  = ReqInput<{}>
+export type Widget_inlineRun_opts  = ReqInput<{text?: string, kind?: `primary`|`warning`}>
 export type Widget_inlineRun_serial = Widget_inlineRun_state
 export type Widget_inlineRun_state  = StateFields<{ type:'inlineRun', active: true; val: boolean }>
 export type Widget_inlineRun_output = boolean
@@ -376,8 +399,12 @@ export class Widget_inlineRun implements IRequest<'inlineRun', Widget_inlineRun_
         public input: Widget_inlineRun_opts,
         serial?: Widget_inlineRun_serial,
     ) {
+        if(input.text){
+            input.label = input.label ?? ` `;
+        }
+
         this.id = serial?.id ?? nanoid()
-        this.state = serial ?? { type: 'inlineRun', id: this.id, active: true, val: false }
+        this.state = serial ?? { type: 'inlineRun', id: this.id, active: true, val: false, }
         makeAutoObservable(this)
     }
     get serial(): Widget_inlineRun_serial { return this.state }
--- src/controls/widgets/WidgetMarkdownUI.tsx
+++ src/controls/widgets/WidgetMarkdownUI.tsx
@@ -1,13 +1,38 @@
 import { marked } from 'marked'
 import { observer } from 'mobx-react-lite'
-import { Widget_markdown } from 'src/controls/Widget'
+import { useState } from 'react'
+import { Widget_CustomComponentPropsUI, Widget_markdown } from 'src/controls/Widget'
+import { ImageUI } from 'src/widgets/galleries/ImageUI'
+import { useDraft } from 'src/widgets/misc/useDraft'
 
 export const WidgetMardownUI = observer(function WidgetMardownUI_(p: { req: Widget_markdown }) {
     const req = p.req
+
     return (
-        <div //
-            className='_MD w-full'
-            dangerouslySetInnerHTML={{ __html: marked(req.markdown) }}
-        ></div>
+        <>
+            {/* {value}
+            {req.renderId} */}
+            {req.customComponent && (
+                <req.customComponent
+                    key={req.renderId}
+                    value={req.componentValue}
+                    onChange={(v) => (req.componentValue = v)}
+                    ui={ui}
+                />
+            )}
+            {!req.customComponent && (
+                <div //
+                    className='_MD w-full'
+                    dangerouslySetInnerHTML={{ __html: marked(req.markdown) }}
+                ></div>
+            )}
+        </>
     )
 })
+
+const ui: Widget_CustomComponentPropsUI = {
+    image: ({ imageId }: { imageId: string }): JSX.Element => {
+        const draft = useDraft()
+        return <ImageUI img={draft.db.media_images.getOrThrow(imageId)} />
+    },
+}
