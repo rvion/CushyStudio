ac96b171dc67d88a3d57c44b0694c27c61e29701
rvion <vion.remi@gmail.com>
2023-12-04 10:20:45 +0100

    wip cards

--- library/CushyStudio/default/playing-cards/_drawCard.ts
+++ library/CushyStudio/default/playing-cards/_drawCard.ts
@@ -1,19 +1,24 @@
 import type { NodeConfig } from 'konva/lib/Node'
 import type { ImageAndMask, Runtime } from 'src/back/Runtime'
 import { CardSuit, CardSuitPosition, CardValue, getCardLayout } from './_cardLayouts'
+import { TextConfig } from 'konva/lib/shapes/Text'
 
 export async function _drawCard(
     //
     flow: Runtime,
-    value: CardValue,
-    suit: CardSuit,
+    opts: {
+        baseUrl: string
+        value: CardValue
+        suit: CardSuit
+        W: number
+        H: number
+    },
 ): Promise<{
     base: ImageAndMask
     mask: ImageAndMask
 }> {
     const I = await flow.loadImageSDK()
-    const W = 512 // 🔴
-    const H = 800 // 🔴
+    const { suit, value, W, H } = opts
     const mkImage = () => {
         const container: HTMLDivElement = I.createContainer()
         const stage = new I.Stage({ container: container, width: W, height: H })
@@ -22,11 +27,24 @@ export async function _drawCard(
         return { container, stage, layer }
     }
 
+    const mkDomImage = (src: string): Promise<HTMLImageElement> => {
+        const image = new Image()
+        image.src = src
+        return new Promise((yes, no) => {
+            image.onload = () => {
+                return yes(image)
+            }
+        })
+        // return image
+    }
+
+    // prettier-ignore
     const image = await (() => {
+        // return I.loadImage('CushyStudio/default/_assets/symbol-diamond.png')
         if (suit === 'diamonds') return I.loadImage('CushyStudio/default/_assets/symbol-diamond.png')
-        if (suit === 'clubs') return I.loadImage('CushyStudio/default/_assets/symbol-club.png')
-        if (suit === 'hearts') return I.loadImage('CushyStudio/default/_assets/symbol-heart.png')
-        if (suit === 'spades') return I.loadImage('CushyStudio/default/_assets/symbol-spades.png')
+        if (suit === 'clubs')    return I.loadImage('CushyStudio/default/_assets/symbol-club.png')
+        if (suit === 'hearts')   return I.loadImage('CushyStudio/default/_assets/symbol-heart.png')
+        if (suit === 'spades')   return I.loadImage('CushyStudio/default/_assets/symbol-spades.png')
         return exhaust(suit)
     })()
 
@@ -34,9 +52,13 @@ export async function _drawCard(
     // transparent base image
     const base = mkImage()
 
+    if (opts.baseUrl) {
+        const image = await mkDomImage(opts.baseUrl)
+        base.layer.add(new I.Image({ image /*x: 0, y: 0, width: W, height: H*/ }))
+    }
     // white mask
     const mask = mkImage()
-    mask.layer.add(new I.Rect({ x: 0, y: 0, width: W, height: H, fill: 'white' }))
+    mask.layer.add(new I.Rect({ x: 0, y: 0, width: W, height: H, fill: 'transparent' }))
 
     const positions: CardSuitPosition[] = getCardLayout(value)
 
@@ -58,26 +80,36 @@ export async function _drawCard(
         // base image
         const norm = normalize(pos)
         const nthSymbol = new I.Image({ image, ...norm })
+
         // base halo
         const norm2 = normalize(pos, 1.4)
         const nthHalo = new I.Image({ image, ...norm2, opacity: 0.5 })
         base.layer.add(nthHalo, nthSymbol)
+
         // mask image
+        const norm3 = normalize(pos, 0.8)
         const maskImg = new I.Image({ image, ...norm })
-        maskImg.cache()
-        maskImg.filters([I.Konva.Filters.Brighten])
-        maskImg.brightness(-0.3)
-        // maskImg.opacity(0.5)
+        // maskImg.cache()
+        // maskImg.filters([I.Konva.Filters.Brighten])
+        // maskImg.brightness(-0.3)
+        // maskImg.opacity(0.6)
         mask.layer.add(maskImg)
     }
 
     // add numbers and suit color on top-left and bottom-right corners
-    const textOptions = { fontFamily: 'Times New Roman', fontSize: 36, fontWeight: 'bold' }
-    const number = new I.Text({ text: '8', ...textOptions, x: 20, y: 10 })
-    const numberBottom = new I.Text({ text: '8', ...textOptions, x: 20, y: 400, scaleY: -1 })
-    const diamondTop = new I.Image({ image, x: 20, y: 60, width: 30, height: 30 })
-    const diamondBottom = new I.Image({ image, x: 20, y: 360, width: 30, height: 30, scaleY: -1 })
-    base.layer.add(number, numberBottom, diamondTop, diamondBottom)
+    const textOptions: TextConfig = {
+        fontFamily: 'Times New Roman',
+        fontSize: W / 10,
+        fontWeight: 'bold',
+        align: 'center',
+        verticalAlign: 'middle',
+        fill: 'white',
+    }
+    const topNumber    = new I.Text({ text: opts.value, x: .1*W, y: .1*W , ...textOptions }) // prettier-ignore
+    const bottomNumber = new I.Text({ text: opts.value, x: .9*W, y: H-.1*W, ...textOptions, scaleY: -1 }) // prettier-ignore
+    const topSuit      = new I.Image({ image,           x: 20,   y: 60 , width: 30, height: 30 }) // prettier-ignore
+    const bottomSuit   = new I.Image({ image,           x: 20,   y: 360, width: 30, height: 30, scaleY: -1 }) // prettier-ignore
+    base.layer.add(topNumber, bottomNumber, topSuit, bottomSuit)
 
     // export the base
     base.stage.draw()
