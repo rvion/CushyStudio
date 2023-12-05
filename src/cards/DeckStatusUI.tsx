0966c3af7735cbcbb5dac2066df7e9e1b2946b0c
rvion <vion.remi@gmail.com>
2023-12-05 22:27:12 +0100

    minor ui tweaks

--- src/cards/DeckStatusUI.tsx
+++ src/cards/DeckStatusUI.tsx
@@ -5,6 +5,6 @@ import { UpdateBtnUI } from 'src/updater/UpdateBtnUI'
 
 export const ActionPackStatusUI = observer(function PackStatusUI_(p: { className?: string; pack: Package }) {
     const ap = p.pack
-    if (ap.isBuiltIn) return <div tw='italic opacity-50 text-xs'>built-in</div>
+    if (ap.isBuiltIn) return null // <div tw='italic opacity-50 text-xs'>built-in</div>
     return <UpdateBtnUI updater={ap.updater} />
 })
