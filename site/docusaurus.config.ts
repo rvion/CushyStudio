139c6990fb20061eb772325a7669135fbb97ab21
rvion <vion.remi@gmail.com>
2023-12-12 22:49:56 +0100

    new website

--- site/docusaurus.config.ts
+++ site/docusaurus.config.ts
@@ -8,15 +8,15 @@ const config: Config = {
     favicon: 'img/favicon.ico',
 
     // Set the production url of your site here
-    url: 'https://your-docusaurus-site.example.com',
+    url: 'https://www.cushystudio.com/',
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
     baseUrl: '/',
 
     // GitHub pages deployment config.
     // If you aren't using GitHub pages, you don't need these.
-    organizationName: 'facebook', // Usually your GitHub org/user name.
-    projectName: 'docusaurus', // Usually your repo name.
+    organizationName: 'CushyStudio', // Usually your GitHub org/user name.
+    projectName: 'CushyStudio', // Usually your repo name.
 
     onBrokenLinks: 'throw',
     onBrokenMarkdownLinks: 'warn',
@@ -56,10 +56,10 @@ const config: Config = {
         // Replace with your project's social card
         image: 'img/docusaurus-social-card.jpg',
         navbar: {
-            title: 'My Site',
+            title: 'CushyStudio',
             logo: {
                 alt: 'My Site Logo',
-                src: 'img/logo.svg',
+                src: 'img/CushyLogo.png',
             },
             items: [
                 {
