7f237c8e57a08739024becd9bd8cb3478fd32e11
rvion <vion.remi@gmail.com>
2023-12-18 23:19:13 +0100

    OutputImageUI: reuse Panel_ViewImage

--- src/outputs/OutputImageUI.tsx
+++ src/outputs/OutputImageUI.tsx
@@ -5,6 +5,7 @@ import { OutputPreviewWrapperUI } from './OutputPreviewWrapperUI'
 
 import { TransformComponent, TransformWrapper } from 'react-zoom-pan-pinch'
 import { assets } from 'src/utils/assets/assets'
+import { Panel_ViewImage } from 'src/panels/Panel_ViewImage'
 
 export const OutputImagePreviewUI = observer(function OutputImagePreviewUI_(p: {
     step?: Maybe<StepL>
@@ -29,45 +30,5 @@ export const OutputImagePreviewUI = observer(function OutputImagePreviewUI_(p: {
 })
 
 export const OutputImageUI = observer(function OutputImageUI_(p: { step?: Maybe<StepL>; output: StepOutput_Image }) {
-    const image = p.output
-    const url = image.url
-    return (
-        <TransformWrapper centerZoomedOut centerOnInit>
-            <TransformComponent
-                wrapperStyle={{ height: '100%', width: '100%', display: 'flex' }}
-                contentStyle={{ height: '100%', width: '100%' }}
-            >
-                {/* {latentUrl && (
-                    <img //
-                        tw='absolute bottom-0 right-0 shadow-xl'
-                        style={{ width: st.latentSizeStr, height: st.latentSizeStr, objectFit: 'contain' }}
-                        src={latentUrl}
-                        alt='last generated image'
-                    />
-                )} */}
-                {url ? (
-                    <img //
-                        style={{ width: '100%', height: '100%', objectFit: 'contain' }}
-                        src={url}
-                        alt='last generated image'
-                    />
-                ) : (
-                    <div tw='w-full h-full relative flex'>
-                        <div
-                            style={{ fontSize: '3rem', textShadow: '0 0 5px #ffffff' }}
-                            tw='animate-pulse absolute self-center w-full text-center text-xl text-black font-bold'
-                        >
-                            no image yet
-                        </div>
-                        <img //
-                            style={{ width: '100%', height: '100%', objectFit: 'contain' }}
-                            src={assets.illustrations_image_home_transp_webp}
-                            alt='last generated image'
-                        />
-                    </div>
-                )}
-                {/* </div> */}
-            </TransformComponent>
-        </TransformWrapper>
-    )
+    return <Panel_ViewImage imageID={p.output.id} />
 })
