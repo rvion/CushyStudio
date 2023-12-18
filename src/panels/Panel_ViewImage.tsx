50969830cd2fb437ea0934860acf22fc1a4326cb
rvion <vion.remi@gmail.com>
2023-12-18 22:59:36 +0100

    improve Panel_ViewImage a lot

--- src/panels/Panel_ViewImage.tsx
+++ src/panels/Panel_ViewImage.tsx
@@ -8,6 +8,8 @@ import { Rate, Toggle } from 'src/rsuite/shims'
 import { useSt } from 'src/state/stateContext'
 import { assets } from 'src/utils/assets/assets'
 import { openExternal, showItemInFolder } from '../app/layout/openExternal'
+import { RevealUI } from 'src/rsuite/RevealUI'
+import { JsonViewUI } from 'src/widgets/workspace/JsonViewUI'
 
 export const Panel_ViewImage = observer(function Panel_ViewImage_(p: {
     //
@@ -19,90 +21,16 @@ export const Panel_ViewImage = observer(function Panel_ViewImage_(p: {
         ? st.db.media_images.get(p.imageID)
         : st.db.media_images.last()
     const url = img?.url
-    // const { img, url, latentUrl } = getPreviewType(st, p.imageID)
-    const imgPathWithFileProtocol = img ? `file://${img.absPath}` : null
-    // if (img == null) return null
     const background = st.configFile.value.galleryBgColor
 
     return (
-        <div
-            //
-            className={p.className}
-            tw='flex flex-col flex-grow bg-base-100'
-            style={{ background }}
-        >
-            <div tw='flex items-center gap-2 bg-base-200'>
-                {/* <FieldAndLabelUI label='Rating'> */}
-                <Rate
-                    name={img?.id ?? 'latent'}
-                    value={img?.data.star ?? 0}
-                    disabled={img == null}
-                    onChange={(next) => {
-                        if (img == null) return
-                        // const next = ev.target.value
-                        img.update({ star: next })
-                    }}
-                />
-                {/* </FieldAndLabelUI> */}
-
-                {/* 2. LATENT PREVIEW TOOGLE */}
-                {/* (only on "last-image" mode; when p.imageID is null )  */}
-                {p.imageID == null ? (
-                    <div tw='flex items-center'>
-                        Sampler
-                        <Toggle
-                            checked={st.showLatentPreviewInLastImagePanel}
-                            onChange={(ev) => (st.showLatentPreviewInLastImagePanel = ev.target.checked)}
-                        />
-                    </div>
-                ) : null}
-
-                {/* <FieldAndLabelUI label='Size'> */}
-                {/* </FieldAndLabelUI> */}
-                {/* 3. OPEN OUTPUT FOLDER */}
-                <Dropdown title='Actions' startIcon={<span className='material-symbols-outlined'>menu</span>}>
-                    <MenuItem
-                        icon={<span className='material-symbols-outlined'>folder</span>}
-                        size='sm'
-                        // appearance='subtle'
-                        disabled={!img?.absPath}
-                        onClick={() => {
-                            if (!img?.absPath) return
-                            showItemInFolder(img.absPath)
-                        }}
-                    >
-                        open folder
-                    </MenuItem>
-                    {/* 3. OPEN FILE ITSELF */}
-                    <MenuItem
-                        icon={<span className='material-symbols-outlined'>folder</span>}
-                        size='xs'
-                        // appearance='subtle'
-                        disabled={!img?.absPath}
-                        onClick={() => {
-                            if (imgPathWithFileProtocol == null) return
-                            openExternal(imgPathWithFileProtocol)
-                        }}
-                    >
-                        open
-                    </MenuItem>
-                </Dropdown>
-                <div tw='virtualBorder p-1'>{`${img?.data.width ?? '?'} x ${img?.data.height ?? '?'}`}</div>
-            </div>
-
+        <div className={p.className} style={{ background }} tw='flex flex-col flex-grow bg-base-100 relative'>
+            <ImageActionBarUI img={img} />
             <TransformWrapper centerZoomedOut centerOnInit>
                 <TransformComponent
-                    wrapperStyle={{ /* border: '5px solid #b53737', */ height: '100%', width: '100%', display: 'flex' }}
-                    contentStyle={{ /* border: '5px solid #38731f', */ height: '100%', width: '100%' }}
+                    wrapperStyle={{ height: '100%', width: '100%', display: 'flex' }}
+                    contentStyle={{ height: '100%', width: '100%' }}
                 >
-                    {/* {latentUrl && (
-                        <img //
-                            tw='absolute bottom-0 right-0 shadow-xl'
-                            style={{ width: st.latentSizeStr, height: st.latentSizeStr, objectFit: 'contain' }}
-                            src={latentUrl}
-                            alt='last generated image'
-                        />
-                    )} */}
                     {url ? (
                         <img //
                             style={{ width: '100%', height: '100%', objectFit: 'contain' }}
@@ -124,9 +52,79 @@ export const Panel_ViewImage = observer(function Panel_ViewImage_(p: {
                             />
                         </div>
                     )}
-                    {/* </div> */}
                 </TransformComponent>
             </TransformWrapper>
+        </div>
+    )
+})
+
+export const ImageActionBarUI = observer(function ImageActionBarUI_(p: { img?: Maybe<MediaImageL> }) {
+    const st = useSt()
+    const img = p.img
+    return (
+        <div tw='flex items-center gap-2 bg-base-200'>
+            {/* <FieldAndLabelUI label='Rating'> */}
+            <Rate
+                name={img?.id ?? 'latent'}
+                value={img?.data.star ?? 0}
+                disabled={img == null}
+                onChange={(next) => {
+                    if (img == null) return
+                    // const next = ev.target.value
+                    img.update({ star: next })
+                }}
+            />
+            <div
+                tw='btn btn-sm'
+                onClick={() => {
+                    if (img == null) return
+                    st.db.media_images.delete(img.id)
+                }}
+            >
+                <span className='material-symbols-outlined'>edit</span>
+                Edit
+            </div>
+            <div
+                tw='btn btn-sm'
+                onClick={() => {
+                    if (img == null) return
+                    st.db.media_images.delete(img.id)
+                }}
+            >
+                <span className='material-symbols-outlined'>delete_forever</span>
+                Delete
+            </div>
+
+            {/* 3. OPEN OUTPUT FOLDER */}
+            <Dropdown title='Actions' startIcon={<span className='material-symbols-outlined'>menu</span>}>
+                <MenuItem
+                    icon={<span className='material-symbols-outlined'>folder</span>}
+                    size='sm'
+                    // appearance='subtle'
+                    disabled={!img?.absPath}
+                    onClick={() => {
+                        if (!img?.absPath) return
+                        showItemInFolder(img.absPath)
+                    }}
+                >
+                    open folder
+                </MenuItem>
+                {/* 3. OPEN FILE ITSELF */}
+                <MenuItem
+                    icon={<span className='material-symbols-outlined'>folder</span>}
+                    size='xs'
+                    // appearance='subtle'
+                    disabled={!img?.absPath}
+                    onClick={() => {
+                        const imgPathWithFileProtocol = img ? `file://${img.absPath}` : null
+                        if (imgPathWithFileProtocol == null) return
+                        openExternal(imgPathWithFileProtocol)
+                    }}
+                >
+                    open
+                </MenuItem>
+            </Dropdown>
+            <div tw='virtualBorder p-1'>{`${img?.data.width ?? '?'} x ${img?.data.height ?? '?'}`}</div>
         </div>
     )
 })
