c3732ff5c6fc4856daf71e3eb00fd84f18d6b748
rvion <vion.remi@gmail.com>
2023-12-18 17:26:51 +0100

    more aliases

--- src/syms/_.cjs
+++ src/syms/_.cjs
@@ -1,24 +1,34 @@
 const modulesToCache = [
-    //
-    'assert',
-    'url',
-    'buffer',
-    'child_process',
-    'fs',
-    'os',
-    'path',
-    'process',
-    'stream',
-    'util',
-    'zlib',
-    'events',
-    'cluster',
+    // node ----------
+    ['assert', 'assert'],
+    ['url', 'url'],
+    ['buffer', 'buffer'],
+    ['child_process', 'child_process'],
+    ['fs', 'fs'],
+    ['os', 'os'],
+    ['path', 'path'],
+    ['process', 'process'],
+    ['stream', 'stream'],
+    ['util', 'util'],
+    ['zlib', 'zlib'],
+    ['events', 'events'],
+    ['cluster', 'cluster'],
+    // externals ----------------
+    ['three', 'three'],
+    ['mobx', 'mobx'],
+    ['cytoscape-klay', 'cytoscape-klay'],
+    ['cytoscape', 'cytoscape'],
+    ['lexical', 'lexical'],
+    // ['react-dom', 'react-dom'],
+    // ['konva', 'konva/lib/index.js'],
+    // 'wildcards',
+    // 'three/examples/jsm/controls/OrbitControls.js',
 ]
 
 const fs = require('fs')
 
-for (const x of modulesToCache) {
-    const symbols = Object.keys(require(x))
+for (const [x, pt] of modulesToCache) {
+    const symbols = Object.keys(require(pt))
     const out1 = JSON.stringify(symbols)
     // fs.writeFileSync(`src/syms/${x}.txt`, out1)
 
