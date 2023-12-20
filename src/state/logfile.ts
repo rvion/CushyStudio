ccf63c24409f7cc95c526df3cf61ce8a564406a0
rvion <vion.remi@gmail.com>
2023-12-20 22:17:34 +0100

    move auth logs

--- src/state/logfile.ts
+++ src/state/logfile.ts
@@ -1,3 +1,3 @@
 import { LogfileService } from 'logfile-service'
 
-export const logger = new LogfileService({ tag: 'debug', utc: false })
+export const logger = new LogfileService({ tag: 'debug', utc: false, dir: 'logs' })
