fffe2de7f094d2fba0266329b77e0122196986c5
rvion <vion.remi@gmail.com>
2023-12-20 15:13:22 +0100

    improve theme menu

--- src/app/appbar/MenuConfigUI.tsx
+++ src/app/appbar/MenuConfigUI.tsx
@@ -1,7 +1,6 @@
 import { observer } from 'mobx-react-lite'
 import { Dropdown, MenuItem } from 'src/rsuite/Dropdown'
 import { useSt } from '../../state/stateContext'
-import { ThemePreviewUI } from './utils/ThemePreviewUI'
 
 export const MenuConfigUI = observer(function MenuConfigUI_(p: {}) {
     const st = useSt()
@@ -21,32 +20,6 @@ export const MenuConfigUI = observer(function MenuConfigUI_(p: {}) {
                 icon={<span className='material-symbols-outlined text-purple-500'>cloud</span>}
                 label='ComfyUI Hosts'
             />
-        </Dropdown>
-    )
-})
-
-export const MenuThemeUI = observer(function MenuThemeUI_(p: {}) {
-    const st = useSt()
-    return (
-        <Dropdown //
-            startIcon={<span className='material-symbols-outlined text-primary'>color_lens</span>}
-            title=''
-            appearance='subtle'
-        >
-            {st.themeMgr.themes.map((theme) => (
-                <div
-                    tw='cursor-pointer hover:bg-base-300 p-0.5'
-                    key={theme}
-                    // icon={<span className='text-orange-400 material-symbols-outlined'>sync</span>}
-                    onClick={(ev) => {
-                        ev.preventDefault()
-                        ev.stopPropagation()
-                        st.themeMgr.theme = theme
-                    }}
-                >
-                    <ThemePreviewUI theme={theme} />
-                </div>
-            ))}
         </Dropdown>
     )
 })
