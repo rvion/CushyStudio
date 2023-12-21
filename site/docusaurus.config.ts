e62d054f1630ce64ec624fbee2fa1bc43194b002
rvion <vion.remi@gmail.com>
2023-12-21 13:51:25 +0100

    allow one-click to edit pages

--- site/docusaurus.config.ts
+++ site/docusaurus.config.ts
@@ -37,13 +37,13 @@ const config: Config = {
                     sidebarPath: './sidebars.ts',
                     // Please change this to your repo.
                     // Remove this to remove the "edit this page" links.
-                    editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+                    editUrl: 'https://github.com/rvion/CushyStudio/tree/master/site',
                 },
                 blog: {
                     showReadingTime: true,
                     // Please change this to your repo.
                     // Remove this to remove the "edit this page" links.
-                    editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+                    editUrl: 'https://github.com/rvion/CushyStudio/tree/master/site',
                 },
                 theme: {
                     customCss: './src/css/custom.css',
