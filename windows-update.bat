6baf3669d238d28e29b49956ff28f8d5550ac793
rvion <vion.remi@gmail.com>
2023-12-21 23:38:07 +0100

    windows update

--- windows-update.bat
+++ windows-update.bat
@@ -5,12 +5,14 @@
 git checkout master
 IF %ERRORLEVEL% NEQ 0 (
     ECHO "git checkout master failed"
+    pause
     EXIT /B 1
 )
 
 git pull origin master
 IF %ERRORLEVEL% NEQ 0 (
     ECHO "git pull origin master failed"
+    pause
     EXIT /B 1
 )
 
