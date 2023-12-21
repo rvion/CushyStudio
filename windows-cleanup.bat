1c5741f042e167c3df19185246b1db7fccb22508
rvion <vion.remi@gmail.com>
2023-12-21 23:59:16 +0100

    tweak cleanup script

--- windows-cleanup.bat
+++ windows-cleanup.bat
@@ -4,11 +4,19 @@
 @REM setlocal: Ensures that the environment changes are local to the script.
 setlocal
 
+ECHO [===================================================]
+ECHO deleting node_modules folder...
+
 @REM rd /s /q node_modules:
 @REM Removes the node_modules directory recursively.
 @REM - /s deletes all files and subdirectories,
 @REM - /q does it quietly without prompting for confirmation.
 rd /s /q node_modules
+
+ECHO [===================================================]
+ECHO deleting recursively .cushy\store (local pnpm cache)...
+
 rd /s /q .cushy\store
 
+ECHO SUCCESS
 pause
