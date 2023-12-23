a3864c5fa39873dfa16c1bf70ed9beb2179afcf1
rvion <vion.remi@gmail.com>
2023-12-23 09:37:41 +0100

    add sdk-canvas example

--- library/sdk-examples/sdk-canvas.ts
+++ library/sdk-examples/sdk-canvas.ts
@@ -1,9 +1,14 @@
 app({
-    ui(form) {
-        return {}
+    ui: (form) => ({}),
+    run: async (run, form) => {
         //
-    },
-    run(flow, form) {
-        //
+        const layer = run.Konva.createStageWithLayer({ width: 512, height: 512 })
+        run.Konva.addGradientToLayer(layer, [0, 'red', Math.random(), 'pink', 1, 'yellow'])
+
+        const b64 = run.Konva.convertLayerToBase64(layer)
+        const loadImageNode = await run.load_dataURL(b64)
+
+        run.nodes.PreviewImage({ images: loadImageNode })
+        run.PROMPT()
     },
 })
