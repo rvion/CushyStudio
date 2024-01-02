c673af37939d6f1084cb5882dc95fd229374e95f
rvion <vion.remi@gmail.com>
2024-01-02 22:01:38 +0100

    add horizontal example

---
+++ library/sdk-examples/sdk-ui-horizontal.ts
@@ -0,0 +1,19 @@
+app({
+    ui: (form) => ({
+        foo: form.group({
+            layout: 'H',
+            items: () => ({
+                a: form.int({}),
+                b: form.int({}),
+            }),
+        }),
+        bar: form.group({
+            layout: 'H',
+            items: () => ({
+                a: form.int({}),
+                b: form.int({}),
+            }),
+        }),
+    }),
+    run: async (flow, form) => {},
+})
