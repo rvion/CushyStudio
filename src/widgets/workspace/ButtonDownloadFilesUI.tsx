131d5b7cf767e2f0472d12f156cf6f207c2d3945
rvion <vion.remi@gmail.com>
2024-01-04 23:40:33 +0100

    makes downloading comfyUI prompt and workflow way simpler

--- src/widgets/workspace/ButtonDownloadFilesUI.tsx
+++ src/widgets/workspace/ButtonDownloadFilesUI.tsx
@@ -5,7 +5,10 @@ import { observer } from 'mobx-react-lite'
 import { Button, Tooltip, Whisper } from 'src/rsuite/shims'
 import { useSt } from '../../state/stateContext'
 
-export const ButtonDownloadFilesUI = observer(function ButtonDownloadFilesUI_(p: { graph: ComfyWorkflowL | GraphID }) {
+export const ButtonDownloadFilesUI = observer(function ButtonDownloadFilesUI_(p: {
+    //
+    graph: ComfyWorkflowL | GraphID
+}) {
     let graphOrGraphID = p.graph
     const st = useSt()
     const graph =
@@ -14,55 +17,52 @@ export const ButtonDownloadFilesUI = observer(function ButtonDownloadFilesUI_(p:
             : graphOrGraphID
 
     return (
-        <>
-            <Whisper placement='auto' speaker={<Tooltip>Download as ComfyUI workflow.json</Tooltip>}>
-                <Button
-                    appearance='link'
-                    icon={<span className='material-symbols-outlined'>account_tree</span>}
-                    size='xs'
-                    onClick={async (ev) => {
-                        ev.preventDefault()
-                        ev.preventDefault()
-                        const jsonWorkflow = await graph.json_workflow()
-                        console.log('>>>🟢', { jsonWorkflow })
-                        // ensure folder exists
-                        const folderExists = existsSync(graph.cacheFolder)
-                        if (!folderExists) mkdirSync(graph.cacheFolder, { recursive: true })
-                        // save file
-                        const path = graph.getTargetWorkflowFilePath()
-                        console.log('>>>🟢', { path })
-                        // open folder containing file
-                        window.require('electron').shell.openExternal(`file://${path}/..`)
-                        writeFileSync(path, JSON.stringify(jsonWorkflow, null, 3))
-                    }}
-                >
-                    {/* download ComfyUI file */}
-                </Button>
-            </Whisper>
-            <Whisper placement='auto' speaker={<Tooltip>Download as ComfyUI prompt</Tooltip>}>
-                <Button
-                    appearance='link'
-                    icon={<span className='material-symbols-outlined'>message</span>}
-                    size='xs'
-                    onClick={async (ev) => {
-                        ev.preventDefault()
-                        ev.stopPropagation()
-                        const jsonPrompt = graph.json_forPrompt('use_class_name_and_number')
-                        // console.log('>>>🟢', { jsonPrompt })
-                        // ensure folder exists
-                        const folderExists = existsSync(graph.cacheFolder)
-                        if (!folderExists) mkdirSync(graph.cacheFolder, { recursive: true })
-                        // save file
-                        const path = graph.getTargetPromptFilePath()
-                        // console.log('>>>🟢', { path })
-                        // open folder containing file
-                        window.require('electron').shell.openExternal(`file://${path}/..`)
-                        writeFileSync(path, JSON.stringify(jsonPrompt, null, 3))
-                    }}
-                >
-                    {/* download ComfyUI file */}
-                </Button>
-            </Whisper>
-        </>
+        <div tw='flex gap-2 items-center'>
+            Download:{' '}
+            <Button
+                appearance='primary'
+                icon={<span className='material-symbols-outlined'>cloud_download</span>}
+                size='sm'
+                onClick={async (ev) => {
+                    ev.preventDefault()
+                    ev.preventDefault()
+                    const jsonWorkflow = await graph.json_workflow()
+                    console.log('>>>🟢', { jsonWorkflow })
+                    // ensure folder exists
+                    const folderExists = existsSync(graph.cacheFolder)
+                    if (!folderExists) mkdirSync(graph.cacheFolder, { recursive: true })
+                    // save file
+                    const path = graph.getTargetWorkflowFilePath()
+                    console.log('>>>🟢', { path })
+                    // open folder containing file
+                    window.require('electron').shell.openExternal(`file://${path}/..`)
+                    writeFileSync(path, JSON.stringify(jsonWorkflow, null, 3))
+                }}
+            >
+                ComfyUI Workflow
+            </Button>
+            <Button
+                appearance={'ghost'}
+                icon={<span className='material-symbols-outlined'>cloud_download</span>}
+                size='sm'
+                onClick={async (ev) => {
+                    ev.preventDefault()
+                    ev.stopPropagation()
+                    const jsonPrompt = graph.json_forPrompt('use_class_name_and_number')
+                    // console.log('>>>🟢', { jsonPrompt })
+                    // ensure folder exists
+                    const folderExists = existsSync(graph.cacheFolder)
+                    if (!folderExists) mkdirSync(graph.cacheFolder, { recursive: true })
+                    // save file
+                    const path = graph.getTargetPromptFilePath()
+                    // console.log('>>>🟢', { path })
+                    // open folder containing file
+                    window.require('electron').shell.openExternal(`file://${path}/..`)
+                    writeFileSync(path, JSON.stringify(jsonPrompt, null, 3))
+                }}
+            >
+                ComfyUI PROMPT
+            </Button>
+        </div>
     )
 })
