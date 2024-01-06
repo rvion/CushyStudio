c08850d4bc4c372d750ca3af34ea3227bd72fdee
rvion <vion.remi@gmail.com>
2024-01-06 23:25:36 +0100

    fix comfy_manager jsons

--- src/wiki/updateJSONs.ts
+++ src/wiki/updateJSONs.ts
@@ -1,18 +1,18 @@
 import { downloadFile } from 'src/utils/fs/downloadFile'
 
 await downloadFile(
-    'https://raw.githubusercontent.com/ltdrdata/ComfyUI-Manager/main/node_db/new/alter-list.json',
+    'https://raw.githubusercontent.com/ltdrdata/ComfyUI-Manager/main/alter-list.json',
     'src/wiki/jsons/alter-list.json',
 )
 await downloadFile(
-    'https://raw.githubusercontent.com/ltdrdata/ComfyUI-Manager/main/node_db/new/custom-node-list.json',
+    'https://raw.githubusercontent.com/ltdrdata/ComfyUI-Manager/main/custom-node-list.json',
     'src/wiki/jsons/custom-node-list.json',
 )
 await downloadFile(
-    'https://raw.githubusercontent.com/ltdrdata/ComfyUI-Manager/main/node_db/new/extension-node-map.json',
+    'https://raw.githubusercontent.com/ltdrdata/ComfyUI-Manager/main/extension-node-map.json',
     'src/wiki/jsons/extension-node-map.json',
 )
 await downloadFile(
-    'https://raw.githubusercontent.com/ltdrdata/ComfyUI-Manager/main/node_db/new/model-list.json',
+    'https://raw.githubusercontent.com/ltdrdata/ComfyUI-Manager/main/model-list.json',
     'src/wiki/jsons/model-list.json',
 )
