cdf781b6ae94713353159ae56707d3967e812e3f
rvion <vion.remi@gmail.com>
2024-01-19 20:06:04 +0100

    remove some more legacy type level around requirements

--- src/cards/App.ts
+++ src/cards/App.ts
@@ -2,8 +2,8 @@ import type { CSSProperties } from 'react'
 import type { FormBuilder } from 'src/controls/FormBuilder'
 import type { Runtime } from 'src/runtime/Runtime'
 import type { Widget, Widget_group } from '../controls/Widget'
-import { AppMetadata } from './AppManifest'
-import { MediaImageL } from 'src/models/MediaImage'
+import type { AppMetadata } from './AppManifest'
+import type { MediaImageL } from 'src/models/MediaImage'
 
 // ACTIONS ============================================================
 // 1. the main abstraction of cushy are actions.
@@ -43,36 +43,4 @@ export type App<FIELDS extends WidgetDict> = {
     customNodeRequired?: string[]
     /** help text to show user when using their card */
     help?: string
-}
-
-// REQUIREMENTS ============================================================
-/** a set of requirements your action expect to be runnable */
-export type Requirements = {
-    [name: string]: Requirement
-}
-
-/** a single requirement */
-export type Requirement<T = any> = {
-    // the required node or value required to be present in the graph / flow
-    type: string
-
-    tag?: string | string[]
-    findOrCreate?: (flow: Runtime) => T
-
-    /** if specified, Cushy will check if missing requirements can be created to
-     * know if it shoul suggest this flow or not
-     */
-    syncCheckIfCreationIsPossible?: () => boolean
-    creationLogic?: () => T
-}
-
-// helper to build requirements in a type-safe way
-export type ReqBuilder = {
-    [k in keyof Requirable]: (req?: Omit<Requirement<Requirable[k]>, 'type'>) => Requirement<Requirable[k]>
-}
-
-// REQUIRABLE ============================================================
-// requirements are lazylly transformed at execution time when needed by the action
-export type Resolved<Reqs extends { [name: string]: Requirement }> = {
-    [K in keyof Reqs]: Reqs[K] extends Requirement<infer T> ? () => T : never
 }
