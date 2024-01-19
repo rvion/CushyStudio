45843fe8b203619dd0be453a1a9548dbd5e4d238
rvion <vion.remi@gmail.com>
2024-01-19 21:36:09 +0100

    cleanup

--- src/controls/widgets/WidgetBoolUI.tsx
+++ src/controls/widgets/WidgetBoolUI.tsx
@@ -5,13 +5,4 @@ import { Widget_bool } from 'src/controls/Widget'
 
 export const WidgetBoolUI = observer(function WidgetBoolUI_(p: { widget: Widget_bool }) {
     return null // fieldWithUI toogle should handle that alreadly
-    // return (
-    //     <Toggle //
-    //         checked={p.req.state.val}
-    //         onChange={(ev) => {
-    //             p.req.state.val = ev.target.checked
-    //             // p.req.state.active = ev.target.checked
-    //         }}
-    //     />
-    // )
 })
