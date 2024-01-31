071d004421f6ec0983d59cca1f110354b9e6bfd8
rvion <vion.remi@gmail.com>
2024-01-31 22:31:46 +0100

    optional: safety net around collapsed child

--- src/controls/widgets/optional/WidgetOptionalUI.tsx
+++ src/controls/widgets/optional/WidgetOptionalUI.tsx
@@ -3,9 +3,11 @@ import type { Widget } from '../../Widget'
 
 import { observer } from 'mobx-react-lite'
 import { WidgetUI } from '../WidgetUI'
+import { WidgetWithLabelUI } from 'src/controls/shared/WidgetWithLabelUI'
 
 export const WidgetOptionalUI = observer(function WidgetBoolUI_<T extends Widget>(p: { widget: Widget_optional<T> }) {
     if (!p.widget.serial.active) return null
     if (p.widget.child == null) return <>❌ ERROR: optional is active but no widget❓</>
+    if (p.widget.child?.serial.collapsed) return <WidgetWithLabelUI rootKey={'_'} widget={p.widget.child} />
     return <WidgetUI widget={p.widget.child} />
 })
