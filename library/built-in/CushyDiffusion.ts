52e81acfcf302e45534f41ea80490f1ccd66a05f
rvion <vion.remi@gmail.com>
2024-02-08 22:41:33 +0100

    fix typo

--- library/built-in/CushyDiffusion.ts
+++ library/built-in/CushyDiffusion.ts
@@ -27,7 +27,7 @@ app({
             default: [
                 //
                 'masterpiece, tree',
-                '?color, ?"3d_term", ?adj_beauty, ?adj_general',
+                '?color, ?3d_term, ?adj_beauty, ?adj_general',
                 '(nature)*0.9, (intricate_details)*1.1',
             ].join('\n'),
         }),
