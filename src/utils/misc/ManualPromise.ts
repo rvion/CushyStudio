ab971582f6c172a7d262572b784bad3c9661afd5
rvion <vion.remi@gmail.com>
2024-02-23 22:55:59 +0100

    cleanup manual promise

--- src/utils/misc/ManualPromise.ts
+++ src/utils/misc/ManualPromise.ts
@@ -7,22 +7,9 @@ export class ManualPromise<T = any> implements PromiseLike<T> {
     reject!: (err: any) => void
     promise: Promise<T>
     then: Promise<T>['then']
-    update = () => this.fn?.(this)
     isRunning = false
-
-    logs: string[] = []
-    addLog = (log: string) => {
-        this.logs.push(log)
-    }
-
-    setValue = (t: T) => {
-        this.value = t
-    }
-
-    constructor(
-        //
-        public fn?: (self: ManualPromise<T>) => {},
-    ) {
+    setValue = (t: T) => { this.value = t } // prettier-ignore
+    constructor() {
         this.promise = new Promise((resolve, reject) => {
             this.resolve = (t: T | PromiseLike<T>) => {
                 runInAction(() => {
