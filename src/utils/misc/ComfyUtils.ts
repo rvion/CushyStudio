2f2b1c85d7532419bfacc5457ba5c98bd8fc57a1
rvion <vion.remi@gmail.com>
2024-02-24 21:19:20 +0100

    comment to ask to leave ComfyUtils.ts minimal

--- src/utils/misc/ComfyUtils.ts
+++ src/utils/misc/ComfyUtils.ts
@@ -1,3 +1,4 @@
+/* please, keep this file as minimal as possible; it's imported by many apps */
 export const exhaust = (x: never) => x
 export const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms))
 export const deepCopyNaive = <T>(x: T): T => JSON.parse(JSON.stringify(x))
