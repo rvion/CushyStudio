5c0b8a732e652a1090bdfb8fe3f80c7c25d605d6
bird_d <relay198@gmail.com>
2024-03-01 11:09:08 -0500

    UI: Improve Plugin_LoraBoxUI - Changes the UI Layout - Adds WeightedExpression support - Allows editing the token weights - It will automatically add the WeightedExpression syntax
    Does not make the delete button do anything
    This could probably use the group panel thing, so it can collapse

--- src/controls/widgets/prompt/plugins/Plugin_LoraBoxUI.tsx
+++ src/controls/widgets/prompt/plugins/Plugin_LoraBoxUI.tsx
@@ -16,9 +16,11 @@ export const Plugin_LoraControlsUI = observer(function Plugin_LoraControlsUI_(p:
     return (
         <>
             {uist.loras.length === 0 && <div tw='italic text-gray-500'>No loras in prompt</div>}
-            {uist.loras.map((x) => {
-                return <LoraBoxUI uist={uist} def={x} onDelete={() => {}} />
-            })}
+            <div tw='flex flex-col p-1 gap-1'>
+                {uist.loras.map((x) => {
+                    return <LoraBoxUI uist={uist} def={x} onDelete={() => {}} />
+                })}
+            </div>
         </>
     )
 })
@@ -36,20 +38,40 @@ const LoraBoxUI = observer(function LoraBoxUI_(p: {
     const loraMetadata = st.configFile.value?.loraPrompts?.[loraName]
     const associatedText = loraMetadata?.text ?? ''
     const associatedUrl = loraMetadata?.url ?? ''
+    // const weightedExpression =
+    const weighted = p.uist.ast.findAll('WeightedExpression').find((weighted) => {
+        let text = weighted.contentText
+        if (text.length == 0 || text.charAt(0) != '@') {
+            return false
+        }
+        return weighted.contentText.indexOf(`@${loraName}`) > -1
+    })
+
     // const numbers = def.ref.node.getChildren('Number')
     return (
-        <div key={loraName}>
-            {node.name}
+        <div key={loraName} tw='bg-base-100 rounded p-2'>
+            <div //Header
+                tw='flex w-full h-10 border-b pb-2 items-center border-base-200 mb-2'
+            >
+                {node.name}
+                <div tw='flex-0 flex-grow'></div>
+                <Button //
+                    size='xs'
+                    icon={<span className='material-symbols-outlined'>delete_forever</span>}
+                    onClick={p.onDelete}
+                    tw='h-full'
+                />
+            </div>
             <div tw='flex gap-1 items-center'>
-                <div tw='w-32'>model strength</div>
-                <div tw='flex items-center'>
+                <div tw='w-32'>Model Strength</div>
+                <div tw='flex flex-grow items-center'>
                     <InputNumberUI
                         value={node.strength_model ?? 1}
-                        style={{ width: '4.5rem' }}
                         mode='float'
                         step={0.1}
                         min={-2}
                         max={2}
+                        hideSlider
                         onValueChange={(v) => {
                             const num1 = node.getChild('Number', 0)
                             if (num1) num1.setNumber(v)
@@ -68,15 +90,15 @@ const LoraBoxUI = observer(function LoraBoxUI_(p: {
                 </div>
             </div>
             <div tw='flex gap-1 items-center'>
-                <div tw='w-32'>clip strength</div>
-                <div tw='flex items-center'>
+                <div tw='w-32'>Clip Strength</div>
+                <div tw='flex flex-grow items-center'>
                     <InputNumberUI
                         value={node.strength_clip ?? 1}
-                        style={{ width: '4.5rem' }}
                         mode='float'
                         step={0.1}
                         min={-2}
                         max={2}
+                        hideSlider
                         onValueChange={(v) => {
                             const num2 = node.getChild('Number', 1)
                             if (num2) num2.setNumber(v)
@@ -96,30 +118,62 @@ const LoraBoxUI = observer(function LoraBoxUI_(p: {
                 </div>
             </div>
             <div tw='flex gap-1 items-center'>
-                <div tw='w-32'>trigger words*</div>
-                <Input
-                    //
-                    type='text'
-                    value={associatedText}
-                    onChange={(ev) => {
-                        const nextText = ev.target.value
-                        st.configFile.update((prev) => {
-                            // ensure prev.loraPrompts
-                            if (!prev.loraPrompts) prev.loraPrompts = {}
-                            const lp = prev.loraPrompts
-                            // ensure entry for lora name
-                            let entry = lp[loraName]
-                            if (!entry) entry = lp[loraName] = { text: '' }
-                            entry.text = nextText
-                        })
-                    }}
-                />
-                <div>x {'<weights>'} </div>
+                <div tw='w-32'>Trigger Words*</div>
+                <div tw='flex flex-grow join'>
+                    <Input
+                        //
+                        tw='WIDGET-FIELD rounded join-item'
+                        type='text'
+                        value={associatedText}
+                        onChange={(ev) => {
+                            const nextText = ev.target.value
+                            st.configFile.update((prev) => {
+                                // ensure prev.loraPrompts
+                                if (!prev.loraPrompts) prev.loraPrompts = {}
+                                const lp = prev.loraPrompts
+                                // ensure entry for lora name
+                                let entry = lp[loraName]
+                                if (!entry) entry = lp[loraName] = { text: '' }
+                                entry.text = nextText
+                            })
+                        }}
+                    />
+                    <InputNumberUI //
+                        tw='join-item'
+                        value={weighted?.weight ?? 1.0}
+                        mode='float'
+                        step={0.1}
+                        min={-2}
+                        max={2}
+                        hideSlider
+                        onValueChange={(v) => {
+                            // Change weight, or add WeightedExpression if there is none.
+                            if (weighted) {
+                                weighted.weight = v
+                            } else {
+                                p.uist.editorView?.dispatch({
+                                    changes: {
+                                        from: node.to,
+                                        to: node.to,
+                                        insert: `)*${v}`,
+                                    },
+                                })
+
+                                p.uist.editorView?.dispatch({
+                                    changes: {
+                                        from: node.from,
+                                        to: node.from,
+                                        insert: '(',
+                                    },
+                                })
+                            }
+                        }}
+                    ></InputNumberUI>
+                </div>
             </div>
             <div tw='opacity-50 italic text-xs'>
-                *: only trigger words will be multiplied by weights; to change the lora model and clip strength, use [x,y] syntax
+                *: Only trigger words will be multiplied by weights; to change the lora model and clip strength, use [x,y] syntax
             </div>
-
             <div>
                 <Button
                     size='sm'
@@ -130,6 +184,7 @@ const LoraBoxUI = observer(function LoraBoxUI_(p: {
                     Associated URL
                 </Button>
                 <Input
+                    tw='WIDGET-FIELD  rounded'
                     type='text'
                     value={associatedUrl}
                     onChange={(ev) => {
@@ -146,7 +201,6 @@ const LoraBoxUI = observer(function LoraBoxUI_(p: {
                     }}
                 />
             </div>
-            <Button size='xs' icon={<span className='material-symbols-outlined'>delete_forever</span>} onClick={p.onDelete} />
         </div>
     )
 })
