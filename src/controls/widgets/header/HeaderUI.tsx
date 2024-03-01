732aa21f6f552701d861d91341ab2de55ad0f10f
bird_d <relay198@gmail.com>
2024-03-01 03:32:36 -0500

    UI: Add HeaderUI Adds a re-usable Header component to keep things consistent and easily change-able across all dock-panels that use it.

---
+++ src/controls/widgets/header/HeaderUI.tsx
@@ -0,0 +1,24 @@
+import { observer } from 'mobx-react-lite'
+import { ReactNode } from 'react'
+
+/** Re-usable Dock-Panel Header, gives a `bg-base-300` bar with a horizontal flex to put widgets in.
+ *
+ * Example:
+ *
+ * ```
+ * <HeaderUI>
+ *      <div tw='btn btn-sm'>Hello World!<div>
+ * </HeaderUI>
+ * ```
+ */
+export const HeaderUI = observer(function HeaderUI_(p: { children?: ReactNode }) {
+    return (
+        <div // Container
+            // This may be good to pass in the future? I don't think this really needs to/should be modifiable though.
+            // className={p.className}
+            tw={['h-10', 'flex select-none w-full h-full', 'items-center p-1', 'bg-base-300']}
+        >
+            {p.children ?? <></>}
+        </div>
+    )
+})
