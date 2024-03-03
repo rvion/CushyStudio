ca157eddce5575e48d70c3d932b18fa29b1e3b20
rvion <vion.remi@gmail.com>
2024-03-03 09:18:14 +0100

    hashBlob: prefer hex (base64 may end in =, which is invalid on some OSes)

--- src/state/hashBlob.ts
+++ src/state/hashBlob.ts
@@ -2,10 +2,10 @@ import crypto from 'crypto'
 
 export const hashBlob = async (blob: Blob): Promise<string> => {
     const buffer = await blob.arrayBuffer()
-    return crypto.createHash('sha1').update(Buffer.from(buffer)).digest('base64')
+    return crypto.createHash('sha1').update(Buffer.from(buffer)).digest('hex')
 }
 
 export const hashArrayBuffer = (buffer: ArrayBuffer): string => {
     const data = Buffer.from(buffer)
-    return crypto.createHash('sha1').update(data).digest('base64')
+    return crypto.createHash('sha1').update(data).digest('hex')
 }
