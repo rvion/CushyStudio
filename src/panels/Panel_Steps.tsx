990bad96362d3e492a46e74fc3571a145e4703b5
bird_d <relay198@gmail.com>
2024-03-06 03:49:13 -0500

    Panel_Steps Update Panel_HeaderUI

--- src/panels/Panel_Steps.tsx
+++ src/panels/Panel_Steps.tsx
@@ -4,26 +4,34 @@ import { StepOutputsBodyV1UI, StepOutputsV1UI } from '../outputs/StepOutputsV1UI
 import { useSt } from '../state/stateContext'
 import { PanelHeaderUI } from './PanelHeader'
 import { InputNumberUI } from 'src/controls/widgets/number/InputNumberUI'
-import { Slider } from 'src/rsuite/shims'
+import { SeparatorUI } from 'src/controls/widgets/separator/SeparatorUI'
+import { RevealUI } from 'src/rsuite/reveal/RevealUI'
 
 export const Panel_Steps = observer(function StepListUI_(p: {}) {
     const st = useSt()
     const steps = st.db.steps.getLastN(st.__TEMPT__maxStepsToShow)
     return (
-        <div className='flex flex-col'>
+        <div className='flex flex-col overflow-hidden'>
             <PanelHeaderUI>
-                <div className='line'>
-                    <div>number of items to display</div>
-                    <InputNumberUI
-                        mode='int'
-                        min={1}
-                        max={100}
-                        softMax={20}
-                        step={1}
-                        value={st.__TEMPT__maxStepsToShow}
-                        onValueChange={(next) => (st.__TEMPT__maxStepsToShow = next)}
-                    />
-                </div>
+                <SeparatorUI />
+                <RevealUI title='Step Options'>
+                    <div tw='flex px-1 cursor-default bg-base-200 rounded w-full h-full items-center justify-center hover:brightness-125 border border-base-100'>
+                        <span className='material-symbols-outlined'>settings</span>
+                        <span className='material-symbols-outlined'>expand_more</span>
+                    </div>
+                    <div tw='w-64 p-2' className='line'>
+                        <InputNumberUI
+                            mode='int'
+                            min={1}
+                            max={100}
+                            softMax={20}
+                            step={1}
+                            text='Items'
+                            value={st.__TEMPT__maxStepsToShow}
+                            onValueChange={(next) => (st.__TEMPT__maxStepsToShow = next)}
+                        />
+                    </div>
+                </RevealUI>
             </PanelHeaderUI>
             <div className='flex flex-col flex-grow' style={{ overflow: 'auto' }}>
                 {steps.map((step) => (
