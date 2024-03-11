3ed5a9978a02d958bbd533e81293033fc2e5b678
Globidev <globi@locomotive.eu>
2024-03-11 14:45:55 +0100

    backport some lococharts improvements

--- src/panels/Panel_Playground/charts/LocoChartsUI.tsx
+++ src/panels/Panel_Playground/charts/LocoChartsUI.tsx
@@ -1,21 +1,30 @@
+import type { CSSProperties } from 'react'
+
 import ReactEChartsCore from 'echarts-for-react/lib/core'
 import { observer } from 'mobx-react-lite'
 
 import { locoCharts, type LocoChartsOpts } from './locoCharts'
-import { nanoid } from 'nanoid'
+
+const ReactEChartsCore_Patched =
+    'default' in ReactEChartsCore ? (ReactEChartsCore.default as typeof ReactEChartsCore) : ReactEChartsCore
 
 type LocoChartProps = {
     options: LocoChartsOpts
     theme?: string
+    dev?: boolean
+    style?: CSSProperties
+    className?: string
 }
 
 export const LocoChartUI = observer(function LocoChartUI_(p: LocoChartProps) {
     return (
-        <ReactEChartsCore //
+        <ReactEChartsCore_Patched //
+            style={p.style}
+            className={p.className}
             echarts={locoCharts}
             theme={p.theme}
             option={p.options}
-            style={{ height: '20rem', width: '100%' }}
+            notMerge={p.dev} // Force re-evaluation of all options when in dev mode
         />
     )
 })
