097d0445fb7b8ad5f174bc2e6658bc5217ae238b
bird_d <relay198@gmail.com>
2024-03-12 03:16:53 -0400

    DraftIllustration: z-50 and add background Makes sure to cover content that could be there. Transparent images will be on top of a bg instead of underlying images.

--- src/cards/fancycard/DraftIllustration.tsx
+++ src/cards/fancycard/DraftIllustration.tsx
@@ -50,7 +50,7 @@ export const DraftIllustrationUI = observer(function DraftIllustrationUI_(p: {
     return (
         <div style={dropStyle} ref={dropRef} className='relative DROP_IMAGE_HANDLER'>
             {p.revealAppIllustrationOnHover ? ( //
-                <div tw='absolute opacity-0 hover:opacity-100' style={{ transition: 'opacity 0.2s' }}>
+                <div tw='absolute opacity-0 hover:opacity-100 z-50 bg-neutral' style={{ transition: 'opacity 0.2s' }}>
                     <AppIllustrationUI app={draft.app} size={p.size} />
                 </div>
             ) : null}
