fe9ac1ef77ce02cfc8b20403a9b087ac01573d1e
GitWilburn <GitWilburn@dontsp.am>
2024-04-04 22:53:20 -0500

    fix ipAdapter standalone

--- library/built-in/_ipAdapter/prefab_ipAdapter_base_standalone.ts
+++ library/built-in/_ipAdapter/prefab_ipAdapter_base_standalone.ts
@@ -78,6 +78,7 @@ export const run_ipadapter_standalone = async (
         // noise: ui.settings.noise,
         start_at: ui.settings.startAtStepPercent,
         end_at: ui.settings.endAtStepPercent,
+        embeds_scaling: 'V only',
         // unfold_batch: ui.settings.unfold_batch,
     })._MODEL
 
