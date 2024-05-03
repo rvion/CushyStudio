9db04e1c20cfdba667662550bcc0e0842a0b879a
rvion <vion.remi@gmail.com>
2024-05-03 10:45:30 +0200

    fix os detection

--- src/app/shortcuts/META_NAME.ts
+++ src/app/shortcuts/META_NAME.ts
@@ -1,13 +1,15 @@
 import type { KeyName } from './CommandManager'
 import type { MouseEvent } from 'react'
 
-const platform = process.platform
+import { getOS } from './_OS'
 
-export const MOD_KEY = (platform === 'darwin' ? 'cmd' : 'ctrl') as KeyName
+const platform = getOS() // process.platform
 
-export const META_NAME = (platform === 'darwin' ? 'cmd' : 'win') as KeyName
+export const MOD_KEY = (platform === 'Mac' ? 'cmd' : 'ctrl') as KeyName
+
+export const META_NAME = (platform === 'Mac' ? 'cmd' : 'win') as KeyName
 
 export const hasMod = (ev: MouseEvent<any, any> | KeyboardEvent): boolean => {
-    if (platform === 'darwin') return ev.metaKey
+    if (platform === 'Mac') return ev.metaKey
     return ev.ctrlKey
 }
