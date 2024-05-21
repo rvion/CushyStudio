bf032e5f49153a04227b4e754cf43bab8bdeedbe
rvion <vion.remi@gmail.com>
2024-05-21 13:24:59 +0200

    merge applyRelative and autoContrast modules

--- src/theme/colorEngine/applyRelative.tsx
+++ src/theme/colorEngine/applyRelative.tsx
@@ -1,7 +1,6 @@
 import type { AbsoluteStyle, RelativeStyle } from './AbsoluteStyle'
 
 import { clamp } from '../../controls/widgets/list/clamp'
-import { autoContrast } from './autoContrast'
 
 export const applyRelative = (a: AbsoluteStyle, b: RelativeStyle): AbsoluteStyle => {
     // oklch(lightness chroma hue);
@@ -11,4 +10,15 @@ export const applyRelative = (a: AbsoluteStyle, b: RelativeStyle): AbsoluteStyle
 
     const hue = b.hue ?? a.hue + (b.hueShift ?? 0)
     return { type: 'absolute', lightness, chroma, hue }
+}
+
+export function autoContrast(
+    //
+    lightness: number,
+    contrast: number,
+) {
+    /* This slightly favors using the darker color by adding a small float to ensure we always have -1/1 from Math.sign */
+    const start = lightness
+    const end = Math.round(lightness) - Math.sign(lightness - 0.5 + 0.00001)
+    return start * (1 - contrast) + end * contrast
 }
