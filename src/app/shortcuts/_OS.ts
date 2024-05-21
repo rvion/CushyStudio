99140b5527203a1e35e52ff4e63d2c446202d6d2
Globidev <globi@locomotive.eu>
2024-05-21 10:14:53 +0200

    make os detection work on platform without window

--- src/app/shortcuts/_OS.ts
+++ src/app/shortcuts/_OS.ts
@@ -11,12 +11,17 @@ export function getOS() {
 }
 
 function getOS_(): 'Mac' | 'iOS' | 'Windows' | 'Android' | 'Linux' | 'unknown' {
-    const userAgent = window.navigator.userAgent
-    const platform = window.navigator.platform
-    if (['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'].indexOf(platform) !== -1) return 'Mac'
-    else if (['iPhone', 'iPad', 'iPod'].indexOf(platform) !== -1) return 'iOS'
-    else if (['Win32', 'Win64', 'Windows', 'WinCE'].indexOf(platform) !== -1) return 'Windows'
-    else if (/Android/.test(userAgent)) return 'Android'
-    else if (/Linux/.test(platform)) return 'Linux'
-    return 'unknown'
+    try {
+
+        const userAgent = window.navigator.userAgent
+        const platform = window.navigator.platform
+        if (['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'].indexOf(platform) !== -1) return 'Mac'
+        else if (['iPhone', 'iPad', 'iPod'].indexOf(platform) !== -1) return 'iOS'
+        else if (['Win32', 'Win64', 'Windows', 'WinCE'].indexOf(platform) !== -1) return 'Windows'
+        else if (/Android/.test(userAgent)) return 'Android'
+        else if (/Linux/.test(platform)) return 'Linux'
+        return 'unknown'
+    } catch (e) {
+        return 'unknown'
+    }
 }
