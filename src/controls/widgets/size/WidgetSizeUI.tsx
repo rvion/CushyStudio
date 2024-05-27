015cb3fdb4ba514b0363267718d15e5c08924bcb
bird_d <relay198@gmail.com>
2024-05-27 00:15:28 -0400

    WidgetSizeUI: (wip) Use color engine + misc. fixes Replaces the aspect rotation button by making the aspect ratio visual clickable
    This patch breaks the aspect ratio visual

--- src/controls/widgets/size/WidgetSizeUI.tsx
+++ src/controls/widgets/size/WidgetSizeUI.tsx
@@ -6,6 +6,9 @@ import { observer } from 'mobx-react-lite'
 
 import { Box } from '../../../theme/colorEngine/Box'
 import { InputNumberUI } from '../number/InputNumberUI'
+import { Button } from '../../../rsuite/button/Button'
+import { Frame } from '../../../rsuite/button/Frame'
+import { InputBoolUI } from '../bool/InputBoolUI'
 
 export const WigetSize_BlockUI = observer(function WigetSize_BlockUI_(p: { widget: Widget_size }) {
     return <WigetSizeXUI sizeHelper={p.widget.sizeHelper} bounds={p.widget.config} />
@@ -21,23 +24,29 @@ export const WidgetSizeX_LineUI = observer(function WidgetSize_LineUI_(p: {
 }) {
     const uist = p.sizeHelper
 
+    const ratio = uist.width / uist.height
+    // const ratioIcon = ratio == 1.0 ? 'mdiApproximatelyEqual' : ratio > 1.0 ? 'mdiCropLandscape' : 'mdiCropPortrait'
+
     return (
         <div className='flex flex-1 flex-col gap-1'>
-            <div //Joined container
+            <Box //Joined container
+                // appearance='headless'
+                hover
+                border={{ contrast: 0.25 }}
                 tw={[
-                    'WIDGET-FIELD w-full h-full flex items-center join rounded gap-0.5 overflow-clip',
-                    'border border-base-100 border-b-base-200',
-                    'border-b-2 hover:border-base-200 hover:border-b-base-300',
+                    'WIDGET-FIELD w-full h-full flex items-center overflow-clip',
+                    // 'border border-base-100 border-b-base-200',
+                    // 'border-b-2 hover:border-base-200 hover:border-b-base-300',
                 ]}
+                style={{ padding: '0px' }}
             >
-                <AspectLockButtonUI sizeHelper={uist} />
                 <InputNumberUI
                     //
                     min={p.bounds?.min ?? 128}
                     max={p.bounds?.max ?? 4096}
                     step={p.bounds?.step ?? 32}
                     mode='int'
-                    tw='join-item !border-none'
+                    tw='!border-none'
                     value={uist.width}
                     hideSlider
                     onValueChange={(next) => uist.setWidth(next)}
@@ -45,22 +54,27 @@ export const WidgetSizeX_LineUI = observer(function WidgetSize_LineUI_(p: {
                     text='Width'
                     suffix='px'
                 />
+                <div tw='h-full' style={{ width: '1px' }} />
                 <InputNumberUI
                     //
-                    tw='join-item !border-none'
                     min={p.bounds?.min ?? 128}
                     max={p.bounds?.max ?? 4096}
                     step={p.bounds?.step ?? 32}
                     hideSlider
                     mode='int'
+                    tw='!border-none'
                     value={uist.height}
                     onValueChange={(next) => uist.setHeight(next)}
                     forceSnap={true}
                     text='Height'
                     suffix='px'
                 />
+                {/* <Button onClick={uist.flip} icon={ratioIcon} style={{ border: 'none', borderRadius: '0px' }} /> */}
+                <div tw='h-full' style={{ width: '1px' }} />
                 <AspectRatioSquareUI sizeHelper={uist} />
-            </div>
+                <div tw='h-full' style={{ width: '1px' }} />
+                <AspectLockButtonUI sizeHelper={uist} />
+            </Box>
             {/* <div tw='flex items-center gap-1'>
                 <div // Extra div because gap-1 will eat in to the child's width for SOME reason
                 ></div>
@@ -72,12 +86,11 @@ export const WidgetSizeX_LineUI = observer(function WidgetSize_LineUI_(p: {
 export const AspectLockButtonUI = observer(function AspectLockButtonUI_(p: { sizeHelper: ResolutionState }) {
     const uist = p.sizeHelper
     return (
-        <button // Aspect Lock button
-            tw={[
-                'btn btn-xs h-8 rounded-none join-item !border-none !outline-none',
-                uist.isAspectRatioLocked && 'bg-primary hover:bg-primary text-primary-content !border-none',
-            ]}
-            onClick={(ev) => {
+        <Frame // Aspect Lock button
+            active={uist.isAspectRatioLocked}
+            style={{ border: 'unset', borderRadius: '0px' }}
+            icon={uist.isAspectRatioLocked ? 'mdiLink' : 'mdiLinkOff'}
+            onMouseDown={(ev) => {
                 uist.isAspectRatioLocked = !uist.isAspectRatioLocked
                 if (!uist.isAspectRatioLocked) {
                     return
@@ -89,9 +102,7 @@ export const AspectLockButtonUI = observer(function AspectLockButtonUI_(p: { siz
                     uist.setWidth(uist.width)
                 }
             }}
-        >
-            <span className='material-symbols-outlined'>{uist.isAspectRatioLocked ? 'link' : 'link_off'}</span>
-        </button>
+        />
     )
 })
 
@@ -99,30 +110,35 @@ export const AspectRatioSquareUI = observer(function AspectRatioSquareUI_(p: { s
     const uist = p.sizeHelper
     const ratioDisplaySize = 26
     return (
-        <Box // Aspect ratio display background
-            border
+        <Frame // Aspect ratio display background
+            square
+            xs
             tw={[
                 //
-                'flex rounded-sm',
+                'flex',
                 'overflow-clip',
                 'items-center justify-center',
             ]}
-            style={{ width: `${ratioDisplaySize}px`, height: `${ratioDisplaySize}px` }}
+            // style={{ width: `${ratioDisplaySize}px`, height: `${ratioDisplaySize}px` }}
+            style={{ border: 'unset', borderRadius: '0px' }}
+            onClick={uist.flip}
         >
-            <Box // Aspect ratio display foreground
-                base={{ contrast: 0.5 }}
-                tw='relative'
+            <Frame
+                tw='!relative w-full h-full'
                 style={{
                     //
                     width: '100%',
                     height: '100%',
+                    borderRadius: '0px',
                     // Use transform here because it works with floats and will not cause popping/mis-alignments.
                     transform: `
-        scaleX(${uist.width < uist.height ? Math.round((uist.width / uist.height) * ratioDisplaySize) / ratioDisplaySize : '1'})
-        scaleY(${uist.height < uist.width ? Math.round((uist.height / uist.width) * ratioDisplaySize) / ratioDisplaySize : '1'})`,
+                 scaleX(${uist.width < uist.height ? Math.round((uist.width / uist.height) * ratioDisplaySize) / ratioDisplaySize : '1'})
+                 scaleY(${uist.height < uist.width ? Math.round((uist.height / uist.width) * ratioDisplaySize) / ratioDisplaySize : '1'})`,
                 }}
-            ></Box>
-        </Box>
+                // icon='mdiCheckboxBlank'
+                square
+            />
+        </Frame>
     )
 })
 
@@ -134,54 +150,59 @@ export const WigetSizeXUI = observer(function WigetSizeXUI_(p: {
     const uist = p.sizeHelper
     // if (!uist.isAspectRatioLocked) return null
     const resoBtn = (ar: AspectRatio) => (
-        <button
-            type='button'
-            tw={['btn btn-xs join-item', uist.desiredAspectRatio === ar ? 'btn-primary' : 'btn-ghost']}
-            onClick={() => uist.setAspectRatio(ar)}
-        >
-            {ar}
-        </button>
+        <InputBoolUI //
+            display='button'
+            active={uist.desiredAspectRatio == ar}
+            onValueChange={() => uist.setAspectRatio(ar)}
+            text={ar}
+        />
     )
 
     const modelBtn = (model: ModelType) => (
-        <button
-            type='button'
-            tw={['btn btn-xs join-item', uist.desiredModelType === model ? 'btn-primary' : 'btn-ghost']}
-            onClick={() => uist.setModelType(model)}
-        >
-            {model}
-        </button>
+        <InputBoolUI
+            //
+            display='button'
+            active={uist.desiredModelType == model}
+            onValueChange={() => uist.setModelType(model)}
+            text={model}
+        />
     )
 
+    const portrait = uist.height / uist.width > 1.0
+
     return (
-        <div className='flex flex-col gap-1 mt-0.5 rounded-b'>
-            <div tw='flex items-start gap-2'>
-                <div tw='join virtualBorder'>
+        <Box
+            border={{ contrast: uist.isAspectRatioLocked ? 0.0 : -0.05 }}
+            base={{ contrast: uist.isAspectRatioLocked ? 0.0 : -0.05 }}
+        >
+            <div tw='flex'>
+                <div tw='join'>
                     {modelBtn('1.5')}
                     {modelBtn('xl')}
                 </div>
-                <div tw='btn btn-xs' onClick={() => uist.flip()}>
-                    <span className='material-symbols-outlined'>rotate_right</span>
-                </div>
-                <div tw='ml-auto flex items-center gap-1'>
-                    <div tw='join virtualBorder'>{resoBtn('1:1')}</div>
+
+                {/* <div tw='btn btn-xs' onClick={() => uist.flip()}> */}
+                {/* <span className='material-symbols-outlined'>rotate_right</span> */}
+                {/* </div> */}
+                <div tw='ml-auto flex items-center gap-1.5'>
+                    <div tw='join'>{resoBtn('1:1')}</div>
                     {/* <div>|</div> */}
-                    <div tw='join virtualBorder'>
+                    <div tw='join'>
                         {resoBtn('16:9')}
                         {resoBtn('9:16')}
                     </div>
                     {/* <div>|</div> */}
-                    <div tw='join virtualBorder'>
+                    <div tw='join'>
                         {resoBtn('4:3')}
                         {resoBtn('3:4')}
                     </div>
                     {/* <div>|</div> */}
-                    <div tw='join virtualBorder'>
+                    <div tw='join'>
                         {resoBtn('3:2')}
                         {resoBtn('2:3')}
                     </div>
                 </div>
             </div>
-        </div>
+        </Box>
     )
 })
