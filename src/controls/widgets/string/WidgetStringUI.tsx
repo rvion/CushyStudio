622787f1f15f0c4f9702fe8a2414e5569ff25587
rvion <vion.remi@gmail.com>
2024-06-01 14:03:58 +0200

    fix bug when color === ''

--- src/controls/widgets/string/WidgetStringUI.tsx
+++ src/controls/widgets/string/WidgetStringUI.tsx
@@ -53,7 +53,7 @@ export const WidgetString_HeaderUI = observer(function WidgetStringUI_(p: { widg
         case 'color':
             inputTailwind = 'absolute w-full h-full !bg-transparent opacity-0 !p-0'
             visualHelper = (
-                <Frame tw='w-full h-full text-xs justify-between' base={val} text={{ contrast: 0.4 }}>
+                <Frame tw='w-full h-full text-xs justify-between' base={val ? val : undefined} text={{ contrast: 0.4 }}>
                     {/* <div>{val}</div> */}
                     <div>{getLCHFromStringAsString(val)}</div>
                 </Frame>
