3cc64ea822bf12564c053afa9ef22050f0a89f2a
rvion <vion.remi@gmail.com>
2024-06-01 23:09:51 +0200

    cleanup most of the legacy theming

--- src/theme/ThemeManager.ts
+++ src/theme/ThemeManager.ts
@@ -1,47 +1,10 @@
+// 🔴 LEGACY ; remove me
 import type { STATE } from '../state/state'
 
-import { makeAutoObservable } from 'mobx'
-
-// prettier-ignore
-export type ThemeName =
-    | 'light'
-    | 'light2'
-    | 'dark'
-    | 'dark2'
-    | 'dark3'
-    | 'cupcake'
-    | 'valentine'
-    | 'wireframe'
-    | 'forest'
-    | 'dracula'
-
+export type ThemeName = 'dark'
 export class ThemeManager {
-    themes: ThemeName[] = [
-        // dark themes
-        'dark',
-        'dark2',
-        'dark3',
-        'forest',
-        'dracula',
-        //
-        'light',
-        'light2',
-        'cupcake',
-        'valentine',
-        'wireframe',
-    ]
-    toggle() {
-        this.theme = this.theme === 'dark' ? 'light' : 'dark'
-    }
-    get theme(): ThemeName {
-        return this.st.configFile.value.theme ?? 'dark'
-    }
-    set theme(v: ThemeName) {
-        this.st.configFile.update({ theme: v })
-    }
-
-    constructor(public st: STATE) {
-        //
-        makeAutoObservable(this)
-    }
+    constructor(public st: STATE) {}
+    themes: ThemeName[] = ['dark']
+    get theme(): ThemeName { return 'dark' } // prettier-ignore
+    set theme(v: ThemeName) { } // prettier-ignore
 }
