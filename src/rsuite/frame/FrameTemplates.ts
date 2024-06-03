eab343248866bb103c49d4191816fe8d1fe3792f
rvion <vion.remi@gmail.com>
2024-06-03 21:35:18 +0200

    primary frames: lower contrast to .1, enough to make that visual distinct with extra chroma on top

--- src/rsuite/frame/FrameTemplates.ts
+++ src/rsuite/frame/FrameTemplates.ts
@@ -20,7 +20,7 @@ const frame_default: BoxNormalized = extractNormalizeBox({
 
 const frame_primary: BoxNormalized = extractNormalizeBox({
     textShadow: 100,
-    base: { contrast: 0.2, chroma: 0.1 },
+    base: { contrast: 0.1, chroma: 0.1 },
     border: 0.1,
 })
 
