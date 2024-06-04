554c823f7814eb8ee4bf3d6bfa79205deb35563b
rvion <vion.remi@gmail.com>
2024-06-04 09:07:36 +0200

    usePanel

--- src/panels/router/RenderPanelUI.tsx
+++ src/panels/router/RenderPanelUI.tsx
@@ -7,6 +7,7 @@ import { ErrorBoundaryUI } from '../../rsuite/errors/ErrorBoundaryUI'
 import { Frame } from '../../rsuite/frame/Frame'
 import { Message } from '../../rsuite/shims'
 import { PanelNames, panels } from './PANELS'
+import { panelContext } from './usePanel'
 
 export const RenderPanelUI = observer(function RenderPanelUI_(p: {
     //
@@ -34,17 +35,28 @@ export const RenderPanelUI = observer(function RenderPanelUI_(p: {
 
     const Component = panelDef.widget
     const panelID = p.node?.getId()
+
+    // 2024-06-03 not sure if that case can happen
+    if (panelID == null)
+        return (
+            <Message type='error' showIcon>
+                Panel have no ID
+            </Message>
+        )
+
     return (
         <ErrorBoundaryUI>
-            <Frame
-                //
-                tw='flex-1 h-full w-full overflow-scroll' // overflow-auto to only show scrollbar when needed
-                className={`Region-${panel}`}
-                data-panel-id={panelID}
-                id={panelID}
-            >
-                <Component {...panelProps} className='w-full h-full border-none' />
-            </Frame>
+            <panelContext.Provider value={{ id: panelID }}>
+                <Frame
+                    //
+                    tw='flex-1 h-full w-full overflow-scroll' // overflow-auto to only show scrollbar when needed
+                    className={`Region-${panel}`}
+                    data-panel-id={panelID}
+                    id={panelID}
+                >
+                    <Component {...panelProps} className='w-full h-full border-none' />
+                </Frame>
+            </panelContext.Provider>
         </ErrorBoundaryUI>
     )
 })
