c7bdad25cbe173f4b284cbfe0545c1bc1ffdf629
bird_d <relay198@gmail.com>
2024-06-10 16:28:41 -0400

    MenuComfyUI: Remove clutter, use new cdiNodes icon

--- src/appbar/MenuComfyUI.tsx
+++ src/appbar/MenuComfyUI.tsx
@@ -5,8 +5,9 @@ import { observer } from 'mobx-react-lite'
 import { openFolderInOS } from '../app/layout/openExternal'
 import { Button } from '../csuite/button/Button'
 import { Dropdown } from '../csuite/dropdown/Dropdown'
-import { MenuItem } from '../csuite/dropdown/MenuItem'
+import { MenuDivider, MenuItem } from '../csuite/dropdown/MenuItem'
 import { useSt } from '../state/stateContext'
+import { Ikon } from '../csuite/icons/iconHelpers'
 
 export const MenuComfyUI = observer(function MenuComfyUI_(p: {}) {
     const st = useSt()
@@ -22,22 +23,46 @@ export const MenuComfyUI = observer(function MenuComfyUI_(p: {}) {
                     <MenuItem
                         onClick={() => st.layout.FOCUS_OR_CREATE('ComfyUI', {})}
                         label='ComfyUI'
-                        icon={<span className='material-symbols-outlined text-cyan-400'>account_tree</span>}
+                        icon={<Ikon.cdiNodes size={'1rem'} />}
                     />
-                    <MenuItem
+                    <MenuItem //
                         onClick={() => st.layout.FOCUS_OR_CREATE('ComfyUINodeExplorer', {})}
-                        icon={<span className='material-symbols-outlined text-cyan-400'>explore</span>}
                         label='Nodes Explorer'
                     />
-                    {/* {Boolean(st.configFile.value.comfyUIHosts?.length) ? null : (
-                        <MenuItem
-                            onClick={() => st.layout.FOCUS_OR_CREATE('Hosts', {})}
-                            icon={<span className='material-symbols-outlined text-cyan-400'>settings</span>}
-                            label='ComfyUI Hosts'
-                        />
-                    )} */}
-                    <HostMenuItemUI host={st.mainHost} showID />
-                    <div className='divider'>All hosts</div>
+                    <MenuDivider>
+                        <div
+                            tw='flex' // TODO(bird_d: JOINER)
+                        >
+                            <Button
+                                icon='mdiClipboard'
+                                onClick={() => {
+                                    navigator.clipboard.writeText(cushy.configFile.value.mainComfyHostID ?? '')
+                                }}
+                            >
+                                Primary Host
+                            </Button>
+                            <Button
+                                icon='mdiFolderOpen'
+                                onClick={(ev) => {
+                                    ev.stopPropagation()
+                                    ev.preventDefault()
+                                    return openFolderInOS(
+                                        `${cushy.rootPath}/schema/hosts/${cushy.configFile.value.mainComfyHostID}` as AbsolutePath,
+                                    )
+                                }}
+                            />
+                        </div>
+                    </MenuDivider>
+                    <HostMenuItemUI host={st.mainHost} />
+                    <MenuDivider>
+                        <Button //
+                            subtle
+                            onClick={() => cushy.layout.FOCUS_OR_CREATE('Hosts', {})}
+                            icon='mdiOpenInApp'
+                        >
+                            Hosts
+                        </Button>
+                    </MenuDivider>
                     {st.hosts.map((host) => {
                         return <HostMenuItemUI key={host.id} host={host} />
                     })}
@@ -47,67 +72,24 @@ export const MenuComfyUI = observer(function MenuComfyUI_(p: {}) {
     )
 })
 
-export const HostMenuItemUI = observer(function HostMenuItemUI_(p: { host: HostL; showID?: boolean }) {
+export const HostMenuItemUI = observer(function HostMenuItemUI_(p: { host: HostL }) {
     const host = p.host
     const isMain = host.id === cushy.configFile.value.mainComfyHostID
     return (
-        <MenuItem
-            icon={
-                <span
-                    tw={[isMain && (cushy.ws?.isOpen ?? false ? 'text-green-500' : 'text-red-500')]}
-                    className='material-symbols-outlined'
-                >
-                    desktop_mac
-                </span>
-            }
+        <MenuItem //
+            icon={isMain ? <Ikon.mdiServerNetwork /> : null}
             onClick={() => host.electAsPrimary()}
         >
-            <div tw='flex-grow'>
-                <div>{host.data.name}</div>
-                {p.showID && (
-                    <div tw='opacity-50 text-xs'>
-                        {host.id}
-                        <Button
-                            onClick={(ev) => {
-                                ev.stopPropagation()
-                                ev.preventDefault()
-                                return openFolderInOS(`${cushy.rootPath}/schema/hosts/${host.id}` as AbsolutePath)
-                            }}
-                        >
-                            <span className='material-symbols-outlined'>open_in_new</span>
-                        </Button>
-                    </div>
-                )}
-            </div>
-            <div className='join'>
-                <Button
-                    subtle
-                    icon='mdiOpenInApp'
-                    onClick={(ev) => {
-                        ev.preventDefault()
-                        ev.stopPropagation()
-                        cushy.layout.FOCUS_OR_CREATE('Hosts', {})
-                    }}
-                />
-                <Button
-                    subtle
-                    icon='mdiOpenInNew'
-                    onClick={(ev) => {
-                        ev.preventDefault()
-                        ev.stopPropagation()
-                        cushy.layout.FOCUS_OR_CREATE('ComfyUI', {})
-                    }}
-                />
-                <Button
-                    subtle
-                    icon='mdiFullscreen'
-                    onClick={(ev) => {
-                        ev.preventDefault()
-                        ev.stopPropagation()
-                        cushy.layout.FOCUS_OR_CREATE('ComfyUI', {}, 'full')
-                    }}
-                ></Button>
-            </div>
+            <div tw='flex-grow pr-3'>{host.data.name}</div>
+            <Button
+                subtle
+                icon='cdiNodes'
+                onClick={(ev) => {
+                    ev.preventDefault()
+                    ev.stopPropagation()
+                    cushy.layout.FOCUS_OR_CREATE('ComfyUI', {})
+                }}
+            />
         </MenuItem>
     )
 })
