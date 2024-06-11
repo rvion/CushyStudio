df67b34cdd3eb0bb4f6e662c09cd31cb29b4d194
bird_d <relay198@gmail.com>
2024-06-10 20:22:07 -0400

    AppBarUI: Make Cushy icon toggle FavBar

--- src/appbar/AppBarUI.tsx
+++ src/appbar/AppBarUI.tsx
@@ -13,19 +13,24 @@ import { MenuSettingsUI } from './MenuSettingsUI'
 import { PanelHeaderUI } from '../panels/PanelHeader'
 import { MenuWindowUI } from './MenuWindowUI'
 import { MenuEditUI } from './MenuEditUI'
+import { Button } from '../csuite/button/Button'
 
 export const AppBarUI = observer(function AppBarUI_(p: {}) {
     const mainHost = cushy.mainHost
     return (
         <Frame base={cushy.theme.value.appbar ?? { contrast: 0.3 }} tw='overflow-auto shrink-0' id='CushyAppBar'>
             <PanelHeaderUI tw='flex items-center px-2 overflow-auto'>
-                <img style={{ width: '1.6rem' }} src={assets.CushyLogo_512_png} alt='' />
+                <Button // TODO(bird_d): Toggle FavBar button here should be temporary, just to save space for now.
+                    square
+                    onClick={cmd_fav_toggleFavBar.execute}
+                >
+                    <img style={{ width: '1.3rem' }} src={assets.CushyLogo_512_png} alt='' />
+                </Button>
                 <div tw='px-1'>
                     <UpdateBtnUI updater={cushy.updater}>CushyStudio </UpdateBtnUI>
                 </div>
                 <MenuWindowUI />
                 <MenuEditUI />
-                <cmd_fav_toggleFavBar.NavBarBtnUI label='Favs' />
                 <MenuSettingsUI />
                 <MenuAboutUI />
                 {/* <MenuAuthUI /> */}
