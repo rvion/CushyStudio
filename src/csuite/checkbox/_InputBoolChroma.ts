64aefb479fa11974609731a887dc8be519a7b082
rvion <vion.remi@gmail.com>
2024-06-12 17:40:27 +0200

    misc

--- src/csuite/checkbox/_InputBoolChroma.ts
+++ src/csuite/checkbox/_InputBoolChroma.ts
@@ -1,3 +1,5 @@
+import type { Kolor } from '../kolor/Kolor'
+
 export const getInputBoolChroma = (isActive: boolean) => {
     // TODO: make that behind a theme color
     return isActive ? 0.04 : 0.01
@@ -5,4 +7,9 @@ export const getInputBoolChroma = (isActive: boolean) => {
 
 export const getInputBoolContrast = (isActive: boolean) => {
     return isActive ? 0.1 : 0.01
+}
+
+export const getBaseColor = (isActive: boolean): Kolor => {
+    if (isActive) return { contrast: 0.1, chroma: 0.04 }
+    return { contrast: 0.01, chroma: 0.01 }
 }
