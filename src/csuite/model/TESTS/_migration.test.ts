3f73c6f87d9c8a521a18f7084dc2421678a3b4fe
rvion <vion.remi@gmail.com>
2024-07-01 22:44:03 +0200

    more TODO

--- src/csuite/model/TESTS/_migration.test.ts
+++ src/csuite/model/TESTS/_migration.test.ts
@@ -4,12 +4,6 @@ import { simpleBuilder as b } from '../../index'
 
 // ------------------------------------------------------------------------------
 describe('auto-migration ', () => {
-    it('can recover from/to links', () => {
-        expect(0).toBe(1)
-        // 2024-07-02: TODO with GUI
-        // see `src/csuite/simple/SimpleSchema.ts`, near the `AUTOMIGRATION` section
-    })
-
     it('can recover from/to lists', () => {
         // E1 works
         const S1: S.SString = b.string({ default: '🔵' })
@@ -37,6 +31,42 @@ describe('auto-migration ', () => {
 
         // E3 is able to PRESERVE the '🟢' when schema has been stripped from list
         expect(E3.serial).toMatchObject({ root: { type: 'str', val: '🟢' } })
+    })
+
+    it('can recover from/to links', () => {
+        expect(0).toBe(1)
+        // 2024-07-02: TODO with GUI
+        // see `src/csuite/simple/SimpleSchema.ts`, near the `AUTOMIGRATION` section
+    })
+
+    it('can recover from/to lists + TEST PROXY SETTERS 🔥 ', () => {
+        // E1 works
+        const S1: S.SString = b.string({ default: '🔵' })
+        const E1 = S1.create()
+        expect(E1.value).toBe('🔵')
+
+        // set E1 value to '🟢'
+        E1.value = '🟢'
+        expect(E1.value).toBe('🟢')
+        expect(E1.serial).toMatchObject({ root: { type: 'str', val: '🟢' } })
+
+        // construct E2 from E1 serial, but with schema wrapped into list
+        const S2 = b.string().list()
+        const E2 = S2.create(() => E1.serial)
+
+        // E2 should able to PRESERVE the '🟢' when schema has been wrapped into list
+        expect(E2.value).toMatchObject(['🟢'])
+        E2.value[0] = '🔴'
+        expect(E2.serial).toMatchObject({ root: { type: 'list', items_: [{ type: 'str', val: '🔴' }] } })
+
+        // E1 should still have the same value, despite its serial having been used to create E2
+        expect(E1.serial).toMatchObject({ root: { type: 'str', val: '🔴' } })
+
+        // construct E3 from E2 serial, but with schema back to simple string (not in list anymore)
+        const E3 = S1.create(() => E2.serial)
+
+        // E3 is able to PRESERVE the '🔴' when schema has been stripped from list
+        expect(E3.serial).toMatchObject({ root: { type: 'str', val: '🔴' } })
     })
 })
 
