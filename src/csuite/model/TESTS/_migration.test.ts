25545bbc1f244d7f7aa687c6c405e5417b7dc34a
rvion <vion.remi@gmail.com>
2024-07-01 22:32:07 +0200

    better tests

--- src/csuite/model/TESTS/_migration.test.ts
+++ src/csuite/model/TESTS/_migration.test.ts
@@ -15,15 +15,16 @@ describe('can recover when field becoming list ', () => {
         expect(E1.value).toBe('🟢')
         expect(E1.serial).toMatchObject({ root: { type: 'str', val: '🟢' } })
 
-        // E2 is able to PRESERVE the '🟢'
+        // E2 is able to PRESERVE the '🟢' when schema has been wrapped into list
         const S2 = b.string().list()
         const E2 = S2.create(() => E1.serial)
         expect(E2.value).toMatchObject(['🟢'])
         expect(E2.serial).toMatchObject({ root: { type: 'list', items_: [{ type: 'str', val: '🟢' }] } })
 
-        // E1 should still have the same value
+        // E1 should still have the same value, despite its serial having been used to create E2
         expect(E1.serial).toMatchObject({ root: { type: 'str', val: '🟢' } })
 
+        // E3 is able to PRESERVE the '🟢' when schema has been stripped into list
         const E3 = S1.create(() => E2.serial)
         expect(E3.serial).toMatchObject({ root: { type: 'str', val: '🟢' } })
     })
