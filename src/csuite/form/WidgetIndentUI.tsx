dccea3ab9c158332e2418d4681445711e310fd71
rvion <vion.remi@gmail.com>
2024-07-01 12:26:22 +0200

    show indent guide

--- src/csuite/form/WidgetIndentUI.tsx
+++ src/csuite/form/WidgetIndentUI.tsx
@@ -3,15 +3,16 @@ import { observer } from 'mobx-react-lite'
 export const WidgetIndentUI = observer(function WidgetIndentUI_(p: { depth: number }) {
     const depth = p.depth
     if (depth - 1 <= 0) return null
+    // TODO: better values here
     return (
         <div
+            className='UI-WidgetIndent'
             style={{
-                height: '100%',
                 width: `${(depth - 1) * 1}rem`,
                 marginRight: '.2rem',
                 flexShrink: 0,
-                // background: 'red',
-                // borderRight: '1px solid black',
+                alignSelf: 'stretch',
+                borderRight: '1px solid oklch(from var(--KLR) calc(l + 0.2 * var(--DIR)) c h)',
             }}
         />
     )
