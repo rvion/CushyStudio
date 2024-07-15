2af7c3d73e8b0ee2c556605d7c1615c9c4c5c0db
rvion <vion.remi@gmail.com>
2024-07-15 21:36:05 +0200

    csuite > Factory > `Factory.use(...)` now dirrectly accept schemas (not just lambdas)

--- src/csuite/model/Factory.ts
+++ src/csuite/model/Factory.ts
@@ -79,11 +79,12 @@ export class Factory<BUILDER extends IBuilder = IBuilder> {
 
     /** simple way to defined forms and in react components */
     use<SCHEMA extends BaseSchema>(
-        schemaExt: (form: BUILDER) => SCHEMA,
+        schemaExt: SCHEMA | ((form: BUILDER) => SCHEMA),
         entityConfig: EntityConfig<NoInfer<SCHEMA>> = {},
         deps: DependencyList = [],
     ): SCHEMA['$Field'] {
-        return useMemo(() => this.entity(schemaExt, entityConfig), deps)
+        const schema: SCHEMA = this.evalSchema(schemaExt)
+        return useMemo(() => this.entity(schema, entityConfig), deps)
     }
 
     /** eval schema if it's a function */
