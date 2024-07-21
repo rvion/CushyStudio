a962b81da9dc57728febda737ef6b0ec8a61144d
rvion <vion.remi@gmail.com>
2024-07-21 22:53:25 +0200

    csuite > utils > getUIDForMemoryStructure : support null, undefined, and primitives (string, number, boolean)

--- src/csuite/utils/getUIDForMemoryStructure.tsx
+++ src/csuite/utils/getUIDForMemoryStructure.tsx
@@ -2,7 +2,14 @@ import { nanoid } from 'nanoid'
 
 const wm = new WeakMap()
 
-export function getUIDForMemoryStructure(x: object): string {
+export function getUIDForMemoryStructure(
+    //
+    x: object | Function | null | undefined | number | string | boolean,
+): string {
+    if (x == null) return 'null'
+    if (typeof x === 'number') return x.toString()
+    if (typeof x === 'string') return x
+    if (typeof x === 'boolean') return x.toString()
     const prev = wm.get(x)
     if (prev) return prev
     const uid = nanoid()
