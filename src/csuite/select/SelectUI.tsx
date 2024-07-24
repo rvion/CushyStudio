4151bb11d0bcb89d1f2126df9a3ae01739ea7260
rvion <vion.remi@gmail.com>
2024-07-25 00:54:16 +0200

    csuite > select > add back the previous grid-based layout to minimize UI items movement when select value container start wrapping
    (need to be seen to be understood; hard to explain that in a commit title)

--- src/csuite/select/SelectUI.tsx
+++ src/csuite/select/SelectUI.tsx
@@ -12,28 +12,28 @@ import { AutoCompleteSelectState } from './SelectState'
 import { SelectValueContainerUI } from './SelectValueContainerUI'
 
 export const SelectUI = observer(function SelectUI_<T>(p: SelectProps<T>) {
-    const s = useMemo(() => new AutoCompleteSelectState(/* st, */ p), [])
+    const select = useMemo(() => new AutoCompleteSelectState(/* st, */ p), [])
     const csuite = useCSuite()
     const border = csuite.inputBorder
     return (
         <RevealUI //
-            ref={s.revealStateRef}
+            ref={select.revealStateRef}
             trigger='pseudofocus'
             shell='popover'
             placement='autoVerticalStart'
             onHidden={() => {
-                s.revealState?.log(`🔶 revealUI - onHidden (focus anchor)`)
+                select.revealState?.log(`🔶 revealUI - onHidden (focus anchor)`)
                 // 🔴 should only focus anchor in certain cases?
                 // (ex: escape while in popup should probably focus the anchor?)
                 // (ex: clicking outside the popup should probably focus the anchor?)
                 // (ex: tab should probably go to the next select, NOT focus this anchor?)
                 // (ex: programmatically or whatever random reason closes the select, should NOT focus the anchor?)
                 // ...but this can probably be improved in a future release
-                s.anchorRef.current?.focus()
-                s.clean()
+                select.anchorRef.current?.focus()
+                select.clean()
             }}
-            content={({ reveal }) => <SelectPopupUI reveal={reveal} selectState={s} />}
-            sharedAnchorRef={s.anchorRef}
+            content={({ reveal }) => <SelectPopupUI reveal={reveal} selectState={select} />}
+            sharedAnchorRef={select.anchorRef}
         >
             <Frame
                 expand
@@ -46,35 +46,44 @@ export const SelectUI = observer(function SelectUI_<T>(p: SelectProps<T>) {
                 base={{ contrast: csuite.inputContrast ?? 0.05 }}
                 onKeyDown={(ev) => {
                     // 🔶 note: the anchor gets all keyboard events even when input inside popup via portal is focused!
-                    s.handleTooltipKeyDown(ev)
-                    s.revealState?.onAnchorKeyDown(ev)
+                    select.handleTooltipKeyDown(ev)
+                    select.revealState?.onAnchorKeyDown(ev)
                 }}
                 onFocus={(ev) => {
-                    s.revealState?.log(`🔶 revealUI - onFocus`)
+                    select.revealState?.log(`🔶 revealUI - onFocus`)
                 }}
                 onBlur={(ev) => {
-                    s.revealState?.log(`🔶 revealUI - onBlur`)
+                    select.revealState?.log(`🔶 revealUI - onBlur`)
                 }}
             >
-                <Ikon.mdiTextBoxSearchOutline //
-                    tw='box-border ml-[5px] mr-[2px]'
-                    size='calc((var(--input-height) - 4px - 4px)' // 2px for parent border + 2 * 2px for icon padding
+                {WRAP_SHOULD_NOT_IMPACT_ICONS ? (
+                    // IN THIS BRANCH, LAYOUT IS DONE VIA GRID
+                    <div tw={['w-full', 'px-0.5', 'grid']} style={{ gridTemplateColumns: '24px 1fr 24px' }}>
+                        {/* 2px for parent border + 2 * 2px for icon padding */}
+                        <Ikon.mdiTextBoxSearchOutline tw='box-border m-[2px]' size='calc((var(--input-height) - 4px - 2px)' />
+                        <SelectValueContainerUI wrap={select.p.wrap ?? true}>{select.displayValue}</SelectValueContainerUI>
+                        <Ikon.mdiChevronDown tw='box-border m-[2px]' size='calc((var(--input-height) - 4px - 2px)' />
+                    </div>
+                ) : (
+                    // IN THIS BRANCH, WE ADD FLEX-NONE
+                    <>
+                        <Ikon.mdiTextBoxSearchOutline
+                            tw='box-border m-[2px] flex-none'
+                            size='calc((var(--input-height) - 4px - 2px)'
                 />
-
-                <SelectValueContainerUI // list of currently selected values
-                    wrap={s.p.wrap}
-                >
-                    {s.displayValue}
-                </SelectValueContainerUI>
-
-                <Ikon.mdiChevronDown // 'v' caret to indicate this is a select
-                    tw='box-border ml-auto m-[2px]'
+                        <SelectValueContainerUI wrap={select.p.wrap ?? true}>{select.displayValue}</SelectValueContainerUI>
+                        <Ikon.mdiChevronDown
+                            tw='flex-none box-border ml-auto m-[2px]'
                     size='calc((var(--input-height) - 4px - 4px)'
                 />
+                    </>
+                )}
             </Frame>
         </RevealUI>
     )
 })
+
+const WRAP_SHOULD_NOT_IMPACT_ICONS = true
 
 // HERE
 // onMouseDown={s.onRootMouseDown}
@@ -86,33 +95,3 @@ export const SelectUI = observer(function SelectUI_<T>(p: SelectProps<T>) {
 //         s.openMenu()
 //     }
 // }}
-/*
-🔴🔴🔴🔴🔴🔴🔴🔴 THE OLD CODE WAS BETTER; GRID WAS MAKING THE
-🔴🔴🔴🔴🔴🔴🔴🔴 START AND END ICONS not move when input was growing
-🔴🔴🔴🔴🔴🔴🔴🔴 to be multiple line heights
-<div // ANCHOR
-        tabIndex={-1}
-        tw={['text-sm', 'flex gap-1', 'p-0 h-full bg-transparent', 'select-none overflow-clip']}
-    >
-    <div tw={['w-full', 'px-0.5', 'grid']} style={{ gridTemplateColumns: '24px 1fr 24px' }}>
-        <Ikon.mdiTextBoxSearchOutline //
-            tw='box-border m-[2px]' // 2px for parent border + 2 * 2px for icon padding
-            size='calc((var(--input-height) - 4px - 2px)'
-        />
-        <div
-            tw={[
-                'flex gap-0.5 flex-grow items-center lh-input-2 ',
-                p.wrap //
-                    ? 'flex-wrap'
-                    : 'overflow-hidden line-clamp-1 text-ellipsis whitespace-nowrap',
-            ]}
-        >
-            {s.displayValue}
-        </div>
-        <Ikon.mdiChevronDown //
-            tw='box-border m-[2px]'
-            size='calc((var(--input-height) - 4px - 2px)'
-        />
-    </div>
-</div>
- */
--- src/csuite/select/TODO-select.md
+++ src/csuite/select/TODO-select.md
@@ -8,4 +8,4 @@ TODO:
 - [ ] hovering the select anchor is applying hover effect on the popup, it should not
 - [ ] adding enough items to cause the select to wrap is not properly updating the associated reveal position => it should go down a bit
 - [ ] single select: picking an item shouldn't close the popup => the MOUSE UP should. otherwise, drag & slide is broken
-- [ ] put back the old select anchor code (using grid layout to avoid icons to be stretched)
+- [🟢] put back the old select anchor code (using grid layout to avoid icons to be stretched)
