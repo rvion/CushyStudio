aa3493aaf6d64fc896ba53fe29a8fd30353a5e18
rvion <vion.remi@gmail.com>
2024-07-24 16:36:17 +0200

    csuite > InputString: forwardRef + onBlur option

--- src/csuite/input-string/InputStringUI.tsx
+++ src/csuite/input-string/InputStringUI.tsx
@@ -1,9 +1,9 @@
 import type { Field_string_config } from '../fields/string/FieldString'
 import type { IconName } from '../icons/icons'
-import type { CSSProperties } from 'react'
+import type { CSSProperties, ForwardedRef } from 'react'
 
 import { observer } from 'mobx-react-lite'
-import { ReactElement, useState } from 'react'
+import { forwardRef, ReactElement, useState } from 'react'
 
 import { Button } from '../button/Button'
 import { useCSuite } from '../ctx/useCSuite'
@@ -14,7 +14,7 @@ import { knownOKLCHHues } from '../tinyCSS/knownHues'
 
 type ClassLike = string | { [cls: string]: any } | null | undefined | boolean
 
-export const InputStringUI = observer(function WidgetStringUI_(p: {
+export type InputStringProps = {
     /** when true => 'mdiText' */
     icon?: IconName // | boolean | null | undefined
 
@@ -42,106 +42,114 @@ export const InputStringUI = observer(function WidgetStringUI_(p: {
     // styling -------------------
     className?: string
     style?: CSSProperties
-}) {
-    const value = p.getValue()
-    const isBuffered = Boolean(p.buffered)
-    const temporaryValue = p.buffered?.getTemporaryValue?.()
-    const isDirty = isBuffered && temporaryValue != null && temporaryValue !== value
 
-    const [reveal, setReveal] = useState(false)
-    let inputTailwind: string | ClassLike[] | undefined
-    let visualHelper: ReactElement<any, any> | undefined
+    onBlur?: (ev: React.FocusEvent<HTMLInputElement, Element>) => void
+}
 
-    switch (p.type) {
-        case 'color':
-            inputTailwind = 'absolute w-full h-full !bg-transparent opacity-0 !p-0'
-            visualHelper = (
-                <Frame //
-                    tw='w-full h-full flex items-center font-mono whitespace-nowrap text-[0.6rem]'
-                    base={value ? value : undefined}
-                    text={{ contrast: 1 }}
-                >
-                    {getLCHFromStringAsString(value)}
-                </Frame>
-            )
-            break
-        default:
-            inputTailwind = 'w-full h-full !outline-none bg-transparent'
-            break
-    }
-    const csuite = useCSuite()
-    return (
-        <Frame
-            base={{ contrast: csuite.inputContrast ?? 0.05 }}
-            text={{ contrast: 1, chromaBlend: 1 }}
-            hover
-            border={
-                isDirty //
-                    ? { contrast: 0.3, hue: knownOKLCHHues.warning, chroma: 0.2 }
-                    : { contrast: csuite.inputBorder }
-            }
-            tw={['h-input flex items-center relative text-sm rounded-sm']}
-            onMouseDown={(ev) => {
-                if (ev.button == 1) {
-                    const textInput = ev.currentTarget.querySelector('input[type="text"') as HTMLInputElement
-                    textInput.focus()
+export const InputStringUI = observer(
+    forwardRef(function WidgetStringUI_(p: InputStringProps, ref: ForwardedRef<HTMLInputElement>) {
+        const value = p.getValue()
+        const isBuffered = Boolean(p.buffered)
+        const temporaryValue = p.buffered?.getTemporaryValue?.()
+        const isDirty = isBuffered && temporaryValue != null && temporaryValue !== value
+
+        const [reveal, setReveal] = useState(false)
+        let inputTailwind: string | ClassLike[] | undefined
+        let visualHelper: ReactElement<any, any> | undefined
+
+        switch (p.type) {
+            case 'color':
+                inputTailwind = 'absolute w-full h-full !bg-transparent opacity-0 !p-0'
+                visualHelper = (
+                    <Frame //
+                        tw='w-full h-full flex items-center font-mono whitespace-nowrap text-[0.6rem]'
+                        base={value ? value : undefined}
+                        text={{ contrast: 1 }}
+                    >
+                        {getLCHFromStringAsString(value)}
+                    </Frame>
+                )
+                break
+            default:
+                inputTailwind = 'w-full h-full !outline-none bg-transparent'
+                break
+        }
+        const csuite = useCSuite()
+        return (
+            <Frame
+                base={{ contrast: csuite.inputContrast ?? 0.05 }}
+                text={{ contrast: 1, chromaBlend: 1 }}
+                hover
+                border={
+                    isDirty //
+                        ? { contrast: 0.3, hue: knownOKLCHHues.warning, chroma: 0.2 }
+                        : { contrast: csuite.inputBorder }
                 }
-            }}
-        >
-            {visualHelper}
-            {p.icon != null && (
-                <IkonOf //
-                    tw='mx-1'
-                    size='1.2rem'
-                    name={typeof p.icon === 'string' ? p.icon : 'mdiText'}
-                />
-            )}
-            <input
-                tw={['px-2', inputTailwind]}
-                type={reveal ? 'text' : p.type}
-                pattern={p.pattern}
-                placeholder={p.placeholder}
-                autoFocus={p.autofocus}
-                value={p.buffered ? temporaryValue ?? value : value}
-                onChange={(ev) => {
-                    if (p.buffered) p.buffered.setTemporaryValue(ev.target.value)
-                    else p.setValue(ev.currentTarget.value)
-                }}
-                /* Prevents drag n drop of selected text, so selecting is easier. */
-                onDragStart={(ev) => ev.preventDefault()}
-                onFocus={(ev) => {
-                    p.buffered?.setTemporaryValue(p.getValue() ?? '')
-                    ev.currentTarget.select()
-                }}
-                onBlur={() => {
-                    // need to be deferenced here because of how it's called in
-                    // the onKeyDown handler a few lines below
-                    const tempValue = p.buffered?.getTemporaryValue?.()
-                    if (tempValue != null) p.setValue(tempValue)
-                }}
-                onKeyDown={(ev) => {
-                    if (ev.key === 'Enter') {
-                        ev.currentTarget.blur()
-                    } else if (ev.key === 'Escape') {
-                        if (!p.buffered && temporaryValue) p.setValue(temporaryValue)
-                        p.buffered?.setTemporaryValue(null)
-                        ev.currentTarget.blur()
+                tw={['h-input flex items-center relative text-sm rounded-sm']}
+                onMouseDown={(ev) => {
+                    if (ev.button == 1) {
+                        const textInput = ev.currentTarget.querySelector('input[type="text"') as HTMLInputElement
+                        textInput.focus()
                     }
                 }}
-            />
-            {p.type === 'password' && (
-                <Button
-                    subtle
-                    borderless
-                    icon={reveal ? 'mdiEyeOff' : 'mdiEye'}
-                    onClick={() => setReveal(!reveal)}
-                    tw='mx-1 cursor-pointer'
-                    square
+            >
+                {visualHelper}
+                {p.icon != null && (
+                    <IkonOf //
+                        tw='mx-1'
+                        size='1.2rem'
+                        name={typeof p.icon === 'string' ? p.icon : 'mdiText'}
+                    />
+                )}
+                <input
+                    ref={ref}
+                    tw={['px-2', inputTailwind]}
+                    type={reveal ? 'text' : p.type}
+                    pattern={p.pattern}
+                    placeholder={p.placeholder}
+                    autoFocus={p.autofocus}
+                    value={p.buffered ? temporaryValue ?? value : value}
+                    onChange={(ev) => {
+                        if (p.buffered) p.buffered.setTemporaryValue(ev.target.value)
+                        else p.setValue(ev.currentTarget.value)
+                    }}
+                    /* Prevents drag n drop of selected text, so selecting is easier. */
+                    onDragStart={(ev) => ev.preventDefault()}
+                    onFocus={(ev) => {
+                        p.buffered?.setTemporaryValue(p.getValue() ?? '')
+                        ev.currentTarget.select()
+                    }}
+                    onBlur={(ev) => {
+                        // need to be deferenced here because of how it's called in
+                        // the onKeyDown handler a few lines below
+                        const tempValue = p.buffered?.getTemporaryValue?.()
+                        if (tempValue != null) p.setValue(tempValue)
+                        p.onBlur?.(ev)
+                    }}
+                    onKeyDown={(ev) => {
+                        if (ev.key === 'Enter') {
+                            ev.currentTarget.blur()
+                        } else if (ev.key === 'Escape') {
+                            if (!p.buffered && temporaryValue) p.setValue(temporaryValue)
+                            p.buffered?.setTemporaryValue(null)
+                            ev.currentTarget.blur()
+                        }
+                    }}
                 />
-            )}
-        </Frame>
-    )
-})
+                {p.type === 'password' && (
+                    <Button
+                        subtle
+                        borderless
+                        icon={reveal ? 'mdiEyeOff' : 'mdiEye'}
+                        onClick={() => setReveal(!reveal)}
+                        tw='mx-1 cursor-pointer'
+                        square
+                    />
+                )}
+            </Frame>
+        )
+    }),
+)
 
 // 1-a 2-a
 // 1-b 2-a
