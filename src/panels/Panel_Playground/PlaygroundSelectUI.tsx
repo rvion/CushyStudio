50a7c67054bebafff0e0227f55a4607cf2568e70
rvion <vion.remi@gmail.com>
2024-07-24 23:22:16 +0200

    select playground: add multi example

--- src/panels/Panel_Playground/PlaygroundSelectUI.tsx
+++ src/panels/Panel_Playground/PlaygroundSelectUI.tsx
@@ -12,30 +12,35 @@ export const PlaygroundSelectUI = observer(function PlaygroundSelectUI_(p: {}) {
     return (
         <ErrorBoundaryUI>
             <div tw='flex flex-col gap-1'>
-                <Frame line tw='m-1'>
-                    <Frame expand base={{ chroma: 0.1, hue: 40 }}>
-                        <SelectUI<string>
-                            value={() => 'test'}
-                            options={() => ['test', 'test2', 'test3']}
-                            getLabelText={(t) => t}
-                            onOptionToggled={() => {}}
-                        />
+                {[false, true].map((b) => (
+                    <Frame line tw='m-1' key={b.toString()}>
+                        <Frame expand base={{ chroma: 0.1, hue: 40 }}>
+                            <SelectUI<string>
+                                multiple={b}
+                                value={() => 'test'}
+                                options={() => ['test', 'test2', 'test3']}
+                                getLabelText={(t) => t}
+                                onOptionToggled={() => {}}
+                            />
+                        </Frame>
+                        <Frame expand base={{ chroma: 0.1, hue: 80 }}>
+                            <SelectUI<string>
+                                //
+                                multiple={b}
+                                value={() => uist2.value.selected}
+                                options={() => [ 'test', 'test2', 'test3', 'test4', 'test5', 'test6', 'test7', 'test8', 'test9', 'test10', 'test11', 'test12', 'test13', 'test14', 'test15', 'test16', 'test17', 'test18', 'test19', 'test20', 'test21', 'test22', 'test23', 'test24', 'test25', 'test26', 'test27', 'test28', 'test29', ]} // prettier-ignore
+                                onOptionToggled={(v) => (uist2.value.selected = v)}
+                                getLabelText={(v) => `🔶 ${v}`}
+                                getLabelUI={(v) => <BadgeUI autoHue>{v}</BadgeUI>}
+                                // disableLocalFiltering={false}
+                                // equalityCheck={(a, b) => a.id === b.id}
+                                // getSearchQuery={() => field.serial.query ?? ''}
+                                // setSearchQuery={(query) => (field.serial.query = query)}
+                            />
+                        </Frame>
                     </Frame>
-                    <Frame expand base={{ chroma: 0.1, hue: 80 }}>
-                        <SelectUI<string>
-                            //
-                            value={() => uist2.value.selected}
-                            options={() => [ 'test', 'test2', 'test3', 'test4', 'test5', 'test6', 'test7', 'test8', 'test9', 'test10', 'test11', 'test12', 'test13', 'test14', 'test15', 'test16', 'test17', 'test18', 'test19', 'test20', 'test21', 'test22', 'test23', 'test24', 'test25', 'test26', 'test27', 'test28', 'test29', ]} // prettier-ignore
-                            onOptionToggled={(v) => (uist2.value.selected = v)}
-                            getLabelText={(v) => `🔶 ${v}`}
-                            getLabelUI={(v) => <BadgeUI autoHue>{v}</BadgeUI>}
-                            // disableLocalFiltering={false}
-                            // equalityCheck={(a, b) => a.id === b.id}
-                            // getSearchQuery={() => field.serial.query ?? ''}
-                            // setSearchQuery={(query) => (field.serial.query = query)}
-                        />
-                    </Frame>
-                </Frame>
+                ))}
+
                 {cushy.forms
                     .fields((ui) => ({
                         test2: ui.selectMany({
