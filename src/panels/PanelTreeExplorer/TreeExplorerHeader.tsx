bc427b54bc313bae50688b1634f579f9306d76f4
rvion <vion.remi@gmail.com>
2024-07-28 20:52:09 +0200

    fixup

--- src/panels/PanelTreeExplorer/TreeExplorerHeader.tsx
+++ src/panels/PanelTreeExplorer/TreeExplorerHeader.tsx
@@ -2,7 +2,7 @@ import { observer } from 'mobx-react-lite'
 
 import { Button } from '../../csuite/button/Button'
 import { useSt } from '../../state/stateContext'
-import { IndexAllAppsBtnUI } from '../Panel_Welcome/LibraryHeaderUI'
+import { IndexAllAppsBtnUI } from '../PanelWelcome/LibraryHeaderUI'
 
 export const LibraryHeaderUI = observer(function LibraryHeaderUI_(p: {}) {
     const st = useSt()
