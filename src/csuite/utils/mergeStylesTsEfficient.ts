31ef6fe1bfab649757b7d9d18f02c525269b4100
rvion <vion.remi@gmail.com>
2024-08-06 22:49:42 +0200

    misc

---
+++ src/csuite/utils/mergeStylesTsEfficient.ts
@@ -0,0 +1,8 @@
+import type { any_TsEfficient } from './objectAssignTsEfficient'
+import type { CSSProperties } from 'react'
+
+export const mergeStylesTsEfficient = (
+    //
+    target: CSSProperties,
+    source?: CSSProperties | undefined,
+): CSSProperties => Object.assign(target as any_TsEfficient, source as any_TsEfficient)
