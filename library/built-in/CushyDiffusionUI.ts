ecbfe53be3003d16214fbf0937f8c7f7840b9a5e
rvion <vion.remi@gmail.com>
2024-09-29 00:00:55 +0200

    new UI_extra prefab

--- library/built-in/CushyDiffusionUI.ts
+++ library/built-in/CushyDiffusionUI.ts
@@ -1,21 +1,12 @@
 import { ui_cnet, type UI_cnet } from './_controlNet/prefab_cnet'
 import { ui_IPAdapterV2, type UI_IPAdapterV2 } from './_ipAdapter/prefab_ipAdapter_baseV2'
 import { ui_IPAdapterFaceIDV2, type UI_IPAdapterFaceIDV2 } from './_ipAdapter/prefab_ipAdapter_faceV2'
-import { ui_highresfix, type UI_HighResFix } from './_prefabs/_prefabs'
-import { ui_3dDisplacement, type UI_3dDisplacement } from './_prefabs/prefab_3dDisplacement'
-import { ui_refiners, type UI_Refiners } from './_prefabs/prefab_detailer'
 import { ui_latent_v3, type UI_LatentV3 } from './_prefabs/prefab_latent_v3'
-import { ui_mask, type UI_Mask } from './_prefabs/prefab_mask'
 import { ui_model, type UI_Model } from './_prefabs/prefab_model'
-import { ui_advancedPrompt, type UI_advancedPrompt } from './_prefabs/prefab_promptsWithButtons'
-import { ui_recursive, type UI_recursive } from './_prefabs/prefab_recursive'
-import { ui_regionalPrompting_v1, type UI_regionalPrompting_v1 } from './_prefabs/prefab_regionalPrompting_v1'
-import { ui_rembg_v1, type UI_rembg_v1 } from './_prefabs/prefab_rembg'
 import { ui_sampler_advanced, type UI_Sampler_Advanced } from './_prefabs/prefab_sampler_advanced'
-import { ui_upscaleWithModel } from './_prefabs/prefab_upscaleWithModel'
-import { ui_watermark_v1, type UI_watermark_v1 } from './_prefabs/prefab_watermark'
 import { ui_customSave, type UI_customSave } from './_prefabs/saveSmall'
 import { sampleNegative, samplePrompts } from './samplePrompts'
+import { extra, type UI_extra } from './UI_extra'
 
 export type CushyDiffusionUI_ = X.XGroup<{
     positive: X.XPrompt
@@ -67,60 +58,4 @@ export function CushyDiffusionUI(ui: X.Builder): CushyDiffusionUI_ {
         faceID: ui_IPAdapterFaceIDV2().optional(),
         extra: extra(ui),
     })
-}
-
-// ================================================================================
-
-export type UI_extra = X.XChoices<{
-    show3d: UI_3dDisplacement
-    regionalPrompt: UI_regionalPrompting_v1
-    removeBG: UI_rembg_v1
-    mask: UI_Mask
-    highResFix: UI_HighResFix
-    upscaleWithModel: X.XGroup<{ model: X.XEnum<Enum_UpscaleModelLoader_model_name> }>
-    refine: UI_Refiners
-    reversePositiveAndNegative: X.XEmpty
-    makeAVideo: X.XEmpty
-    summary: X.XEmpty
-    gaussianSplat: X.XEmpty
-    promtPlus: UI_advancedPrompt
-    displayAsBeerCan: X.XEmpty
-    displayAsSpriteSheet: X.XEmpty
-    recursiveImgToImg: UI_recursive
-    watermark: UI_watermark_v1
-    fancyWatermark: X.XEmpty
-}>
-
-function extra(ui: X.Builder): UI_extra {
-    return ui.choices(
-        {
-            show3d: ui_3dDisplacement(),
-            regionalPrompt: ui_regionalPrompting_v1(),
-            mask: ui_mask(),
-            removeBG: ui_rembg_v1(),
-            highResFix: ui_highresfix(),
-            upscaleWithModel: ui_upscaleWithModel(), //.withConfig({ label: 'Model' }),
-            refine: ui_refiners(),
-            reversePositiveAndNegative: ui.empty({
-                label: 'swap +/-',
-                tooltip: 'swap the positive and negative prompts. USE AT YOUR OWN RISK.',
-            }),
-            makeAVideo: ui.empty({
-                icon: 'mdiMessageVideo',
-                tooltip: 'generate a video from all the generated images in that step ',
-            }),
-            summary: ui.empty({
-                icon: 'mdiLanguageMarkdown',
-                tooltip: 'outputs a markdown summary about the execution and outputs',
-            }),
-            gaussianSplat: ui.empty({ icon: 'mdiDotsHexagon' }),
-            promtPlus: ui_advancedPrompt(),
-            displayAsBeerCan: ui.empty({ icon: 'mdiBeerOutline' }),
-            displayAsSpriteSheet: ui.empty({ icon: 'mdiMovie' }),
-            recursiveImgToImg: ui_recursive(),
-            watermark: ui_watermark_v1(),
-            fancyWatermark: ui.empty({ icon: 'mdiWatermark' }),
-        },
-        { appearance: 'tab', icon: 'mdiAlien' },
-    )
 }
