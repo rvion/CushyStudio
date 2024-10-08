7ea19ecb4d9fe34d157674e2c37b76207cf587c4
rvion <vion.remi@gmail.com>
2024-10-08 21:22:46 +0200

    icon/btn fix

--- src/csuite/errors/ErrorBoundaryFallback.tsx
+++ src/csuite/errors/ErrorBoundaryFallback.tsx
@@ -8,9 +8,13 @@ export const ErrorBoundaryFallback = (p: {
     return (
         <div role='alert'>
             <p tw='flex gap-2 items-center'>
-                <span onClick={() => p.resetErrorBoundary()} tw='btn btn-square btn-sm btn-error rounded'>
-                    <span className='material-symbols-outlined'>refresh</span>
-                </span>
+                <Button //
+                    icon='mdiRefresh'
+                    onClick={() => p.resetErrorBoundary()}
+                    size='sm'
+                    look='error'
+                    square
+                />
                 Something went wrong:
             </p>
             <pre style={{ color: 'red' }}>{p.error?.message}</pre>
