0d02eaf8539cc5f14b0500d350bd3345c6137105
bird_d <relay198@gmail.com>
2024-10-09 12:51:21 -0400

    FooterBar: Do not round the corners

--- src/app/layout/FooterBarUI.tsx
+++ src/app/layout/FooterBarUI.tsx
@@ -10,6 +10,7 @@ export const FooterBarUI = observer(function FooterBarUI_(p: {}) {
     return (
         <Frame
             //
+            border={0}
             base={cushy.theme.value.appbar ?? { contrast: -0.077 }}
             tw='flex items-center px-1 py-1'
         >
