af88a4111d2fc53174c6c358d2356c0384d8f630
rvion <vion.remi@gmail.com>
2024-10-12 23:35:16 +0200

    fix/cleanup BuilderNumberTypes

--- src/csuite/model/builders/BuilderNumberTypes.ts
+++ src/csuite/model/builders/BuilderNumberTypes.ts
@@ -10,21 +10,26 @@ interface SchemaAndAliasesᐸ_ᐳ extends HKT<FieldTypes> {
 export class BuilderNumber<Schemaᐸ_ᐳ extends SchemaAndAliasesᐸ_ᐳ> extends BaseBuilder<Schemaᐸ_ᐳ> {
     static fromSchemaClass = BaseBuilder.buildfromSchemaClass(BuilderNumber)
 
-    private _int(config: Field_number_config): Schemaᐸ_ᐳ['Number'] {
-        return this.buildSchema(Field_number, config)
-    }
-
+    // #region ints
     int_(config: Omit<Field_number_config, 'mode'> = {}): Schemaᐸ_ᐳ['Number'] {
-        return this._int({ mode: 'int', ...config })
+        return this._number({ mode: 'int', ...config })
     }
-
     int(config: Omit<Field_number_config, 'mode'> = {}): Schemaᐸ_ᐳ['Number'] {
-        return this.int_({ default: config.default ?? 0, ...config })
+        return this.int_({ default: this._autotDefault(config), ...config })
     }
 
+    // #region float
+    float(config: Omit<Field_number_config, 'mode'> = {}): Schemaᐸ_ᐳ['Number'] {
+        return this._number({ mode: 'float', default: this._autotDefault(config), ...config })
+    }
+    float_(config: Omit<Field_number_config, 'mode'> = {}): Schemaᐸ_ᐳ['Number'] {
+        return this._number({ mode: 'float', ...config })
+    }
+
+    // #region ratios
     /** [number variant] precent = mode=int, default=100, step=10, min=1, max=100, suffix='%', */
     percent(config: Omit<Field_number_config, 'mode'> = {}): Schemaᐸ_ᐳ['Number'] {
-        return this._int({
+        return this._number({
             mode: 'int',
             default: 100,
             step: 10,
@@ -35,11 +40,19 @@ export class BuilderNumber<Schemaᐸ_ᐳ extends SchemaAndAliasesᐸ_ᐳ> extend
         })
     }
 
-    float(config: Omit<Field_number_config, 'mode'> = {}): Schemaᐸ_ᐳ['Number'] {
-        return this._int({ mode: 'float', default: config.default ?? 0, ...config })
+    // #region numbers
+    number(config: Omit<Field_number_config, 'mode'> = {}): Schemaᐸ_ᐳ['Number'] {
+        return this._number({ mode: 'float', default: this._autotDefault(config), ...config })
+    }
+    number_(config: Omit<Field_number_config, 'mode'> = {}): Schemaᐸ_ᐳ['Number'] {
+        return this._number({ mode: 'float', ...config })
     }
 
-    number(config: Omit<Field_number_config, 'mode'> = {}): Schemaᐸ_ᐳ['Number'] {
-        return this._int({ mode: 'float', ...config })
+    // #region _utils
+    private _autotDefault(config: { min?: number; default?: number }): number {
+        return config.default ?? config.min ?? 0
+    }
+    private _number(config: Field_number_config): Schemaᐸ_ᐳ['Number'] {
+        return this.buildSchema(Field_number, config)
     }
 }
