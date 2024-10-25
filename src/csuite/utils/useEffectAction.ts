9bd705c4fe453d745454118ac88ccbe4dd311b2c
rvion <vion.remi@gmail.com>
2024-10-26 00:37:39 +0200

    cleanup types

--- src/csuite/utils/useEffectAction.ts
+++ src/csuite/utils/useEffectAction.ts
@@ -1,5 +1,7 @@
+import type { DependencyList } from 'react'
+
 import { runInAction } from 'mobx'
-import { type DependencyList, useEffect } from 'react'
+import { useEffect } from 'react'
 
 export function useEffectAction(effect: () => void | (() => void), deps: DependencyList): void {
    useEffect(() => runInAction(() => effect()), deps)
