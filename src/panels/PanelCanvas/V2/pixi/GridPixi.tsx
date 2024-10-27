c5b87d4f4f9e79744694f3f849c59f2a810fe12f
rvion <vion.remi@gmail.com>
2024-10-27 23:02:42 +0100

    remove deprecations

--- src/panels/PanelCanvas/V2/pixi/GridPixi.tsx
+++ src/panels/PanelCanvas/V2/pixi/GridPixi.tsx
@@ -1,11 +1,9 @@
-// import { Graphics } from '@pixi/react'
 import { extend } from '@pixi/react'
 import { observer } from 'mobx-react-lite'
 import { Graphics } from 'pixi.js'
 
 extend({ Graphics })
 export const GridPixi = observer(function PixiGridUI_(p: {
-   //
    x: number
    y: number
    height: number
@@ -13,25 +11,18 @@ export const GridPixi = observer(function PixiGridUI_(p: {
 }) {
    const cellW = p.width / 10
    const cellH = p.height / 10
-   // return null
    return (
       <pixiGraphics
-         // anchor={0.5}
          draw={(g) => {
-            // draw a white and black grid
-            g.lineStyle(0, 0x0, 0)
-            // g.setStrokeStyle(0)
             for (let x = 0; x < 10; x++) {
                for (let y = 0; y < 10; y++) {
-                  g.beginFill((x + y) % 2 ? 0x656565 : 0x333333)
-                  g.drawRect(
+                  g.rect(
                      //
                      p.x + x * cellW,
                      p.y + y * cellH,
                      cellW,
                      cellH,
-                  )
-                  g.endFill()
+                  ).fill((x + y) % 2 ? 0x656565 : 0x333333)
                }
             }
          }}
