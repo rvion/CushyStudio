e3c97a107a50f95dded672726f92a8029a073494
rvion <vion.remi@gmail.com>
2024-11-03 00:25:02 +0100

    remove abiliti to explicitely specify chanel ID

--- src/panels/PanelCaptioning/PanelCaptioningState.ts
+++ src/panels/PanelCaptioning/PanelCaptioningState.ts
@@ -1,5 +1,4 @@
 import type { CushySchemaBuilder } from '../../controls/Builder'
-import type { Field_list } from '../../csuite/fields/list/FieldList'
 
 import { clamp } from 'three/src/math/MathUtils'
 
@@ -8,8 +7,8 @@ import { Channel } from '../../csuite'
 export type CaptioningDocSchema = ReturnType<typeof captioningDocSchema>
 export type CaptioningDoc = CaptioningDocSchema['$Field']
 
-const chan = new Channel<number>('captioning.activeImage')
-const chan2 = new Channel<string[]>('captioning.activeImage')
+const chan = new Channel<number>()
+const chan2 = new Channel<string[]>()
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 export function captioningDocSchema(b: CushySchemaBuilder) {
@@ -22,19 +21,25 @@ export function captioningDocSchema(b: CushySchemaBuilder) {
          captions: b
             .string()
             .list()
-            .publish(chan, (self) => self.length), //  🟢 WORKS
-         // .publishValue(chan2), //                 🔴 DOESN'T (mobx issue)
+            .publish(chan, (self) => self.length) //  🟢 WORKS
+            .publishValue(chan2), //                  🔴 DOESN'T (mobx issue)
       }),
 
       activeCaption: b.fields({
          text: b.string(),
-         index: b.number().subscribe(chan, (captionsLen, self) => {
-            const nextValue = clamp(self.value, 0, captionsLen - 1)
-            self.value = nextValue
-         }),
-         // .subscribe(chan2, (captions, self) => {
-         //    console.log(`[🔴] OK`, captions)
-         // }),
+         index: b
+            .number()
+            .subscribe(chan, (captionsLen, self) => {
+               const nextValue = clamp(self.value, 0, captionsLen - 1)
+               // console.log(`[🤠] FUCKME`, {
+               //    captionsLen,
+               //    nextValue,
+               // })
+               self.value = nextValue
+            })
+            .subscribe(chan2, (captions, self) => {
+               console.log(`[🔴] OK`, captions)
+            }),
       }),
       activeGlobalCaption: b.group({
          items: {
