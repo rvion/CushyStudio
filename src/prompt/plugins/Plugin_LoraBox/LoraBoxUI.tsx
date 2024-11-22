785699c6e8daf8904b5c09d029f83bb3ff8c3292
rvion <vion.remi@gmail.com>
2024-11-22 23:28:07 +0100

    PluginLoraBox: fix hot-reload by splitting components

--- src/prompt/plugins/Plugin_LoraBox/LoraBoxUI.tsx
+++ src/prompt/plugins/Plugin_LoraBox/LoraBoxUI.tsx
@@ -1,71 +1,19 @@
-import type { Prompt_Lora, Prompt_WeightedExpression } from '../grammar/grammar.practical'
-import type { WidgetPromptUISt } from '../WidgetPromptUISt'
+import type { Prompt_Lora, Prompt_WeightedExpression } from '../../grammar/grammar.practical'
+import type { WidgetPromptUISt } from '../../WidgetPromptUISt'
 
 import { observer } from 'mobx-react-lite'
 
-import { openExternal } from '../../app/layout/openExternal'
-import { Button } from '../../csuite/button/Button'
-import { MenuDivider } from '../../csuite/dropdown/MenuDivider'
-import { Frame } from '../../csuite/frame/Frame'
-import { InputNumberUI } from '../../csuite/input-number/InputNumberUI'
-import { InputStringUI } from '../../csuite/input-string/InputStringUI'
-import { InputLegacy } from '../../csuite/inputs/shims'
-import { MessageErrorUI } from '../../csuite/messages/MessageErrorUI'
-import { SelectUI } from '../../csuite/select/SelectUI'
-import { useSt } from '../../state/stateContext'
+import { openExternal } from '../../../app/layout/openExternal'
+import { MessageErrorUI } from '../../../csuite'
+import { Button } from '../../../csuite/button/Button'
+import { MenuDivider } from '../../../csuite/dropdown/MenuDivider'
+import { Frame } from '../../../csuite/frame/Frame'
+import { InputNumberUI } from '../../../csuite/input-number/InputNumberUI'
+import { InputStringUI } from '../../../csuite/input-string/InputStringUI'
+import { SelectUI } from '../../../csuite/select/SelectUI'
+import { useSt } from '../../../state/stateContext'
 
-// TODO: Once it is possible to get the modifier key's states, holding shift when pressing the trash button should not trim whitespace/commas
-
-export const Plugin_LoraControlsUI = observer(function Plugin_LoraControlsUI_(p: {
-   //
-   uist: WidgetPromptUISt
-}) {
-   const uist = p.uist
-   if (uist.loras.length === 0) return null
-   return (
-      <>
-         {/* {uist.loras.length === 0 && <div tw='italic text-gray-500'>No loras in prompt</div>} */}
-         <div tw='flex flex-col gap-1 p-1'>
-            {uist.loras.map((loraASTNode, ix) => {
-               const weighted = loraASTNode.firstAncestor('WeightedExpression')
-               const isOnlyLora = weighted?.contentText == loraASTNode?.text
-
-               return (
-                  <LoraBoxUI //
-                     key={`${loraASTNode.name}-${ix}`}
-                     uist={uist}
-                     loraASTNode={loraASTNode}
-                     weightedASTNode={isOnlyLora ? weighted : null}
-                     onDelete={() => {
-                        let from = loraASTNode.from
-                        // Delete surrounding "()*val" if exists and only contains the lora, otherwise remove only lora
-                        if (weighted && isOnlyLora) {
-                           weighted.remove()
-                           from = weighted.from
-                        } else {
-                           loraASTNode.remove()
-                        }
-
-                        // Clean-up trailing white-space/commas
-                        const text = uist.text.slice(from)
-                        uist.editorView?.dispatch({
-                           changes: {
-                              //
-                              from,
-                              to: from + text.length - text.replace(/^[,\s]+/, '').length,
-                              insert: '',
-                           },
-                        })
-                     }}
-                  />
-               )
-            })}
-         </div>
-      </>
-   )
-})
-
-const LoraBoxUI = observer(function LoraBoxUI_(p: {
+export const LoraBoxUI = observer(function LoraBoxUI_(p: {
    //
    uist: WidgetPromptUISt
    loraASTNode: Prompt_Lora
@@ -116,13 +64,13 @@ const LoraBoxUI = observer(function LoraBoxUI_(p: {
             />
          </Frame>
          {/* {node.name}
-                <div tw='flex-0 flex-grow'></div>
-                <Button //
-                    size='xs'
-                    icon='mdiDeleteForever'
-                    onClick={p.onDelete}
-                    tw='h-full'
-                /> */}
+                   <div tw='flex-0 flex-grow'></div>
+                   <Button //
+                       size='xs'
+                       icon='mdiDeleteForever'
+                       onClick={p.onDelete}
+                       tw='h-full'
+                   /> */}
          <MenuDivider />
          {/* {node.name} */}
          <div tw='flex flex-col items-center gap-1 w-full flex-grow'>
