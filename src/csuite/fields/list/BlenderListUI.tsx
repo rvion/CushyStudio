1d44f51f5f6e4c51bba8a62b197bacb0c6ad0dd1
rvion <vion.remi@gmail.com>
2024-11-23 23:34:41 +0100

    blender list > proper delete

--- src/csuite/fields/list/BlenderListUI.tsx
+++ src/csuite/fields/list/BlenderListUI.tsx
@@ -2,6 +2,7 @@ import type { BaseSchema } from '../../model/BaseSchema'
 import type { Field_list } from './FieldList'
 import type { ReactNode } from 'react'
 
+import { runInAction } from 'mobx'
 import { observer, useLocalObservable } from 'mobx-react-lite'
 
 import { Button } from '../../button/Button'
@@ -53,11 +54,20 @@ export const BlenderListUI = observer(function BlenderListUI_<T extends Field_li
                   )
                })}
             </ResizableFrame>
-
             <div>
                {/* <ListButtonClearUI field={field} /> */}
                <ListButtonAddUI field={field} />
                <Button icon='mdiChevronDown'></Button>
+               <Button
+                  disabled={selectedChild == null}
+                  icon='mdiMinus'
+                  onClick={() =>
+                     runInAction(() => {
+                        if (x.selectedIx > 0) x.selectedIx--
+                        field.removeItemAt(x.selectedIx)
+                     })
+                  }
+               />
             </div>
          </Frame>
          <Frame base={10}>{selectedChild && <selectedChild.UI />}</Frame>
