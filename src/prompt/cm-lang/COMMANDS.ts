6744cf57eb3918f54004efa00cd89b7b87ec4812
bird_d <relay198@gmail.com>
2024-12-01 19:42:00 -0500

    Prompting: Add ability to contract (WeightedExpression:1.1)'s

--- src/prompt/cm-lang/COMMANDS.ts
+++ src/prompt/cm-lang/COMMANDS.ts
@@ -1,5 +1,5 @@
 import type { PromptLangNodeName } from '../grammar/grammar.types'
-import type { ChangeSpec, Extension } from '@codemirror/state'
+import type { ChangeSet, ChangeSpec, Extension } from '@codemirror/state'
 import type { EditorView } from '@codemirror/view'
 import type { SyntaxNode } from '@lezer/common'
 
@@ -56,9 +56,24 @@ export const PromptKeymap1 = (): Extension =>
          run: expandWeights(-1, ['WeightedExpression', 'Lora', 'Wildcard']),
       },
       {
-         key: 'shift-a-ArrowRight',
+         key: 'ctrl-a-ArrowRight',
          preventDefault: true,
          run: expandWeights(1, ['WeightedExpression', 'Lora', 'Wildcard']),
+      },
+      {
+         key: 'shift-a-ArrowDown',
+         preventDefault: true,
+         run: contractWeights(0, ['WeightedExpression', 'Lora', 'Wildcard']),
+      },
+      {
+         key: 'ctrl-a-ArrowLeft',
+         preventDefault: true,
+         run: contractWeights(-1, ['WeightedExpression', 'Lora', 'Wildcard']),
+      },
+      {
+         key: 'shift-a-ArrowRight',
+         preventDefault: true,
+         run: contractWeights(1, ['WeightedExpression', 'Lora', 'Wildcard']),
       },
       // { key: 'm-s-j', preventDefault: true, run: changeWeight(0.1, ['Lora', 'Wildcard']) },
       // { key: 'm-s-k', preventDefault: true, run: changeWeight(-0.1, ['Lora', 'Wildcard']) },
@@ -161,7 +176,6 @@ const expandWeights =
 
          // Token was not inside a WeightedExpression, so do nothing
          if (!a) {
-            console.log('[FD] "a" was null!')
             return true
          }
 
@@ -242,5 +256,138 @@ const growWeight = (view: EditorView, a: SyntaxNode, next: boolean): ChangeSpec 
       { from: a.from, to: a.from + 1, insert: '' },
       // Insert '(' in front of new place
       { from: token.from, to: token.from, insert: '(' },
+   ]
+}
+
+const contractWeights =
+   (direction: number, stopAt: PromptLangNodeName[]) =>
+   (view: EditorView): boolean => {
+      const ranges = view.state.selection.ranges
+      const tree = syntaxTree(view.state)
+
+      let index = 0
+      for (const r of ranges) {
+         // TODO(bird_d/prompting/logic): Make this find the first weighted expression inside the selected range and go from there.
+         // Do nothing if there is a selection for now
+         if (r.from != r.to) {
+            index++
+            continue
+         }
+
+         let a: SyntaxNode | null = $smartResolve(tree, r.to)
+
+         // Make sure we have the entire WeightedExpression group selected
+         while (a && a.name != 'WeightedExpression') {
+            a = a.parent
+         }
+
+         // Token was not inside a WeightedExpression, so do nothing
+         if (!a) {
+            return true
+         }
+
+         // Store changes to do at once later on so undo does not get extra steps
+         const changes: Array<ChangeSpec> = []
+
+         // Expand to the left
+         if (direction != 1) {
+            changes.push(shrinkWeight(view, a, false))
+         }
+
+         // Expand to the right
+         if (direction != -1) {
+            changes.push(shrinkWeight(view, a, true))
+         }
+
+         // Expand in both ways if not direction
+         view.dispatch({
+            changes,
+         })
+
+         // XXX: Not type-safe. There should always be a change, so it should be fine?
+         const position = changes[0] && (changes[0] as any)[1].from
+
+         view.dispatch({
+            selection: {
+               anchor: position,
+               head: position,
+            },
+         })
+
+         continue
+      }
+      return true
+   }
+
+const shrinkWeight = (view: EditorView, a: SyntaxNode, next: boolean): ChangeSpec => {
+   const tree = syntaxTree(view.state)
+   let token: SyntaxNode | undefined
+   let offset = a.to
+
+   // Find the token to expand the group to
+   while (token === undefined) {
+      if (a.name == 'WeightedExpression') {
+         let child: SyntaxNode | null | undefined = next ? a.firstChild : a.lastChild
+
+         while (child != null && child.name != 'Identifier') {
+            if (child && child.name == 'Content') {
+               const children = child.getChildren('Identifier')
+
+               if (children.length > 0) {
+                  child = children[next ? 0 : children.length - 1]
+                  if (child) {
+                     child = next ? child.nextSibling : child.prevSibling
+                  }
+                  break
+               }
+            }
+
+            // Go one over to shrink
+            child = next ? child.nextSibling : child.prevSibling
+         }
+
+         if (child == null) {
+            return { from: 0, to: 0, insert: '' }
+         }
+
+         token = child
+         break
+      }
+
+      // SAFEGUARD
+      if (++offset - a.to > 5000) {
+         break
+      }
+   }
+
+   if (!token) {
+      return { from: 0, to: 0, insert: '' }
+   }
+
+   const number = bang(a.node.lastChild, 'A')
+   if ((number.name as PromptLangNodeName) !== 'Number') {
+      throw new Error(`❌ Expected a number`)
+   }
+
+   const numberTxt = view.state.doc.sliceString(number.from, number.to)
+   const weight = parseFloat(numberTxt)
+
+   const text = view.state.doc.sliceString(token.from, token.to)
+   // Contract to the left
+   if (next) {
+      return [
+         // Remove '('
+         { from: a.from, to: a.from + 1, insert: '' },
+         // Insert '(' in front of new place
+         { from: token.from, to: token.from, insert: '(' },
+      ]
+   }
+
+   // Expand to the left
+   return [
+      // Remove weights
+      { from: number.from - 1, to: number.to + 1, insert: '' },
+      // Add weights back in
+      { from: token.to, to: token.to, insert: `:${weight})` },
    ]
 }
