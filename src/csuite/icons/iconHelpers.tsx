422ce85ac3919a6b3ab29d7213ef51d179d48fc8
rvion <vion.remi@gmail.com>
2024-12-07 11:49:44 +0100

    type annotations in iconHelpers

--- src/csuite/icons/iconHelpers.tsx
+++ src/csuite/icons/iconHelpers.tsx
@@ -6,6 +6,7 @@ import { allIcons, type IconName } from './icons'
 
 const Icon = IconImport.Icon
 
+// eslint-disable-next-line @typescript-eslint/consistent-type-imports
 type RawIconProps = import('@mdi/react/dist/IconProps.d.ts').IconProps
 type MyIconProps = Omit<RawIconProps, 'path'>
 
@@ -18,14 +19,16 @@ type MyIconProps = Omit<RawIconProps, 'path'>
 export const Ikon: {
    [key in keyof typeof allIcons]: FC<MyIconProps>
 } = new Proxy({} as any, {
-   get(target, key) {
+   get(target, key): React.FC<MyIconProps> {
       if (key in target) return target[key]
-      return (target[key] = (p: any) => <Icon path={(allIcons as any)[key]} size='1.1em' {...p} />)
+      return (target[key] = (p: any): JSX.Element => (
+         <Icon path={(allIcons as any)[key]} size='1.1em' {...p} />
+      ))
    },
 }) as any
 
 /** reexport Icon from `@mdi/react` and add siz='1.1em' */
-export const IkonOf = function IkonOf_({ name, size, ...p }: { name: IconName } & MyIconProps) {
+export const IkonOf = function IkonOf_({ name, size, ...p }: { name: IconName } & MyIconProps): JSX.Element {
    return (
       <Icon //
          path={(allIcons as any)[name]}
