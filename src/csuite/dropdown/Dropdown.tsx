d52cdcc81e7a6611251220e0ed575d0c14d74424
rvion <vion.remi@gmail.com>
2025-03-08 22:43:03 +0100

    salvage

--- src/csuite/dropdown/Dropdown.tsx
+++ src/csuite/dropdown/Dropdown.tsx
@@ -2,12 +2,12 @@ import type { IconName } from '../icons/icons'
 import type { Tint } from '../kolor/Tint'
 import type { ReactNode } from 'react'
 
-import { observer } from 'mobx-react-lite'
-
 import { Button } from '../button/Button'
 import { Frame } from '../frame/Frame'
 import { RevealUI } from '../reveal/RevealUI'
-import { MenuDivider, MenuItem } from './MenuItem'
+import { MenuDivider } from './MenuDivider'
+import { MenuItem } from './MenuItem'
+import { observerWC } from './observerWC'
 
 export type DropdownProps = {
    title: ReactNode
@@ -17,6 +17,7 @@ export type DropdownProps = {
    content?: () => ReactNode
    button?: ReactNode
    expand?: boolean
+   debugName?: string
 }
 
 /**
@@ -24,30 +25,31 @@ export type DropdownProps = {
  * to build menus
  * see modules like `src/appbar/MenuDebugUI.tsx`
  */
-export const _Dropdown = observer(function Dropdown(p: DropdownProps): React.JSX.Element {
-   return (
-      <RevealUI
-         tw={[p.className]}
-         hideTriggers={{ shellClick: true, backdropClick: true, escapeKey: true }}
-         content={() => <Frame tabIndex={0} tw='z-[1] flex flex-col' children={p.content?.()} />}
-         children={
-            p.button ?? (
-               <Button
-                  borderless
-                  subtle
-                  icon={p.startIcon}
-                  tabIndex={0}
-                  expand={p.expand}
-                  children={p.title}
-               />
-            )
-         }
-      />
-   )
-})
-
-export const Dropdown = Object.assign(_Dropdown, {
-   // name: 'BasicShelfUI',
-   Divider: MenuDivider,
-   Item: MenuItem,
-})
+export const Dropdown = observerWC(
+   function Dropdown_(p: DropdownProps): React.JSX.Element {
+      return (
+         <RevealUI
+            tw={[p.className]}
+            hideTriggers={{ shellClick: true, backdropClick: true, escapeKey: true }}
+            content={() => <Frame tabIndex={0} tw='z-[1] flex flex-col' children={p.content?.()} />}
+            debugName={p.debugName}
+            children={
+               p.button ?? (
+                  <Button
+                     borderless
+                     subtle
+                     icon={p.startIcon}
+                     tabIndex={0}
+                     expand={p.expand}
+                     children={p.title}
+                  />
+               )
+            }
+         />
+      )
+   },
+   {
+      Divider: MenuDivider,
+      Item: MenuItem,
+   },
+)
--- src/types/Misc.ts
+++ src/types/Misc.ts
@@ -3,3 +3,5 @@
  * Usage: PartialOmit<{ a: string, b: string }, 'a'> -> { a?: string, b: string }
  */
 export type PartialOmit<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>
+
+export type IsEqual<T, S> = [T] extends [S] ? ([S] extends [T] ? true : false) : false
