551ec0e1253e268991ee177b934f6ddfef00125e
rvion <vion.remi@gmail.com>
2025-03-15 23:11:15 +0100

    fix typo

--- src/csuite-cushy/shells/ShellCushy.tsx
+++ src/csuite-cushy/shells/ShellCushy.tsx
@@ -16,7 +16,7 @@ const CushyShellUI = observer(function CushySHell(
    p: CompiledRenderProps & {
       // card?: boolean
       border?: boolean
-      HEADER: React.React.JSX.Element
+      HEADER: React.JSX.Element
    },
 ) {
    const field = p.field
