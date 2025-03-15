4883ce8bd234a33229d6451a3e1870fb6ca111c1
rvion <vion.remi@gmail.com>
2025-03-15 19:15:37 +0100

    misc > convert PlaygroundForms (hacky conversion) to modern useClass syntax, with real classes

--- src/panels/PanelPlayground/PlaygroundForms.tsx
+++ src/panels/PanelPlayground/PlaygroundForms.tsx
@@ -2,64 +2,67 @@ import { observer } from 'mobx-react-lite'
 
 import { simpleFactory } from '../../csuite'
 import { ErrorBoundaryUI } from '../../csuite/errors/ErrorBoundaryUI'
+import { Field_group } from '../../csuite/fields/group/FieldGroup'
+import { Field_string } from '../../csuite/fields/string/FieldString'
 import { Frame } from '../../csuite/frame/Frame'
-
-//
 
 /** Freely modify this as you like, then pick the "Scratch Pad" option in the top left. Do not commit changes made to this. */
 export const PlaygroundForms = observer(function PlaygroundImportFromComfy_(p: {}) {
-   const field = simpleFactory.use((b) =>
-      b
-         .fields({
-            a: b.percent({ suffix: '% of banana' }),
-            b: b.string({}).useMixin((self) => ({
-               UIWithFancyBorder: (): React.JSX.Element => (
+   const field = simpleFactory.use((b) => {
+      const raw = b.fields({
+         a: b.percent({ suffix: '% of banana' }),
+         b: b.string({}).useClass(
+            class extends Field_string {
+               UIWithFancyBorder = (): React.JSX.Element => (
                   <Frame //
                      border={30}
                      tw='p-2'
-                     children={self.UI({ Shell: UY.Shell.HeaderOnly })}
+                     children={this.UI({ Shell: UY.Shell.HeaderOnly })}
                   />
-               ),
-               UIWithSuperFancyBorder: (): React.JSX.Element => (
-                  <Frame border={30} tw='p-8' children={<self.UI Shell={UY.Shell.HeaderOnly} />} />
-               ),
-               UIWithSuperFancyBorder2: (p: { size: 'big' | 'small' }): React.JSX.Element => (
-                  <Frame border={30} tw='p-8' children={<self.UI Shell={UY.Shell.HeaderOnly} />} />
-               ),
-            })),
-            d: b.fields({
-               d1: b.string(),
-               d2: b.string(),
-            }),
-         })
-         .useMixin((self) => {
-            return {
-               v1: (): React.JSX.Element => {
-                  const { a, b } = field.fields
-                  return (
-                     <ErrorBoundaryUI>
-                        <h3>play with forms</h3>
-                        <Frame border={20} tw='m-8'>
-                           <Frame base={10}>
-                              {field.type}({field._uid})
-                           </Frame>
-                           <Frame base={{ hueShift: 100 }}>
-                              <div>
-                                 <b.UIWithFancyBorder />
-                                 <b.UIWithSuperFancyBorder />
-                              </div>
-                           </Frame>
-                           <Frame base={{ hueShift: 200 }}>
-                              {b.type}({b.id})<div>{a.UI({ Shell: 'HeaderOnly' })}</div>
-                              <div>{b.UIWithFancyBorder()}</div>
-                           </Frame>
-                        </Frame>
-                     </ErrorBoundaryUI>
-                  )
-               },
-            }
+               )
+               UIWithSuperFancyBorder = (): React.JSX.Element => (
+                  <Frame border={30} tw='p-8' children={<this.UI Shell={UY.Shell.HeaderOnly} />} />
+               )
+               UIWithSuperFancyBorder2 = (p: { size: 'big' | 'small' }): React.JSX.Element => (
+                  <Frame border={30} tw='p-8' children={<this.UI Shell={UY.Shell.HeaderOnly} />} />
+               )
+            },
+            null,
+         ),
+         d: b.fields({
+            d1: b.string(),
+            d2: b.string(),
          }),
-   )
+      })
+      return raw.useClass(
+         class extends Field_group<(typeof raw)['$field']['$subfields']> {
+            v1 = (): React.JSX.Element => {
+               const { a, b } = field.fields
+               return (
+                  <ErrorBoundaryUI>
+                     <h3>play with forms</h3>
+                     <Frame border={20} tw='m-8'>
+                        <Frame base={10}>
+                           {field.type}({field._uid})
+                        </Frame>
+                        <Frame base={{ hueShift: 100 }}>
+                           <div>
+                              <b.UIWithFancyBorder />
+                              <b.UIWithSuperFancyBorder />
+                           </div>
+                        </Frame>
+                        <Frame base={{ hueShift: 200 }}>
+                           {b.type}({b._uid})<div>{a.UI({ Shell: 'HeaderOnly' })}</div>
+                           <div>{b.UIWithFancyBorder()}</div>
+                        </Frame>
+                     </Frame>
+                  </ErrorBoundaryUI>
+               )
+            }
+         },
+         null,
+      )
+   })
    return (
       <>
          {/*  */}
