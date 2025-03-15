1da6c66f87481bc103bcb3dbd8c17095a731cd17
rvion <vion.remi@gmail.com>
2025-03-15 20:22:23 +0100

    Field > remove one instance of interface merging : simpler to follow code this way

--- src/csuite/model/Field.tsx
+++ src/csuite/model/Field.tsx
@@ -118,28 +118,26 @@ export type FieldCtorProps_ALT<TYPES extends Field = any> = [
    serial?: TYPES['$serial'],
 ]
 
-export interface Field {
-   $type: CATALOG.AllFieldTypes
-   $ownConfig: unknown
-   $ownSerial: unknown
-
-   $serial: FieldSerialFor<this>
-   $config: FieldConfigFor<this>
-
-   $value: unknown
-   $setValue: unknown
-   $unchecked: unknown
-   $child: unknown
-   $opts: unknown
-   $ownPatch: Patch_Common<this['$type']>
-
-   // 2025-02-11 new addition
-   $Schema: CSchema<this>
-}
-
 type PathObject = [string, Maybe<PathObject>]
 
 export abstract class Field {
+   declare $type: CATALOG.AllFieldTypes
+   declare $ownConfig: unknown
+   declare $ownSerial: unknown
+
+   declare $serial: FieldSerialFor<this>
+   declare $config: FieldConfigFor<this>
+
+   declare $value: unknown
+   declare $setValue: unknown
+   declare $unchecked: unknown
+   declare $child: unknown
+   declare $opts: unknown
+   declare $ownPatch: Patch_Common<this['$type']>
+
+   // 2025-02-11 new addition
+   declare $Schema: CSchema<this>
+
    /**
     * @internal
     */
