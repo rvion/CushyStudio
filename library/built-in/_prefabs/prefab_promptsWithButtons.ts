bb3dc30d614120eed4e18100374949e25ef94b2e
rvion <vion.remi@gmail.com>
2025-03-16 21:06:57 +0100

    misc type fixes

--- library/built-in/_prefabs/prefab_promptsWithButtons.ts
+++ library/built-in/_prefabs/prefab_promptsWithButtons.ts
@@ -8,17 +8,17 @@ import type { OutputFor } from './_prefabs'
 // with codebase typechecking performances, and making breaking
 // changes more explicit.
 export type UI_advancedPrompt = Z.List<
-   Z.Choice<{
+   Z.Union<{
       prompt: Z.Prompt
-      characters: Z.Choice<{
-         [k: string]: Z.Choice<{
-            [k: string]: Z.Group<SchemaDict>
+      characters: Z.Union<{
+         [k: string]: Z.Union<{
+            [k: string]: Z.Record<SchemaDict>
          }>
       }>
-      styles: Z.Choice<{
-         [k: string]: Z.Choice<{
-            [k: string]: Z.Choice<{
-               [k: string]: Z.Group<SchemaDict>
+      styles: Z.Union<{
+         [k: string]: Z.Union<{
+            [k: string]: Z.Union<{
+               [k: string]: Z.Record<SchemaDict>
             }>
          }>
       }>
@@ -31,52 +31,47 @@ export function ui_advancedPrompt(): UI_advancedPrompt {
       min: 1,
       layout: 'H',
       icon: IKONS.mdiBookAlphabet,
-      element: () =>
-         form.choice(
-            {
-               prompt: form.prompt({ default: ' \n' }),
-               characters: form.choice(
-                  Object.fromEntries(
-                     Object.entries(CHARACTER_GROUPS).map(([group, characters]) => [
-                        group,
-                        form.choice(
-                           Object.fromEntries(characters.map((character) => [character, form.group({})])),
-                           {
-                              appearance: 'tab',
-                           },
-                        ),
-                     ]),
-                  ),
-                  {
-                     appearance: 'tab',
-                  },
+      element: form.choice(
+         {
+            prompt: form.prompt({ default: ' \n' }),
+            characters: form.choice(
+               Object.fromEntries(
+                  Object.entries(CHARACTER_GROUPS).map(([group, characters]) => [
+                     group,
+                     form.choice(
+                        Object.fromEntries(characters.map((character) => [character, form.group({})])),
+                        { appearance: 'tab' },
+                     ),
+                  ]),
                ),
-               styles: form.choice(
-                  Object.fromEntries(
-                     Object.entries(STYLE_GROUPS).map(([group, subgroups]) => [
-                        group,
-                        form.choice(
-                           Object.fromEntries(
-                              Object.entries(subgroups).map(([subgroup, styles]) => [
-                                 subgroup,
+               { appearance: 'tab' },
+            ),
+            styles: form.choice(
+               Object.fromEntries(
+                  Object.entries(STYLE_GROUPS).map(([group, subgroups]) => [
+                     group,
+                     form.choice(
+                        Object.fromEntries(
+                           Object.entries(subgroups).map(([subgroup, styles]) => [
+                              subgroup,
 
-                                 form.choice(
-                                    Object.fromEntries(styles.map((style) => [style, form.group({})])),
-                                    {
-                                       appearance: 'tab',
-                                    },
-                                 ),
-                              ]),
-                           ),
-                           { appearance: 'tab' },
+                              form.choice(
+                                 Object.fromEntries(styles.map((style) => [style, form.group({})])),
+                                 {
+                                    appearance: 'tab',
+                                 },
+                              ),
+                           ]),
                         ),
-                     ]),
-                  ),
-                  { appearance: 'tab' },
+                        { appearance: 'tab' },
+                     ),
+                  ]),
                ),
-            },
-            { appearance: 'tab' },
-         ),
+               { appearance: 'tab' },
+            ),
+         },
+         { appearance: 'tab' },
+      ),
    })
 }
 
@@ -132,43 +127,15 @@ export const CHARACTER_GROUPS = {
     Zelda: [ 'Link', 'Zelda', 'Ganondorf', 'Navi', 'Impa', 'Paya', 'Midna', 'Sheik', 'Tetra', 'Malon', 'Saria', 'Ruto'],
     Mario: [ 'Mario', 'Luigi', 'Princess Peach', 'Daisy', 'Rosalina', 'Bowser', 'Bowsette', 'King Boo', 'Boosette', 'Toad', 'Toadette'],
 }
-
+// prettier-ignore
 export const STYLE_GROUPS = {
    ShowStyles: {
       Sonic: ['Classic', 'Modern', 'Boom', 'Comic', 'Pixel Art'],
       Zelda: ['Breath of the Wild', 'Twilight Princess', 'Wind Waker', 'Ocarina of Time', "Majora's Mask"],
    },
    ArtStyles: {
-      VideoGame: [
-         'Pixel Art',
-         'Low Poly',
-         'Cel Shaded',
-         'Realistic',
-         'Anime',
-         'Cartoon',
-         'Retro',
-         'Minimalist lineless',
-      ],
+      VideoGame: [ 'Pixel Art', 'Low Poly', 'Cel Shaded', 'Realistic', 'Anime', 'Cartoon', 'Retro', 'Minimalist lineless', ],
       Drawing: ['Sketch', 'Line Art', 'Coloring', 'Shading'],
-      Painting: [
-         'Impressionism',
-         'Surrealism',
-         'Cubism',
-         'Abstract',
-         'Expressionism',
-         'Pop Art',
-         'Dada',
-         'Futurism',
-         'Art Nouveau',
-         'Art Deco',
-         'Suprematism',
-         'Pointillism',
-         'Symbolism',
-         'Romanticism',
-         'Neoclassicism',
-         'Renaissance',
-         'Mannerism',
-         'Gothic',
-      ],
+      Painting: [ 'Impressionism', 'Surrealism', 'Cubism', 'Abstract', 'Expressionism', 'Pop Art', 'Dada', 'Futurism', 'Art Nouveau', 'Art Deco', 'Suprematism', 'Pointillism', 'Symbolism', 'Romanticism', 'Neoclassicism', 'Renaissance', 'Mannerism', 'Gothic', ],
    },
 }
