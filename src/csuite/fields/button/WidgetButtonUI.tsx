f621b853ed08767ae04922b5bc31766083bf3a6d
rvion <vion.remi@gmail.com>
2025-03-18 22:18:21 +0100

    comment out WidgetButtonUI

--- src/csuite/fields/button/WidgetButtonUI.tsx
+++ src/csuite/fields/button/WidgetButtonUI.tsx
@@ -1,29 +1,29 @@
-import type { Field_button, Field_button_context } from './FieldButton'
+// import type { Field_button, Field_button_context } from './FieldButton'
 
-import { runInAction } from 'mobx'
-import { observer } from 'mobx-react-lite'
+// import { runInAction } from 'mobx'
+// import { observer } from 'mobx-react-lite'
 
-import { Button } from '../../button/Button'
+// import { Button } from '../../button/Button'
 
-export const WidgetButtonUI = observer(function WidgetButtonUI_<K extends any>(p: {
-   field: Field_button<K>
-}) {
-   const extra = p.field.config.useContext?.() as K
-   const context: Field_button_context<K> = { widget: p.field, context: extra }
-   return (
-      <Button
-         look={p.field.config.look}
-         className='self-start'
-         icon={p.field.icon}
-         expand={p.field.config.expand}
-         onClick={() =>
-            runInAction(() => {
-               p.field.config.onClick?.(context)
-               p.field.touch()
-            })
-         }
-      >
-         {p.field.config.text ?? `Run`}
-      </Button>
-   )
-})
+// export const WidgetButtonUI = observer(function WidgetButtonUI_<K extends any>(p: {
+//    field: Field_button<K>
+// }) {
+//    const extra = p.field.config.useContext?.() as K
+//    const context: Field_button_context<K> = { widget: p.field, context: extra }
+//    return (
+//       <Button
+//          look={p.field.config.look}
+//          className='self-start'
+//          icon={p.field.icon}
+//          expand={p.field.config.expand}
+//          onClick={() =>
+//             runInAction(() => {
+//                p.field.config.onClick?.(context)
+//                p.field.touch()
+//             })
+//          }
+//       >
+//          {p.field.config.text ?? `Run`}
+//       </Button>
+//    )
+// })
