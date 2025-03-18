a11ee5e1c54a329fce9ae8895662496db08c411c
rvion <vion.remi@gmail.com>
2025-03-18 22:16:35 +0100

    fix > add missing import

--- src/csuite/fields/shared/FieldShared.tsx
+++ src/csuite/fields/shared/FieldShared.tsx
@@ -1,5 +1,5 @@
 import type { CSchema } from '../../model/CSchema'
-import type { CodegenOpts } from '../../model/FieldConstructor'
+import type { CodegenOpts, FieldConstructor } from '../../model/FieldConstructor'
 import type { Patch } from '../../model/Patch'
 import type { Repository } from '../../model/Repository'
 import type { Problem_Ext } from '../../model/Validation'
